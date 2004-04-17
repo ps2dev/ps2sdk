d4273cef561d82a233aa362ec0f59ae2e726cca6
oobles <oobles@fd1d1198-7684-4768-b9be-5b3c44f97897>
2004-04-17 05:04:25 +0000

    irx imports.

---
+++ iop/hdd/pfs/src/irx_imports.h
@@ -0,0 +1,25 @@
+/*
+ * irx_imports.h - Defines all IRX imports.
+ *
+ * Copyright (c) 2003 Marcus R. Brown <mrbrown@0xd6.org>
+ *
+ * See the file LICENSE included with this distribution for licensing terms.
+ */
+
+#ifndef IOP_IRX_IMPORTS_H
+#define IOP_IRX_IMPORTS_H
+
+#include "irx.h"
+
+/* Please keep these in alphabetical order!  */
+#include "cdvdman.h"
+#include "intrman.h"
+#include "iomanX.h"
+#include "stdio.h"
+#include "sysclib.h"
+#include "sysmem.h"
+#include "thbase.h"
+#include "thevent.h"
+#include "thsemap.h"
+
+#endif /* IOP_IRX_IMPORTS_H */
