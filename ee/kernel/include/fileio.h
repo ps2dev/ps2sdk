b13cbab54294a4cd124f2e9ee3c00c59d2bdc936
oobles <oobles@fd1d1198-7684-4768-b9be-5b3c44f97897>
2004-04-17 02:57:13 +0000

    files missed from ps2lib.

---
+++ ee/kernel/include/fileio.h
@@ -0,0 +1,63 @@
+/*
+  _____     ___ ____
+   ____|   |    ____|      PSX2 OpenSource Project
+  |     ___|   |____       (C)2001, Gustavo Scotti (gustavo@scotti.com)
+                           (c) 2003 Marcus R. Brown (mrbrown@0xd6.org)
+  ------------------------------------------------------------------------
+  fileio.c
+                        EE FILE IO handling
+                        These are my findings based on debug-info elf files.
+*/
+#ifndef _FILEIO_H
+#define _FILEIO_H
+
+#include <io_common.h>
+
+#define FIO_PATH_MAX	256
+
+#define FIO_WAIT		0
+#define FIO_NOWAIT		1
+
+#define FIO_COMPLETE	1
+#define FIO_INCOMPLETE	0
+
+#define	EOF	(-1)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int fioInit(void);
+void fioExit(void);
+int fioOpen(const char *fname, int mode);
+int fioClose( int fd);
+int fioRead( int fd, void *buff, int buff_size);
+int fioWrite( int fd, void *buff, int buff_size);
+int fioLseek( int fd, int offset, int whence);
+int fioMkdir(const char* dirname);
+int fioPutc(int fd,int c);
+int fioGetc(int fd);
+int fioGets(int fd, char* buff, int n);
+void fioSetBlockMode(int blocking);
+int fioSync(int mode, int *retVal);
+
+int nprintf(const char *format, ...);
+int printf(const char *format, ...);
+int sprintf(char*, const char *format, ...);
+
+// Yet to be properly tested..
+int fioIoctl(int fd, int request, void *data);
+int fioDopen(const char *name);
+int fioDclose(int fd);
+int fioDread(int fd, fio_dirent_t *buf);
+int fioGetstat(const char *name, fio_stat_t *buf);
+int fioChstat(const char *name, fio_stat_t *buf, unsigned int cbit);
+int fioRemove(const char *name);
+int fioFormat(const char *name);
+int fioRmdir(const char* dirname);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _FILEIO_H
