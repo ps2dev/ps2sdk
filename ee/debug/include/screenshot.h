1406dbc573b7bedc8e45a083d75eed361f50b178
emoon <emoon@fd1d1198-7684-4768-b9be-5b3c44f97897>
2004-05-23 11:22:12 +0000

    forgot to add ifdef _cplusplus

--- ee/debug/include/screenshot.h
+++ ee/debug/include/screenshot.h
@@ -1,9 +1,9 @@
 #ifndef __PS2SCREENSHOT_H__
 #define __PS2SCREENSHOT_H__
 
-//#ifdef __cplusplus
-//extern "C" {
-//#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 int ps2_screenshot_file( const char* pFilename,unsigned int VramAdress, 
                          unsigned int Width, unsigned int Height, unsigned int Psm );
@@ -11,8 +11,8 @@ int ps2_screenshot_file( const char* pFilename,unsigned int VramAdress,
 int ps2_screenshot( void* pTemp, unsigned int VramAdress,unsigned int x,unsigned int y,
                     unsigned int Width, unsigned int Height, unsigned int Psm );
 
-//#ifdef __cplusplus
-//}
-//#endif
+#ifdef __cplusplus
+}
+#endif
 
 #endif //__PS2SCREENSHOT_H__
