f8f7d96d64872d27eb26f3e16c2c0c0d057e8716
tyranid <tyranid@fd1d1198-7684-4768-b9be-5b3c44f97897>
2004-07-30 17:24:18 +0000

    Added Kernel Errors from TESTSPU in the BIOS. They were going begging :)

---
+++ iop/kernel/include/kerr.h
@@ -0,0 +1,64 @@
+/*
+ * kerr.h - Kernel Errors for the IOP
+ *
+ * Copyright (c) 2004 Probably Sony ;)
+ * Extracted from TESTSPU by TyRaNiD
+ *
+ * See the file LICENSE included with this distribution for licensing terms.
+ */
+
+/* I am not going to explain these errors, not sure myself for most of them */
+#ifndef __KERR_H__
+
+#define KE_OK			0
+#define KE_ERROR		-1
+#define KE_ILLEGAL_EXPCODE	-50
+#define KE_EXPHANDLER_NOUSE	-51
+#define KE_EXPHANDLER_USED	-52
+#define KE_ILLEGAL_CONTEXT	-100
+#define KE_ILLEGAL_INTRCODE	-101
+#define KE_CPUDI		-102
+#define KE_INTRDISABLE		-103
+#define KE_FOUND_HANDLER	-104
+#define KE_NOTFOUND_HANDLER	-105
+#define KE_NO_TIMER		-150
+#define KE_ILLEGAL_TIMERID	-151
+#define KE_UNIT_USED		-160
+#define KE_UNIT_NOUSE		-161
+#define KE_NO_ROMDIR		-162
+#define KE_LINKERR		-200
+#define KE_ILLEGAL_OBJECT	-201
+#define KE_UNKNOWN_MODULE	-202
+#define KE_NOFILE		-203
+#define KE_FILEERR		-204
+#define KE_MEMINUSE		-205
+#define KE_NO_MEMORY		-400
+#define KE_ILLEGAL_ATTR		-401
+#define KE_ILLEGAL_ENTRY	-402
+#define KE_ILLEGAL_PRIORITY	-403
+#define KE_ILLEGAL_STACK_SIZE	-404
+#define KE_ILLEGAL_MODE		-405
+#define KE_ILLEGAL_THID		-406
+#define KE_UNKNOWN_THID		-407
+#define KE_UNKNOWN_SEMID	-408
+#define KE_UNKNOWN_EVFID	-409
+#define KE_UNKNOWN_MBXID	-410
+#define KE_UNKNOWN_VPLID	-411
+#define KE_UNKNOWN_FPLID	-412
+#define KE_DORMANT		-413
+#define KE_NOT_DORMANT		-414
+#define KE_NOT_SUSPEND		-415
+#define KE_NOT_WAIT		-416
+#define KE_CAN_NOT_WAIT		-417
+#define KE_RELEASE_WAIT		-418
+#define KE_SEMA_ZERO		-419
+#define KE_SEMA_OVF		-420
+#define KE_EVF_COND		-421
+#define KE_EVF_MULTI		-422
+#define KE_EVF_ILPAT		-423
+#define KE_MBOX_NOMSG		-424
+#define KE_WAIT_DELETE		-425
+#define KE_ILLEGAL_MEMBLOCK	-426
+#define KE_ILLEGAL_MEMSIZE	-427
+
+#endif
