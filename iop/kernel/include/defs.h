6b4567fedb63150293f8f30594b188e6d050b94b
pixel <pixel@fd1d1198-7684-4768-b9be-5b3c44f97897>
2004-09-11 22:29:24 +0000

    Removing double defined stuff.

--- iop/kernel/include/defs.h
+++ iop/kernel/include/defs.h
@@ -21,16 +21,6 @@
 #define KSEG1		0xa0000000
 #define KSEG1ADDR(a)	((__typeof__(a))(((u32)(a) & 0x1fffffff) | KSEG1))
 
-extern inline u8  _lb(u32 addr) { return *(volatile u8 *)addr; }
-extern inline u16 _lh(u32 addr) { return *(volatile u16 *)addr; }
-extern inline u32 _lw(u32 addr) { return *(volatile u32 *)addr; }
-extern inline u64 _ld(u32 addr) { return *(volatile u64 *)addr; }
-
-extern inline void _sb(u8 val, u32 addr) { *(volatile u8 *)addr = val; }
-extern inline void _sh(u16 val, u32 addr) { *(volatile u16 *)addr = val; }
-extern inline void _sw(u32 val, u32 addr) { *(volatile u32 *)addr = val; }
-extern inline void _sd(u64 val, u32 addr) { *(volatile u64 *)addr = val; }
-
 static inline void *iop_memcpy(void *dest, const void *src, int size)
 {
 	u8 *d = (u8 *)dest, *s = (u8 *)src;
--- iop/kernel/include/types.h
+++ iop/kernel/include/types.h
@@ -11,33 +11,6 @@
 
 /* Pull in some standard types and definitions.  */
 #include <stddef.h>
-
-typedef __signed__ char __s8;
-typedef unsigned char __u8;
-#if !defined(s8) && !defined(u8)
-#define s8 __s8
-#define u8 __u8
-#endif
-
-typedef __signed__ short __s16;
-typedef unsigned short __u16;
-#if !defined(s16) && !defined(u16)
-#define s16 __s16
-#define u16 __u16
-#endif
-
-typedef __signed__ int __s32;
-typedef unsigned int __u32;
-#if !defined(s32) && !defined(u32)
-#define s32 __s32
-#define u32 __u32
-#endif
-
-typedef __signed__ long long __s64;
-typedef unsigned long long __u64;
-#if !defined(s64) && !defined(u64)
-#define s64 __s64
-#define u64 __u64
-#endif
+#include <tamtypes.h>
 
 #endif /* IOP_TYPES_H */
