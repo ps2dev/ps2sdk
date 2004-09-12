721dfcc42ee3ab6a2eebcb87e6b27e6c315f926a
pixel <pixel@fd1d1198-7684-4768-b9be-5b3c44f97897>
2004-09-12 00:14:05 +0000

    Trying to put some true "floating point supporting" setjmp/longjmp.

--- common/include/setjmp.h
+++ common/include/setjmp.h
@@ -6,10 +6,11 @@
 #include <tamtypes.h>
 
 #if defined(_EE)
-#define _JBLEN 11
+/* we need some room after the gpr, to store some fp registers. */
+#define _JBLEN 14
 #define _JBTYPE u128
 #else
-/* seems the IOP's sysclib does have one more value... */
+/* seems the IOP's sysclib does have one more value than the newlib's version... */
 #define _JBLEN 12
 #define _JBTYPE u32
 #endif
