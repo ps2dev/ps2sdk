7872ac908edba8177e6958f2d153973b3225b6e3
pixel <pixel@fd1d1198-7684-4768-b9be-5b3c44f97897>
2004-09-15 12:18:46 +0000

    Aah, yeah, forgot that some freaks are still using C++ :p

--- common/include/setjmp.h
+++ common/include/setjmp.h
@@ -30,7 +30,15 @@
 
 typedef _JBTYPE jmp_buf[_JBLEN];
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int setjmp(jmp_buf env);
 void longjmp(jmp_buf env, int val);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
