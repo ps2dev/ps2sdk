0847ad6844cb41fa87945f33045b0381b6921848
pixel <pixel@fd1d1198-7684-4768-b9be-5b3c44f97897>
2004-10-01 12:24:57 +0000

    Moving functions around...

---
+++ ee/libc/include/ctype.h
@@ -0,0 +1,39 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2001-2004, ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+#
+# $Id$
+# A few ctype's functions. They should be macros but...
+*/
+
+#ifndef _CTYPE_H
+#define _CTYPE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+int isalnum(int);
+int isalpha(int);
+int iscntrl(int);
+int isdigit(int);
+int isgraph(int);
+int islower(int);
+int isprint(int);
+int ispunct(int);
+int isspace(int);
+int isupper(int);
+int isxdigit(int);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	// _CTYPE_H
--- ee/libc/include/string.h
+++ ee/libc/include/string.h
@@ -75,21 +75,8 @@ char *	strstr(const char *, const char *);
 char * strupr(char *);
 char * strlwr(char *);
 
-int tolower(int);
-int toupper(int);
-
-int isalnum(int);
-int isalpha(int);
-int iscntrl(int);
-int isdigit(int);
-int isgraph(int);
-int islower(int);
-int isprint(int);
-int ispunct(int);
-int isspace(int);
-int isupper(int);
-int isxdigit(int);
-
+/* Backward compatibility... */
+#include <ctype.h>
 
 #ifdef __cplusplus
 }
