77e803ea32318eca4ac382a858da3f2eb18a7b77
stefan <stefan@fd1d1198-7684-4768-b9be-5b3c44f97897>
2004-10-19 05:25:33 +0000

    Cleanup ps2_sbrk().

--- ee/libc/src/sbrk.c
+++ ee/libc/src/sbrk.c
@@ -13,44 +13,24 @@
 # Primary function to malloc - reserved
 */
 
-#include <tamtypes.h>
 #include <kernel.h>
 
 extern void * _end;
-static void * _heap_ptr = &_end;
 
 void *ps2_sbrk(size_t increment)
 {
+	static void * _heap_ptr = &_end;
 	void *mp, *ret = (void *)-1;
-	int state = 0;
 
-	/* XXX: Workaround for the obscure cases when _heap_ptr isn't
-	   resolved by the linker.  */
-	if (!_heap_ptr)
-		_heap_ptr = &_end;
-
-	/* increment is 0 if the caller just wants the top of the heap.  */
-	if (!increment)
+	if (increment == 0)
 		return _heap_ptr;
 
-	asm volatile ("mfc0 %0, $12" : "=r" (state));
-	state = state & 0x10000;
-
-	if (state)
-		DI();
-
-	/* If the area we want to allocated is past the end of our heap, we
-	   have a problem.  */
+	/* If the area we want to allocated is past the end of our heap, we have a problem. */
 	mp = _heap_ptr + increment;
-	if (mp > EndOfHeap())
-		goto out;
-
-	ret = _heap_ptr;
-	_heap_ptr = mp;
-
-out:
-	if (state)
-		EI();
+	if (mp <= EndOfHeap()) {
+		ret = _heap_ptr;
+		_heap_ptr = mp;
+	}
 
 	return ret;
 }
