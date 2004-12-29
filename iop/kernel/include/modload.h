628ab36f7b38744243b19c53a0e7a0654c94ebb3
pixel <pixel@fd1d1198-7684-4768-b9be-5b3c44f97897>
2004-12-29 12:53:56 +0000

    Just copied some stuff from kernel.s into modload.h, and filled some argument names using a bit of RE work on the rom0:MODLOAD file.

--- iop/kernel/include/modload.h
+++ iop/kernel/include/modload.h
@@ -20,7 +20,25 @@
 #define modload_IMPORTS_start DECLARE_IMPORT_TABLE(modload, 1, 1)
 #define modload_IMPORTS_end END_IMPORT_TABLE
 
+int ReBootStart(int);
+#define I_ReBootStart DECLARE_IMPORT(4, ReBootStart)
+
+int LoadModuleAddress(const char *name, int, int);
+#define I_LoadModuleAddress DECLARE_IMPORT(5, LoadModuleAddress)
+
+int LoadModule(const char *name);
+#define I_LoadModule DECLARE_IMPORTS(6, LoadModule)
+
 int LoadStartModule(const char *name, int arglen, const char *args, int *result);
 #define I_LoadStartModule DECLARE_IMPORT(7, LoadStartModule)
+
+int StartModule(int, const char *name, int arg_len, const char *args, int *result);
+#define I_StartModule DECLARE_IMPORTS(8, StartModule)
+
+int LoadModuleBufferAddress(void *buffer, int, int);
+#define I_LoadModuleBufferAddress DECLARE_IMPORTS(9, LoadModuleBufferAddress)
+
+int LoadModuleBuffer(void *buffer);
+#define I_LoadModuleBuffer DECLARE_IMPORTS(10, LoadModuleBuffer)
 
 #endif /* IOP_MODLOAD_H */
