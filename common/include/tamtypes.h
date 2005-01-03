3e4f147f80d2d6e19fa32d6bf1b30ee18a90270f
pixel <pixel@fd1d1198-7684-4768-b9be-5b3c44f97897>
2005-01-03 00:32:43 +0000

    Hey herbie, don't let the job half done like this ;)
    Also fixed the IOP's s32 and s64... (they were *un*signed!)

--- common/include/tamtypes.h
+++ common/include/tamtypes.h
@@ -18,7 +18,7 @@ typedef	unsigned char 		u8;
 typedef unsigned short 		u16;
 
 typedef	volatile unsigned char 		vu8;
-typedef volatile unsigned short 		vu16;
+typedef volatile unsigned short 	vu16;
 
 #ifdef _EE
 typedef unsigned int		u32;
@@ -27,7 +27,7 @@ typedef unsigned int		u128 __attribute__(( mode(TI) ));
 
 typedef volatile unsigned int		vu32;
 typedef volatile unsigned long int	vu64;
-
+typedef volatile unsigned int		vu128 __attribute__(( mode(TI) ));
 #else
 typedef unsigned long int	u32;
 typedef unsigned long long	u64;
@@ -39,32 +39,42 @@ typedef volatile unsigned long long	vu64;
 typedef signed char 		s8;
 typedef signed short 		s16;
 
+typedef volatile signed char	vs8;
+typedef volatile signed short	vs16;
+
 #ifdef _EE
 typedef signed int		s32;
 typedef signed long int		s64;
 typedef signed int		s128 __attribute__(( mode(TI) ));
+
+typedef volatile signed int		vs32;
+typedef volatile signed long int	vs64;
+typedef volatile signed int		vs128 __attribute__(( mode(TI) ));
 #else
-typedef unsigned long int	s32;
-typedef unsigned long long	s64;
+typedef signed long int		s32;
+typedef signed long long	s64;
+
+typedef volatile signed int		vs32;
+typedef volatile signed long long	vs64;
 #endif
 
 #ifndef NULL
 #define NULL	(void *)0
 #endif
 
-static inline u8  _lb(u32 addr) { return *(volatile u8 *)addr; }
-static inline u16 _lh(u32 addr) { return *(volatile u16 *)addr; }
-static inline u32 _lw(u32 addr) { return *(volatile u32 *)addr; }
-static inline u64 _ld(u32 addr) { return *(volatile u64 *)addr; }
+static inline u8  _lb(u32 addr) { return *(vu8 *)addr; }
+static inline u16 _lh(u32 addr) { return *(vu16 *)addr; }
+static inline u32 _lw(u32 addr) { return *(vu32 *)addr; }
+static inline u64 _ld(u32 addr) { return *(vu64 *)addr; }
 
-static inline void _sb(u8 val, u32 addr) { *(volatile u8 *)addr = val; }
-static inline void _sh(u16 val, u32 addr) { *(volatile u16 *)addr = val; }
-static inline void _sw(u32 val, u32 addr) { *(volatile u32 *)addr = val; }
-static inline void _sd(u64 val, u32 addr) { *(volatile u64 *)addr = val; }
+static inline void _sb(u8 val, u32 addr) { *(vu8 *)addr = val; }
+static inline void _sh(u16 val, u32 addr) { *(vu16 *)addr = val; }
+static inline void _sw(u32 val, u32 addr) { *(vu32 *)addr = val; }
+static inline void _sd(u64 val, u32 addr) { *(vu64 *)addr = val; }
 
 #ifdef _EE
-static inline u128 _lq(u32 addr) { return *(volatile u128 *)addr; }
-static inline void _sq(u128 val, u32 addr) { *(volatile u128 *)addr = val; }
+static inline u128 _lq(u32 addr) { return *(vu128 *)addr; }
+static inline void _sq(u128 val, u32 addr) { *(vu128 *)addr = val; }
 #endif
 
 #endif
