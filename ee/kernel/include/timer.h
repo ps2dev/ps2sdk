da6d2a6141e0019393d851fd03aaaf181c480ea5
pixel <pixel@fd1d1198-7684-4768-b9be-5b3c44f97897>
2005-01-05 16:36:14 +0000

    Per request.. ;)

---
+++ ee/kernel/include/timer.h
@@ -0,0 +1,27 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2001-2004, ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+#
+# $Id$
+# Timer prototypes
+*/
+
+#ifndef __TIMER_H__
+#define __TIMER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+u32 cpu_ticks(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
