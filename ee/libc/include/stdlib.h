50f8b4afae202f0b7eb4c6adc0b58000c7bb809f
gawd <gawd@fd1d1198-7684-4768-b9be-5b3c44f97897>
2005-02-11 17:56:56 +0000

    added prototype for exit()

--- ee/libc/include/stdlib.h
+++ ee/libc/include/stdlib.h
@@ -86,6 +86,7 @@ void          abort(void) __attribute__ ((noreturn));
 int           abs(int);
 int           atexit(void (*)(void));
 double        atof(const char *);
+void          exit(int);
 //int           atoi(const char *);
 //long          atol(const char *);
 #define       atoi(x) strtol(x, NULL, 10)
