a56a8b284d4c201c1e1119773626807861202439
gawd <gawd@fd1d1198-7684-4768-b9be-5b3c44f97897>
2005-02-12 13:14:13 +0000

    initial checkin (dummy header file)

---
+++ common/include/sys/select.h
@@ -0,0 +1,16 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2001-2005, ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+#
+# $Id$
+*/
+
+#ifndef SYS_SELECT_H
+#define SYS_SEELCT_H
+
+#endif /* SYS_SELECT_H */
