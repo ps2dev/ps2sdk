bf99641d02a7b6b490c9b54650220b1610c87889
gawd <gawd@fd1d1198-7684-4768-b9be-5b3c44f97897>
2005-03-04 10:31:22 +0000

    added time.c & time.h

---
+++ ee/libc/include/time.h
@@ -0,0 +1,34 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2001-2005, ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+#
+# $Id$
+# Struct and function declarations for dealing with time
+*/
+
+#ifndef _TIME_H
+#define _TIME_H
+
+typedef unsigned long clock_t;
+
+struct tm
+{
+  int	tm_sec;
+  int	tm_min;
+  int	tm_hour;
+  int	tm_mday;
+  int	tm_mon;
+  int	tm_year;
+  int	tm_wday;
+  int	tm_yday;
+  int	tm_isdst;
+};
+
+clock_t clock();
+
+#endif  // TIME_H
---
+++ ee/libc/src/time.c
@@ -0,0 +1,27 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2001-2005, ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+#
+# $Id$
+# Standard libc time functions
+*/
+
+#include <time.h>
+
+/* According to the POSIX standard, if the process time can't be determined
+ * clock should return (clock_t)-1
+ */
+clock_t clock(void) 
+{
+	/* POSIX standard require clock() to return -1 if process time
+	 * cannot be determined.
+	 */
+	return (clock_t)-1;
+}
+
+
