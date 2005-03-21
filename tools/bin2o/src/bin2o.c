2eb5cf06c7a53e6a97736042969461270530bcee
pixel <pixel@fd1d1198-7684-4768-b9be-5b3c44f97897>
2005-03-21 10:04:33 +0000

    Adding various and entertaining options.

--- tools/bin2o/src/bin2o.c
+++ tools/bin2o/src/bin2o.c
@@ -8,6 +8,7 @@
 # Review ps2sdk README & LICENSE files for further details.
 #
 # $Id$
+# Convets any file into a PS2's .o file.
 */
 
 #include <sys/types.h>
@@ -21,6 +22,10 @@
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
+
+int alignment = 16;
+int have_size = 1;
+int have_irx = 0;
 
 u32 LE32(u32 b) {
     u32 t = 0x12345678;
@@ -83,26 +88,39 @@ struct elf_symbol_t {
 //                 0 1234567890 12345678 90123456 78901
 char shstrtab[] = "\0.shstrtab\0.symtab\0.strtab\0.data";
 
-void create_elf(FILE * dest, unsigned char * source, u32 size, char * label) {
-    int i;
+void create_elf(FILE * dest, const unsigned char * source, u32 size, const char * label) {
+    int i, l_size;
     struct elf_section_t section;
     struct elf_symbol_t symbol;
     u32 strtab_size;
     char strtab[512];
     u32 data_size[4];
     
+    if (have_irx) {
+	elf_header[36] = 1;
+	elf_header[37] = 0;
+	elf_header[38] = 0;
+	elf_header[39] = 0;
+    }
+    
     for (i = 0; i < sizeof(elf_header); i++) {
 	fputc(elf_header[i], dest);
     }
     
-    strtab_size = (strlen(label) * 3 + 1 + 7 + 5 + 6);
+    l_size = strlen(label);
+    
     strtab[0] = 0;
     strcpy(strtab + 1, label);
     strcat(strtab + 1, "_start");
-    strcpy(strtab + 1 + strlen(label) + 7, label);
-    strcat(strtab + 1 + strlen(label) + 7, "_end");
-    strcpy(strtab + 1 + strlen(label) + 7 + strlen(label) + 5, label);
-    strcat(strtab + 1 + strlen(label) + 7 + strlen(label) + 5, "_size");
+    strcpy(strtab + 1 + l_size + 7, label);
+    strcat(strtab + 1 + l_size + 7, "_end");
+    if (have_size) {
+        strtab_size = (l_size * 3 + 1 + 7 + 5 + 6);
+        strcpy(strtab + 1 + l_size + 7 + l_size + 5, label);
+        strcat(strtab + 1 + l_size + 7 + l_size + 5, "_size");
+    } else {
+        strtab_size = (l_size * 2 + 1 + 7 + 5);
+    }
     
     // section 0 (NULL)
     section.sh_name = section.sh_type = section.sh_flags = section.sh_addr = section.sh_offset = section.sh_size =
@@ -132,7 +150,7 @@ void create_elf(FILE * dest, unsigned char * source, u32 size, char * label) {
     section.sh_flags = 0;
     section.sh_addr = 0;
     section.sh_offset = LE32(40 * 5 + 0x34 + 33);
-    section.sh_size = LE32(0x30);
+    section.sh_size = LE32(have_size ? 0x30 : 0x20);
     section.sh_link = LE32(3);
     section.sh_info = 0;
     section.sh_addralign = LE32(4);
@@ -146,7 +164,7 @@ void create_elf(FILE * dest, unsigned char * source, u32 size, char * label) {
     section.sh_type = LE32(3); // STRTAB
     section.sh_flags = 0;
     section.sh_addr = 0;
-    section.sh_offset = LE32(40 * 5 + 0x34 + 33 + 0x30);
+    section.sh_offset = LE32(40 * 5 + 0x34 + 33 + (have_size ? 0x30 : 0x20));
     section.sh_size = LE32(strtab_size);
     section.sh_link = 0;
     section.sh_info = 0;
@@ -161,10 +179,10 @@ void create_elf(FILE * dest, unsigned char * source, u32 size, char * label) {
     section.sh_type = LE32(1); // PROGBITS
     section.sh_flags = LE32(3); // Write + Alloc
     section.sh_addr = 0;
-    section.sh_offset = LE32(40 * 5 + 0x34 + 33 + 0x30 + strtab_size);
-    section.sh_size = LE32(size + 16);
+    section.sh_offset = LE32(40 * 5 + 0x34 + 33 + (have_size ? 0x30 : 0x20) + strtab_size);
+    section.sh_size = LE32(size + have_size * 16);
     section.sh_link = 0;
-    section.sh_addralign = LE32(16);
+    section.sh_addralign = LE32(alignment);
     section.sh_entsize = 0;
     
     fwrite(&section, 1, sizeof(section), dest);
@@ -174,37 +192,52 @@ void create_elf(FILE * dest, unsigned char * source, u32 size, char * label) {
     fwrite(shstrtab, 1, 33, dest);
 
     symbol.st_name = LE32(1);
-    symbol.st_value = 16;
+    symbol.st_value = LE32(have_size * 16);
     symbol.st_size = LE32(size);
     symbol.st_info = 0x11;
     symbol.st_other = 0;
     symbol.st_shndx = LE16(4);
     fwrite(&symbol, 1, sizeof(symbol), dest);
 
-    symbol.st_name = LE32(strlen(label) + 1 + 7);
-    symbol.st_value = LE32(size + 16);
+    symbol.st_name = LE32(l_size + 1 + 7);
+    symbol.st_value = LE32(size + have_size * 16);
     symbol.st_size = 0;
     symbol.st_info = 0x11;
     symbol.st_other = 0;
     symbol.st_shndx = LE16(4);
     fwrite(&symbol, 1, sizeof(symbol), dest);
 
-    symbol.st_name = LE32(strlen(label) * 2 + 1 + 7 + 5);
-    symbol.st_value = 0;
-    symbol.st_size = 4;
-    symbol.st_info = 0x11;
-    symbol.st_other = 0;
-    symbol.st_shndx = LE16(4);
-    fwrite(&symbol, 1, sizeof(symbol), dest);
+    if (have_size) {
+        symbol.st_name = LE32(l_size * 2 + 1 + 7 + 5);
+        symbol.st_value = 0;
+        symbol.st_size = 4;
+        symbol.st_info = 0x11;
+        symbol.st_other = 0;
+        symbol.st_shndx = LE16(4);
+        fwrite(&symbol, 1, sizeof(symbol), dest);
+    }
 
     fwrite(strtab, 1, strtab_size, dest);
     
-    data_size[0] = LE32(size);
-    data_size[1] = 0;
-    data_size[2] = 0;
-    data_size[3] = 0;
-    fwrite(data_size, 4, 4, dest);
+    if (have_size) {
+        data_size[0] = LE32(size);
+        data_size[1] = 0;
+        data_size[2] = 0;
+        data_size[3] = 0;
+        fwrite(data_size, 4, 4, dest);
+    }
     fwrite(source, 1, size, dest);
+}
+
+void usage() {
+    printf("bin2o - Converts a binary file into a .o file.\n"
+           "Usage: bin2o [-a XX] [-n] [-i] infile outfile label\n"
+	   "  -i    - create an iop-compatible .o file.\n"
+	   "  -n    - don't add label_size symbol.\n"
+	   "  -a XX - set .data section alignment to XX.\n"
+	   "          (has to be a power of two).\n"
+	   "\n"
+	  );
 }
 
 int main(int argc, char *argv[])
@@ -212,15 +245,64 @@ int main(int argc, char *argv[])
     u32 fd_size;
     unsigned char * buffer;
     FILE * source, * dest;
+    char * f_source = 0, * f_dest = 0, * f_label = 0;
+    int i;
 
-    if (argc != 4) {
-	printf("bin2o - Converts a binary file into a .o file.\n"
-	       "Usage: bin2o infile outfile label\n\n");
+    for (i = 1; i < argc; i++) {
+	if (argv[i][0] == '-') {
+	    switch (argv[i][1]) {
+	    case 'a':
+		i++;
+		if (!argv[i]) {
+		    usage();
+		    printf("-a requires an argument.\n");
+		    return 1;
+		}
+		if (argv[i][0] == '-') {
+		    usage();
+		    printf("-a requires an argument.\n");
+		    return 1;
+		}
+		alignment = atoi(argv[i]);
+		if ((alignment - 1) & alignment) {
+		    printf("Error: alignment must be a power of 2.\n");
+		    return 1;
+		}
+		break;
+	    case 'n':
+		have_size = 0;
+		break;
+	    case 'i':
+		have_irx = 1;
+		break;
+	    default:
+		usage();
+		printf("Unknow option: %s.\n", argv[i]);
+		return 1;
+	    }
+	} else {
+	    if (!f_source) {
+		f_source = argv[i];
+	    } else if (!f_dest) {
+		f_dest = argv[i];
+	    } else if (!f_label) {
+		f_label = argv[i];
+	    } else {
+		usage();
+		printf("Too many arguments.\n");
+		return 1;
+	    }
+	}
+    }
+    
+    if (!f_source || !f_dest || !f_label) {
+	usage();
+	printf("Not enough arguments.\n");
 	return 1;
     }
 
-    if (!(source = fopen(argv[1], "rb"))) {
-	printf("Error opening %s for reading.\n", argv[1]);
+    if (!(source = fopen(f_source, "rb"))) {
+	printf("Error opening %s for reading.\n", f_source);
 	return 1;
     }
 
@@ -240,12 +322,12 @@ int main(int argc, char *argv[])
     }
     fclose(source);
 
-    if (!(dest = fopen(argv[2], "wb+"))) {
-	printf("Failed to open/create %s.\n",argv[2]);
+    if (!(dest = fopen(f_dest, "wb+"))) {
+	printf("Failed to open/create %s.\n", f_dest);
 	return 1;
     }
     
-    create_elf(dest, buffer, fd_size, argv[3]);
+    create_elf(dest, buffer, fd_size, f_label);
 
     fclose(dest);
     free(buffer);
