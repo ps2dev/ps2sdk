fd69129f98e583925797a0cbabc4d9800b3917a6
gawd <gawd@fd1d1198-7684-4768-b9be-5b3c44f97897>
2005-04-24 14:26:32 +0000

    initial import of audsrv 0.75

---
+++ iop/sound/audsrv/src/irx_imports.h
@@ -0,0 +1,18 @@
+#ifndef IOP_IRX_IMPORTS_H
+#define IOP_IRX_IMPORTS_H
+
+#include <irx.h>
+
+#include <intrman.h>
+#include <loadcore.h>
+#include <stdio.h>
+#include <sifcmd.h>
+#include <sifrpc.h>
+#include <sysclib.h>
+#include <sysmem.h>
+#include <thbase.h>
+#include <thsemap.h>
+
+#include <libsd.h>
+
+#endif
