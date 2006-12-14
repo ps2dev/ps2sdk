fbdea39a7d700e743ea950427beefaa0af43edc3
eeug <eeug@fd1d1198-7684-4768-b9be-5b3c44f97897>
2006-12-14 19:41:04 +0000

    Added copyright header

--- ee/mpeg/src/libmpeg_core.s
+++ ee/mpeg/src/libmpeg_core.s
@@ -1,3 +1,10 @@
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright (c) 2006 Eugene Plotnikov <e-plotnikov@operamail.com>
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
 .set noreorder
 .set nomacro
 .set noat
