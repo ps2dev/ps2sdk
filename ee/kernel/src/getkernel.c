c324b4a3f51f901ddd930f2268f56826dbcc7052
Herben <Herben@fd1d1198-7684-4768-b9be-5b3c44f97897>
2009-10-07 17:02:46 +0000

    Fixed some bugs in GetExceptionHandler().

--- ee/kernel/src/getkernel.c
+++ ee/kernel/src/getkernel.c
@@ -19,40 +19,40 @@ static u32* g_pSyscallTable = NULL;
 static void InitSyscallTable(void)
 {
     u32 oldintr, oldop;
-	u32 *pAddr;
+    u32 *pAddr;
 
     oldintr = DIntr();
-    oldop = ee_opmode_set(0);
-	pAddr = (u32 *) 0x800002f0;
-	g_pSyscallTable = (u32 *) ((pAddr[0] << 16) | (pAddr[2] & 0xFFFF));
-    ee_opmode_set(oldop);
+    oldop = ee_set_opmode(0);
+    pAddr = (u32 *) 0x800002f0;
+    g_pSyscallTable = (u32 *) ((pAddr[0] << 16) | (pAddr[2] & 0xFFFF));
+    ee_set_opmode(oldop);
     if(oldintr) { EIntr(); }
 }
 
 /** Get the address of an EE syscall handler function.
     @param syscall_no - The syscall number.
-	@return - The address of the syscall handler function (or NULL)
+    @return - The address of the syscall handler function (or NULL)
 */
 void *GetSyscallHandler(int syscall_no)
 {
     u32 oldintr, oldop;
-	u32 addr = 0;
+    u32 addr = 0;
 
-	if(g_pSyscallTable == NULL)
-	{
-		InitSyscallTable();
-	}
+    if(g_pSyscallTable == NULL)
+    {
+        InitSyscallTable();
+    }
 
-	if(g_pSyscallTable != NULL)
-	{
+    if(g_pSyscallTable != NULL)
+    {
         oldintr = DIntr();
-	    oldop = ee_opmode_set(0);
-		addr = g_pSyscallTable[syscall_no];
-	    ee_opmode_set(oldop);
+        oldop = ee_set_opmode(0);
+        addr = g_pSyscallTable[syscall_no];
+        ee_set_opmode(oldop);
         if(oldintr) { EIntr(); }
-	}
+    }
 
-	return (void *) addr;
+    return (void *) addr;
 }
 #endif
 
@@ -62,51 +62,38 @@ extern void *GetSyscallHandler(int syscall_no);
 
 /** Get the address of an EE exception handler function.
     @param ex_cause_no - The exception cause number.
-	@return - The address of the exception handler function (or NULL)
+    @return - The address of the exception handler function (or NULL)
 */
 void *GetExceptionHandler(int ex_cause_no)
 {
     u32 oldintr, oldop;
-	u32 addr;
+    u32 addr, table_addr;
     u16 lo16, hi16;
 
-    if((ex_cause_no >= 1) && (ex_cause_no < 4))
+    if((ex_cause_no < 1) || (ex_cause_no > 15))
     {
-        // TLB-related exceptions(1-3)
-
-        // get address of the syscall "SetVTLBRefillHandler"
-        addr = GetSyscallHandler(13);
-    }
-    else if((ex_cause_no >= 4) && (ex_cause_no < 16))
-    {
-        // "common" exceptions(4-15)
-
-        // get address of the syscall "SetVCommonHandler"
-        addr = GetSyscallHandler(14);
-
-        ex_cause_no -= 4; // adjust the cause number
-    }
-    else
-    {
-        // everything else
         return(NULL);
     }
+    
+    // get address of the syscall "SetVTLBRefillHandler"
+    addr = GetSyscallHandler(13);
 
     // suspend interrupts and enter "kernel" operating mode.
     oldintr = DIntr();
-	oldop = ee_opmode_set(0);
+    oldop = ee_set_opmode(0);
 
-    // harvest the address of the corresponding exception handler table.
+    // harvest the address of the exception handler table.
     lo16 = ((vu32 *) addr)[0x20 / 4];
     hi16 = ((vu32 *) addr)[0x14 / 4];
+    table_addr = ((u32) (hi16 << 16) | lo16);
     
-    addr = ((u32 *) ((u32) (hi16 << 16) | lo16))[ex_cause_no];
+    addr = ((u32 *) table_addr)[ex_cause_no];
 
     // return to the old operating mode and resume interrupts.
-	ee_opmode_set(oldop);
+    ee_set_opmode(oldop);
     if(oldintr) { EIntr(); }
 
-	return (void *) addr;
+    return((void *) addr);
 }
 #endif
 
@@ -116,12 +103,12 @@ extern void *GetSyscallHandler(int syscall_no);
 
 /** Get the address of an EE interrupt handler function.
     @param intr_cause_no - The interrupt number.
-	@return - The address of the interrupt handler function (or NULL)
+    @return - The address of the interrupt handler function (or NULL)
 */
 void *GetInterruptHandler(int intr_cause_no)
 {
     u32 oldintr, oldop;
-	u32 addr;
+    u32 addr;
     u16 lo16, hi16;
 
     // make sure intr_cause_no is between 0 and 7
@@ -135,7 +122,7 @@ void *GetInterruptHandler(int intr_cause_no)
 
     // suspend interrupts and enter "kernel" operating mode.
     oldintr = DIntr();
-	oldop = ee_opmode_set(0);
+    oldop = ee_set_opmode(0);
 
     // harvest the address of the corresponding exception handler table.
     lo16 = ((vu32 *) addr)[0x10 / 4];
@@ -144,10 +131,10 @@ void *GetInterruptHandler(int intr_cause_no)
     addr = ((u32 *) ((u32) (hi16 << 16) | lo16))[intr_cause_no];
 
     // return to the old operating mode and resume interrupts.
-	ee_opmode_set(oldop);
+    ee_set_opmode(oldop);
     if(oldintr) { EIntr(); }
 
-	return (void *) addr;
+    return (void *) addr;
 }
 #endif
 
