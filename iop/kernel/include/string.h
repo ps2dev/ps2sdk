1a8186df1bb91323f48cc4756f7d3014b91e8db4
Herben <Herben@fd1d1198-7684-4768-b9be-5b3c44f97897>
2009-11-03 23:24:14 +0000

    Added a dummy "string.h" to IOP include.  Simply includes "sysclib.h".  This allows lwip 1.3.1 to compile, and perhaps other things.

---
+++ iop/kernel/include/string.h
@@ -0,0 +1,20 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2001-2004, ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+#
+# $Id: string.h 1099 2005-05-20 07:11:14Z pixel $
+# String function prototypes
+*/
+
+#ifndef _STRING_H
+#define _STRING_H
+
+#include <sysclib.h>
+
+#endif	// _STRING_H
+
