1a5defdf8eaa57f9a40cdf504bf1c2725c8b3aba
jim <jim@fd1d1198-7684-4768-b9be-5b3c44f97897>
2010-05-18 17:44:20 +0000

    More keywords

--- iop/usb/usbd/src/hcd.c
+++ iop/usb/usbd/src/hcd.c
@@ -7,7 +7,7 @@
 # Licenced under Academic Free License version 2.0
 # Review ps2sdk README & LICENSE files for further details.
 #
-# $Id: $
+# $Id$
 # USB Driver function prototypes and constants.
 */
 
