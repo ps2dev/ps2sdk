ac8204b5ca68281f20e68520bb6f75593b8cc9e9
jim <jim@fd1d1198-7684-4768-b9be-5b3c44f97897>
2010-08-03 18:33:56 +0000

    http://github.com/ragnarok2040/ps2sdk/commit/6b92a95c24a615ea437c1fb7855d61ff7830f2cb.diff

--- ee/libc/src/libgen.c
+++ ee/libc/src/libgen.c
@@ -21,22 +21,103 @@
 char *basename(char *path)
 {
     static char base[PATH_MAX];
-    char *end;
+    char all_slashes = 1;
+    char *start,*end;
 
     strcpy(base,".");
 
     if (path == NULL || *path == '\0')
         return base;
 
+    // If no directory specified, return to :
     if ((end = strrchr(path, '/')) != NULL);
     else if ((end = strrchr(path, '\\')) != NULL);
-    else if ((end = strrchr(path, ':')) != NULL);
+    else if ((end = strrchr(path,':')) != NULL);
     else { strcpy(base,path); return base; }
+
+    if (end == path)
+    {
+        if (*end != ':')
+        {
+            base[0] = *end;
+            base[1] = '\0';
+        }
+
+        return base;
+    }
 
     end++;
 
+    // if we're not at the end of a path, 
+    // then the path ended in a '/','\\', or ':'
     if (*end != '\0')
+    {
         strcpy(base,end);
+    }
+    else
+    {
+
+        end--;
+
+        // The path most likely ends in a '/'
+        // so search for a previous '/','\\', or':'
+        start = end;
+
+        while(start != path)
+        {
+            start--;
+
+            if (*start == '/')
+            {
+                if (!all_slashes)
+                {
+                    start++;
+                    memcpy(base,start,end-start);
+                    base[end-start] = '\0';
+                    return base;
+                }
+                else
+                {
+                    end--;
+                    continue;
+                }
+            }
+            else if (*start == '\\')
+            {
+                if (!all_slashes)
+                {
+                    start++;
+                    memcpy(base,start,end-start);
+                    base[end-start] = '\0';
+                    return base;
+                }
+                else
+                {
+                    end--;
+                    continue;
+                }
+            }
+            else if (*start == ':')
+            {
+                if (!all_slashes)
+                {
+                    start++;
+                    memcpy(base,start,end-start);
+                    base[end-start] = '\0';
+                    return base;
+                }
+                else
+                {
+                    end--;
+                    continue;
+                }
+            }
+            else
+            {
+                all_slashes = 0;
+            }
+        }
+    }
 
     return base;
 }
@@ -46,6 +127,7 @@ char *basename(char *path)
 char *dirname(char *path)
 {
     static char dir[PATH_MAX];
+    char all_slashes = 1;
     char *end;
 
     strcpy(dir,".");
@@ -53,15 +135,114 @@ char *dirname(char *path)
     if (path == NULL || *path == '\0')
         return dir;
 
+    // If no device specifier, treat whole string as invalid
+    //if ((end = strrchr(path,':')) == NULL) { return dir; }
+
     if ((end = strrchr(path, '/')) != NULL);
     else if ((end = strrchr(path, '\\')) != NULL);
     else if ((end = strrchr(path, ':')) != NULL);
     else return dir;    
 
-    if (*end == ':') end++;
+    // Only a "device:" or "device:file" type path
+    if ((*end == ':') && (end != path))
+    {
+        end++;
+        memcpy(dir,path,end-path);
+        dir[end-path] = '\0';
+        return dir;
+    }
 
+    if ((*end == '/') || (*end == '\\'))
+    {
+        if (*(end-1) == ':')
+        {
+            // Only a "device:/file" or "device:/" type path
+            end++;
+            memcpy(dir,path,end-path);
+            dir[end-path] = '\0';
+            return dir;
+        }
+    }
+
+    // Take care of paths that only have one '/' at the beginning
+    if (end == path)
+    {
+        if (*end != ':')
+        {
+            dir[0] = *end;
+            dir[1] = '\0';
+        }
+
+        return dir;
+    }
+
+    end++;
+
+    if (*end == '\0')
+    {
+
+        while((end != path))
+        {
+
+            end--;
+
+            if (*end == '/')
+            {
+                while (*end == '/') end--;
+                end++;
+                if (!all_slashes)
+                {
+                    if (end > path)
+                    {
+                        if (*(end-1) == ':') end++;
+                    }
+                    break;
+                }
+            }
+            else if (*end == '\\')
+            {
+                while (*end == '\\') end--;
+                end++;
+                if (!all_slashes)
+                {
+                    if (end > path)
+                    {
+                        if (*(end-1) == ':') end++;
+                    }
+                    break;
+                }
+            }
+            else if (*end == ':')
+            {
+                if (!all_slashes)
+                {
+                    end++;
+                    break;
+                }
+            }
+            else all_slashes = 0;
+
+        }
+
+    }
+    else
+    {
+        end--;
+        memcpy(dir,path,end-path);
+        dir[end-path] = '\0';
+        return dir;
+    }
+
+    if (all_slashes)
+    {
+        return dir;
+    }
+
+    if (end != path)
+    {
     memcpy(dir,path,end-path);
     dir[end-path] = '\0';
+    }
 
     return dir;
 }
