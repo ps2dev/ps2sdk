bb5294af9fed7260af22e06037153ee5d7316bba
BoGanon <michaelkey@gmail.com>
2016-10-05 17:00:59 -0400

    Add some additional headers to common/sys for compatibility.

---
+++ common/include/sys/types.h
@@ -0,0 +1,24 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2001-2004, ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+#
+# $Id$
+# Miscellaneous data types
+*/
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+#include <stddef.h>
+
+typedef int dev_t;
+typedef int ino_t;
+typedef int mode_t;
+typedef long off_t;
+typedef long ssize_t;
+
+#endif /*__TYPES_H__*/
