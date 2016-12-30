333c24e43923e78d419abdbc715517f46d4c694c
sp193 <pirorin187@gmail.com>
2016-12-30 18:08:26 +0800

    (LWIP) Added PACK_STRUCT_FLD_8 and PACK_STRUCT_FLD_S macros to inhibit packed attribute ignored warnings.

--- iop/tcpip/tcpip-base/include/arch/cc.h
+++ iop/tcpip/tcpip-base/include/arch/cc.h
@@ -25,6 +25,12 @@ typedef u32_t			mem_ptr_t;
 
 #define PACK_STRUCT_FIELD(x) x __attribute((packed))
 #define PACK_STRUCT_STRUCT __attribute((packed))
+/* Used for struct fields of u8_t,
+ * where some compilers warn that packing is not necessary */
+#define PACK_STRUCT_FLD_8(x) x
+/* Used for struct fields of that are packed structs themself,
+ * where some compilers warn that packing is not necessary */
+#define PACK_STRUCT_FLD_S(x) x
 #define PACK_STRUCT_BEGIN
 #define PACK_STRUCT_END
 
