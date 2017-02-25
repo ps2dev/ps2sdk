15a3c39d4b4b2b6574ce2616c8079973839b3750
uyjulian <uyjulian@gmail.com>
2017-02-24 23:22:17 -0600

    Comment cleanup ee/inputx

--- ee/inputx/include/input.h
+++ ee/inputx/include/input.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * Extended input library
+ */
+
 #ifndef __INPUT_H__
 #define __INPUT_H__
 
@@ -67,44 +72,44 @@ typedef struct {
 		padRead(pad->port,pad->slot,pad->buttons);
 	}
 
-	// Close the pad
+	/** Close the pad */
 	void pad_close(pad_t *pad);
 
-	// Wait until the pad is ready to accept commands
+	/** Wait until the pad is ready to accept commands */
 	void pad_wait(pad_t *pad);
 
 #ifdef DEBUG
-	// Get the pad's current state and print it
+	/** Get the pad's current state and print it */
 	void pad_print_state(pad_t *pad);
 
-	// Print the pad's supported modes
+	/** Print the pad's supported modes */
 	void pad_print_supported_modes(pad_t *pad);
 #endif
-	// Set the pad's mode and mode lock
+	/** Set the pad's mode and mode lock */
 	void pad_set_mode(pad_t *pad, int mode, int lock);
 
-	// Set the pad's pressure sensitivity
+	/** Set the pad's pressure sensitivity */
 	void pad_set_sensitivity(pad_t *pad, int enable);
 
-	// Init actuators
+	/** Init actuators */
 	void pad_init_actuators(pad_t *pad);
 
-	// Set actuators
+	/** Set actuators */
 	void pad_set_actuators(pad_t *pad, int small, unsigned char large);
 
-	// Get the pad's state
+	/** Get the pad's state */
 	int pad_get_state(pad_t *pad);
 
-	// Get number of modes supported by pad
+	/** Get number of modes supported by pad */
 	int pad_get_num_modes(pad_t *pad);
 
-	// Check if a type is supported by pad
+	/** Check if a type is supported by pad */
 	int pad_has_type(pad_t *pad, int type);
 
-	// Get the pad's type
+	/** Get the pad's type */
 	int pad_get_type(pad_t *pad);
 
-	// Get the pad's current mode
+	/** Get the pad's current mode */
 	int pad_get_mode(pad_t *pad);
 
 #ifdef __cplusplus
