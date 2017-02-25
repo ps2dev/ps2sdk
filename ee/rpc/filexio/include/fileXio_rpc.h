fc1e6d3655a5f2f4a41d47bdfb468634fa2128e5
uyjulian <uyjulian@gmail.com>
2017-02-25 14:33:27 -0600

    Comment cleanup ee/rpc/filexio

--- ee/rpc/filexio/include/fileXio_rpc.h
+++ ee/rpc/filexio/include/fileXio_rpc.h
@@ -6,9 +6,12 @@
 # Copyright 2001-2004, ps2dev - http://www.ps2dev.org
 # Licenced under Academic Free License version 2.0
 # Review ps2sdk README & LICENSE files for further details.
-#
-# fileXio RPC client header file
 */
+
+/**
+ * @file
+ * fileXio RPC client header file
+ */
 
 #ifndef _FILEXIO_RPC_H
 #define _FILEXIO_RPC_H
--- ee/rpc/filexio/src/fileXio_rpc.c
+++ ee/rpc/filexio/src/fileXio_rpc.c
@@ -1,14 +1,17 @@
 /*
-# _____	 ___ ____	 ___ ____
-#  ____|   |	____|   |		| |____|
-# |	 ___|   |____ ___|	____| |	\	PS2DEV Open Source Project.
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
 #-----------------------------------------------------------------------
 # Copyright 2001-2004, ps2dev - http://www.ps2dev.org
 # Licenced under Academic Free License version 2.0
 # Review ps2sdk README & LICENSE files for further details.
-#
-# fileXio RPC client
 */
+
+/**
+ * @file
+ * fileXio RPC client
+ */
 
 #include <tamtypes.h>
 #include <kernel.h>
