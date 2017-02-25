d9fe0eb1f0fd39c46b4800c8a1ed429bb04d29a9
uyjulian <uyjulian@gmail.com>
2017-02-25 16:09:10 -0600

    Comment cleanup ee/rpc/sior

--- ee/rpc/sior/include/sior_rpc.h
+++ ee/rpc/sior/include/sior_rpc.h
@@ -6,9 +6,12 @@
 # Copyright 2001-2004, ps2dev - http://www.ps2dev.org
 # Licenced under Academic Free License version 2.0
 # Review ps2sdk README & LICENSE files for further details.
-#
-# EE-side SIO remote service.
 */
+
+/**
+ * @file
+ * EE-side SIO remote service.
+ */
 
 #ifndef __SIOR_RPC_H__
 #define __SIOR_RPC_H__
