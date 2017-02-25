7fb38cd48845744ef1792492ceb478bf1603f5ca
uyjulian <uyjulian@gmail.com>
2017-02-25 14:04:06 -0600

    Comment cleanup ee/rpc/audsrv

--- ee/rpc/audsrv/include/audsrv.h
+++ ee/rpc/audsrv/include/audsrv.h
@@ -6,18 +6,15 @@
 # Copyright 2005, ps2dev - http://www.ps2dev.org
 # Licenced under GNU Library General Public License version 2
 # Review ps2sdk README & LICENSE files for further details.
-#
-# audsrv EE-side RPC code.
 */
+
+/**
+ * @file
+ * audsrv EE-side RPC code
+ */
 
 #ifndef _AUDSRV_H
 #define _AUDSRV_H
-
-/**
- * \file audsrv.h
- * \author gawd (Gil Megidish)
- * \date 04-24-05
- */
 
 #ifdef __cplusplus
 extern "C" {
@@ -47,9 +44,12 @@ extern "C" {
 /** structure used to set new format */
 typedef struct audsrv_fmt_t
 {
-	int freq;	                ///< output frequency in hz
-	int bits;                       ///< bits per sample (8, 16)
-	int channels;                   ///< output channels (1, 2)
+    /** output frequency in hz */
+	int freq;
+    /** bits per sample (8, 16) */
+	int bits;
+    /** output channels (1, 2) */
+	int channels;
 } audsrv_fmt_t;
 
 /** adpcm sample definition */
@@ -65,182 +65,182 @@ typedef struct audsrv_adpcm_t
 typedef int (*audsrv_callback_t)(void *arg);
 
 /** Initializes audsrv library
-    @returns error code
-*/
+ * @returns error code
+ */
 int audsrv_init();
 
 /** Shutdowns audsrv
-    @returns AUDSRV_ERR_NOERROR
-*/
+ * @returns AUDSRV_ERR_NOERROR
+ */
 int audsrv_quit();
 
 /** Configures audio stream
-    @param fmt output specification structure
-    @returns 0 on success, or one of the error codes otherwise
-
-    This sets up audsrv to accept stream in this format and convert
-    it to SPU2's native format if required. Note: it is possible to
-    change the format at any point. You might want to stop audio prior
-    to that, to prevent mismatched audio output.
-*/
+ * @param fmt output specification structure
+ * @returns 0 on success, or one of the error codes otherwise
+ *
+ * This sets up audsrv to accept stream in this format and convert
+ * it to SPU2's native format if required. Note: it is possible to
+ * change the format at any point. You might want to stop audio prior
+ * to that, to prevent mismatched audio output.
+ */
 int audsrv_set_format(struct audsrv_fmt_t *fmt);
 
 /** Blocks until there is enough space to enqueue chunk
-    @param bytes size of chunk requested to be enqueued (in bytes)
-    @returns error code
-
-    Blocks until there are enough space to store the upcoming chunk
-    in audsrv's internal ring buffer.
-*/
+ * @param bytes size of chunk requested to be enqueued (in bytes)
+ * @returns error code
+ *
+ * Blocks until there are enough space to store the upcoming chunk
+ * in audsrv's internal ring buffer.
+ */
 int audsrv_wait_audio(int bytes);
 
 /** Sets output volume
-    @param vol volume in percentage
-    @returns error code
-*/
+ * @param vol volume in percentage
+ * @returns error code
+ */
 int audsrv_set_volume(int volume);
 
 /** Uploads audio buffer to SPU
-    @param chunk   audio buffer
-    @param bytes   size of chunk in bytes
-    @returns positive number of bytes sent to processor or negative error status
-
-    Plays an audio buffer; It will not interrupt a playing
-    buffer, rather queue it up and play it as soon as possible without
-    interfering with fluent streaming. The buffer and buflen are given
-    in host format (i.e, 11025hz 8bit stereo.)
-*/
+ * @param chunk   audio buffer
+ * @param bytes   size of chunk in bytes
+ * @returns positive number of bytes sent to processor or negative error status
+ *
+ * Plays an audio buffer; It will not interrupt a playing
+ * buffer, rather queue it up and play it as soon as possible without
+ * interfering with fluent streaming. The buffer and buflen are given
+ * in host format (i.e, 11025hz 8bit stereo.)
+ */
 int audsrv_play_audio(const char *chunk, int bytes);
 
 /** Stops audio from playing.
-    @returns status code
-*/
+ * @returns status code
+ */
 int audsrv_stop_audio();
 
 /** Returns the last error audsrv raised
-    @returns error code
-*/
+ * @returns error code
+ */
 int audsrv_get_error();
 
 /** Translates audsrv_get_error() response to readable string
-    @returns string representation of error code
-*/
+ * @returns string representation of error code
+ */
 const char *audsrv_get_error_string();
 
 /** Starts playing the request track
-    @param track segment to play
-    @returns status code
-*/
+ * @param track segment to play
+ * @returns status code
+ */
 int audsrv_play_cd(int track);
 
 /** Stops CD from playing.
-    @returns status code
-*/
+ * @returns status code
+ */
 int audsrv_stop_cd();
 
 /** Returns the current playing sector
-    @returns sector number
-
-    CDDA type discs have sector size of 2352 bytes. There are 75
-    such sectors per second.
-*/
+ * @returns sector number
+ *
+ * CDDA type discs have sector size of 2352 bytes. There are 75
+ * such sectors per second.
+ */
 int audsrv_get_cdpos();
 
 /** Returns the current playing sector, relative to track
-    @returns sector number
-
-    There are 75 sectors a second. To translate this position to mm:ss:ff
-    use the following:
-    mm = sector / (75*60)
-    ss = (sector / 75) % 60
-    ff = sector % 75
-
-    where ff is the frame number, 1/75th of a second.
-*/
+ * @returns sector number
+ *
+ * There are 75 sectors a second. To translate this position to mm:ss:ff
+ * use the following:
+ * mm = sector / (75*60)
+ * ss = (sector / 75) % 60
+ * ff = sector % 75
+ *
+ * where ff is the frame number, 1/75th of a second.
+ */
 int audsrv_get_trackpos();
 
 /** Returns the number of tracks available on the CD in tray
-    @returns positive track count, or negative error status code
-*/
+ * @returns positive track count, or negative error status code
+ */
 int audsrv_get_numtracks();
 
 /** Returns the first sector for the given track
-    @param track   track index, must be between 1 and the trackcount
-    @returns sector number, or negative status code
-*/
+ * @param track   track index, must be between 1 and the trackcount
+ * @returns sector number, or negative status code
+ */
 int audsrv_get_track_offset(int track);
 
 /** Pauses CDDA playing
-    @returns error status code
-
-    If CDDA is paused, no operation is taken
-*/
+ * @returns error status code
+ *
+ * If CDDA is paused, no operation is taken
+ */
 int audsrv_pause_cd();
 
 /** Resumes CDDA playing
-    @returns error status code
-
-    If CDDA was not paused, no operation is taken
-*/
+ * @returns error status code
+ *
+ * If CDDA was not paused, no operation is taken
+ */
 int audsrv_resume_cd();
 
 /** Starts playing at a specific sector
-    @param start first sector to play
-    @param end   last sector to play
-    @returns status code
-*/
+ * @param start first sector to play
+ * @param end   last sector to play
+ * @returns status code
+ */
 int audsrv_play_sectors(int start, int end);
 
 /** Returns the status of the CD tray (open, closed, seeking etc.)
-    @returns value as defined in libcdvd, negative on error
-*/
+ * @returns value as defined in libcdvd, negative on error
+ */
 int audsrv_get_cd_status();
 
 /** Returns the type of disc currently in tray
-    @returns value as defined in libcdvd, negative on error
-*/
+ * @returns value as defined in libcdvd, negative on error
+ */
 int audsrv_get_cd_type();
 
 /** Installs a callback function to be called when ringbuffer has enough
-    space to transmit the request number of bytes.
-    @param bytes request a callback when this amount of bytes is available
-    @param cb your callback
-    @param arg extra parameter to pass to callback function later
-    @returns AUDSRV_ERR_NOERROR, AUDSRV_ERR_ARGS if amount is greater than sizeof(ringbuf)
-*/
+ * space to transmit the request number of bytes.
+ * @param bytes request a callback when this amount of bytes is available
+ * @param cb your callback
+ * @param arg extra parameter to pass to callback function later
+ * @returns AUDSRV_ERR_NOERROR, AUDSRV_ERR_ARGS if amount is greater than sizeof(ringbuf)
+ */
 int audsrv_on_fillbuf(int amount, audsrv_callback_t cb, void *arg);
 
 /** Initializes adpcm unit of audsrv
-    @returns zero on success, negative value on error
-
-    Frees up all memory taken by samples, and stops all voices from
-    being played. This can be called multiple times
-*/
+ * @returns zero on success, negative value on error
+ *
+ * Frees up all memory taken by samples, and stops all voices from
+ * being played. This can be called multiple times
+ */
 int audsrv_adpcm_init();
 
 /** Uploads a sample to SPU2 memory
-    @param adpcm    adpcm descriptor structure
-    @param buffer   pointer to adpcm sample
-    @param size     size of sample (including the header)
-    @returns zero on success, negative error code otherwise
-*/
+ * @param adpcm    adpcm descriptor structure
+ * @param buffer   pointer to adpcm sample
+ * @param size     size of sample (including the header)
+ * @returns zero on success, negative error code otherwise
+ */
 int audsrv_load_adpcm(audsrv_adpcm_t *adpcm, void *buffer, int size);
 
 /** Plays an adpcm sample already uploaded with audsrv_load_adpcm()
-    @param id    sample identifier, as specified in load()
-    @returns zero on success, negative value on error
-
-    The sample will be played in an unoccupied channel. If all 24 channels
-    are used, then -AUDSRV_ERR_NO_MORE_CHANNELS is returned. Trying to play
-    a sample which is unavailable will result in -AUDSRV_ERR_ARGS
-*/
+ * @param id    sample identifier, as specified in load()
+ * @returns zero on success, negative value on error
+ *
+ * The sample will be played in an unoccupied channel. If all 24 channels
+ * are used, then -AUDSRV_ERR_NO_MORE_CHANNELS is returned. Trying to play
+ * a sample which is unavailable will result in -AUDSRV_ERR_ARGS
+ */
 int audsrv_play_adpcm(audsrv_adpcm_t *adpcm);
 
 /** Installs a callback function upon completion of a cdda track
-    @param cb your callback
-    @param arg extra parameter to pass to callback function later
-    @returns status code
-*/
+ * @param cb your callback
+ * @param arg extra parameter to pass to callback function later
+ * @returns status code
+ */
 int audsrv_on_cdda_stop(audsrv_callback_t cb, void *arg);
 
 
--- ee/rpc/audsrv/src/audsrv_rpc.c
+++ ee/rpc/audsrv/src/audsrv_rpc.c
@@ -6,14 +6,11 @@
 # Copyright 2005, ps2dev - http://www.ps2dev.org
 # Licenced under GNU Library General Public License version 2
 # Review ps2sdk README & LICENSE files for further details.
-#
-# audsrv EE-side RPC code.
 */
 
 /**
- * \file audsrv_rpc.c
- * \author gawd (Gil Megidish)
- * \date 04-24-05
+ * @file
+ * audsrv EE-side RPC code.
  */
 
 #include <stdio.h>
@@ -49,8 +46,8 @@ static audsrv_callback_t on_fillbuf = NULL;
 static void *on_fillbuf_arg = NULL;
 
 /** Internal function to set last error
-    @param err
-*/
+ * @param err
+ */
 static void set_error(int err)
 {
 	audsrv_error = err;
@@ -61,18 +58,15 @@ static void _audsrv_intr()
 	iSignalSema(completion_sema);
 }
 
-/** Returns the last error audsrv raised
-    @returns error code
-*/
 int audsrv_get_error()
 {
 	return audsrv_error;
 }
 
 /** Internal function to simplify RPC calling
-    @param func    procedure to invoke
-    @param arg     optional argument
-    @returns value returned by RPC server
+ * @param func    procedure to invoke
+ * @param arg     optional argument
+ * @returns value returned by RPC server
 */
 static int call_rpc_1(int func, int arg)
 {
@@ -86,10 +80,10 @@ static int call_rpc_1(int func, int arg)
 }
 
 /** Internal function to simplify RPC calling
-    @param func    procedure to invoke
-    @param arg1    optional argument
-    @param arg2    optional argument
-    @returns value returned by RPC server
+ * @param func    procedure to invoke
+ * @param arg1    optional argument
+ * @param arg2    optional argument
+ * @returns value returned by RPC server
 */
 static int call_rpc_2(int func, int arg1, int arg2)
 {
@@ -103,9 +97,6 @@ static int call_rpc_2(int func, int arg1, int arg2)
 	return sbuff[0];
 }
 
-/** Shutdowns audsrv
-    @returns AUDSRV_ERR_NOERROR
-*/
 int audsrv_quit()
 {
 	WaitSema(completion_sema);
@@ -122,15 +113,6 @@ int audsrv_quit()
 	return 0;
 }
 
-/** Configures audio stream
-    @param fmt output specification structure
-    @returns 0 on success, or one of the error codes otherwise
-
-    This sets up audsrv to accept stream in this format and convert
-    it to SPU2's native format if required. Note: it is possible to
-    change the format at any point. You might want to stop audio prior
-    to that, to prevent mismatched audio output.
-*/
 int audsrv_set_format(struct audsrv_fmt_t *fmt)
 {
 	WaitSema(completion_sema);
@@ -143,22 +125,11 @@ int audsrv_set_format(struct audsrv_fmt_t *fmt)
 	return sbuff[0];
 }
 
-/** Blocks until there is enough space to enqueue chunk
-    @param bytes size of chunk requested to be enqueued (in bytes)
-    @returns error code
-
-    Blocks until there are enough space to store the upcoming chunk
-    in audsrv's internal ring buffer.
-*/
 int audsrv_wait_audio(int bytes)
 {
 	return call_rpc_1(AUDSRV_WAIT_AUDIO, bytes);
 }
 
-/** Sets output volume
-    @param vol volume in percentage
-    @returns error code
-*/
 int audsrv_set_volume(int volume)
 {
 	unsigned short vol_values[26] =
@@ -183,28 +154,16 @@ int audsrv_set_volume(int volume)
 	return call_rpc_1(AUDSRV_SET_VOLUME, vol_values[volume/4]);
 }
 
-/** Starts playing the request track
-    @param track segment to play
-    @returns status code
-*/
 int audsrv_play_cd(int track)
 {
 	return call_rpc_1(AUDSRV_PLAY_CD, track);
 }
 
-/** Starts playing at a specific sector
-    @param start first sector to play
-    @param end   last sector to play
-    @returns status code
-*/
 int audsrv_play_sectors(int start, int end)
 {
 	return call_rpc_2(AUDSRV_PLAY_SECTORS, start, end);
 }
 
-/** Stops CD from playing.
-    @returns status code
-*/
 int audsrv_stop_cd()
 {
 	int ret;
@@ -212,96 +171,46 @@ int audsrv_stop_cd()
 	return ret;
 }
 
-/** Returns the current playing sector
-    @returns sector number
-
-    CDDA type discs have sector size of 2352 bytes. There are 75
-    such sectors per second.
-*/
 int audsrv_get_cdpos()
 {
 	return call_rpc_1(AUDSRV_GET_CDPOS, 0);
 }
 
-/** Returns the current playing sector, relative to track
-    @returns sector number
-
-    There are 75 sectors a second. To translate this position to mm:ss:ff
-    use the following:
-    mm = sector / (75*60)
-    ss = (sector / 75) % 60
-    ff = sector % 75
-
-    where ff is the frame number, 1/75th of a second.
-*/
 int audsrv_get_trackpos()
 {
 	return call_rpc_1(AUDSRV_GET_TRACKPOS, 0);
 }
 
-/** Returns the number of tracks available on the CD in tray
-    @returns positive track count, or negative error status code
-*/
 int audsrv_get_numtracks()
 {
 	return call_rpc_1(AUDSRV_GET_NUMTRACKS, 0);
 }
 
-/** Returns the first sector for the given track
-    @param track   track index, must be between 1 and the trackcount
-    @returns sector number, or negative status code
-*/
 int audsrv_get_track_offset(int track)
 {
 	return call_rpc_1(AUDSRV_GET_TRACKOFFSET, track);
 }
 
-/** Pauses CDDA playing
-    @returns error status code
-
-    If CDDA is paused, no operation is taken
-*/
 int audsrv_pause_cd()
 {
 	return call_rpc_1(AUDSRV_PAUSE_CD, 0);
 }
 
-/** Resumes CDDA playing
-    @returns error status code
-
-    If CDDA was not paused, no operation is taken
-*/
 int audsrv_resume_cd()
 {
 	return call_rpc_1(AUDSRV_RESUME_CD, 0);
 }
 
-/** Returns the type of disc currently in tray
-    @returns value as defined in libcdvd, negative on error
-*/
 int audsrv_get_cd_type()
 {
 	return call_rpc_1(AUDSRV_GET_CD_TYPE, 0);
 }
 
-/** Returns the status of the CD tray (open, closed, seeking etc.)
-    @returns value as defined in libcdvd, negative on error
-*/
 int audsrv_get_cd_status()
 {
 	return call_rpc_1(AUDSRV_GET_CD_STATUS, 0);
 }
 
-/** Uploads audio buffer to SPU
-    @param chunk   audio buffer
-    @param bytes   size of chunk in bytes
-    @returns positive number of bytes sent to processor or negative error status
-
-    Plays an audio buffer; It will not interrupt a playing
-    buffer, rather queue it up and play it as soon as possible without
-    interfering with fluent streaming. The buffer and buflen are given
-    in host format (i.e, 11025hz 8bit stereo.)
-*/
 int audsrv_play_audio(const char *chunk, int bytes)
 {
 	int copy, maxcopy;
@@ -335,9 +244,6 @@ int audsrv_play_audio(const char *chunk, int bytes)
 	return sent;
 }
 
-/** Stops audio from playing.
-    @returns status code
-*/
 int audsrv_stop_audio()
 {
 	int ret;
@@ -370,9 +276,6 @@ static void rpc_server_thread(void *arg)
 	SifRpcLoop(&cb_queue);
 }
 
-/** Initializes audsrv library
-    @returns error code
-*/
 int audsrv_init()
 {
 	ee_sema_t compSema;
@@ -439,21 +342,11 @@ int audsrv_init()
 	return AUDSRV_ERR_NOERROR;
 }
 
-/** Initializes adpcm unit of audsrv
-
-    @returns zero on success, negative value on error
-*/
 int audsrv_adpcm_init()
 {
 	return call_rpc_1(AUDSRV_INIT_ADPCM, 0);
 }
 
-/** Uploads a sample to SPU2 memory
-    @param adpcm    adpcm descriptor structure
-    @param buffer   pointer to adpcm sample
-    @param size     size of sample (including the header)
-    @returns zero on success, negative error code otherwise
-*/
 int audsrv_load_adpcm(audsrv_adpcm_t *adpcm, void *buffer, int size)
 {
 	void* iop_addr;
@@ -500,23 +393,12 @@ int audsrv_load_adpcm(audsrv_adpcm_t *adpcm, void *buffer, int size)
 	}
 }
 
-/** Plays an adpcm sample already uploaded with audsrv_load_adpcm()
-    @param adpcm   exact same adpcm descriptor used in load()
-    @returns zero on success, negative value on error
-
-    The sample will be played in an unoccupied channel. If all 24 channels
-    are used, then -AUDSRV_ERR_NO_MORE_CHANNELS is returned. Trying to play
-    a sample which is unavailable will result in -AUDSRV_ERR_ARGS
-*/
 int audsrv_play_adpcm(audsrv_adpcm_t *adpcm)
 {
 	/* on iop side, the sample id is like the pointer on ee side */
 	return call_rpc_1(AUDSRV_PLAY_ADPCM, (u32)adpcm);
 }
 
-/** Translates audsrv_get_error() response to readable string
-    @returns string representation of error code
-*/
 const char *audsrv_get_error_string()
 {
 	switch(audsrv_get_error())
@@ -543,11 +425,6 @@ const char *audsrv_get_error_string()
 	return "Unknown error";
 }
 
-/** Installs a callback function upon completion of a cdda track
-    @param cb your callback
-    @param arg extra parameter to pass to callback function later
-    @returns status code
-*/
 int audsrv_on_cdda_stop(audsrv_callback_t cb, void *arg)
 {
 	on_cdda_stop = cb;
@@ -555,13 +432,6 @@ int audsrv_on_cdda_stop(audsrv_callback_t cb, void *arg)
 	return AUDSRV_ERR_NOERROR;
 }
 
-/** Installs a callback function to be called when ringbuffer has enough
-    space to transmit the request number of bytes.
-    @param bytes request a callback when this amount of bytes is available
-    @param cb your callback
-    @param arg extra parameter to pass to callback function later
-    @returns AUDSRV_ERR_NOERROR, AUDSRV_ERR_ARGS if amount is greater than sizeof(ringbuf)
-*/
 int audsrv_on_fillbuf(int amount, audsrv_callback_t cb, void *arg)
 {
 	int err;
