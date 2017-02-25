a0de5c15ffdd50af17d3aa071db2259bc10fb21c
uyjulian <uyjulian@gmail.com>
2017-02-25 14:40:58 -0600

    Comment cleanup ee/rpc/keyboard

--- ee/rpc/keyboard/include/libkbd.h
+++ ee/rpc/keyboard/include/libkbd.h
@@ -6,9 +6,12 @@
 # Copyright 2001-2004, ps2dev - http://www.ps2dev.org
 # Licenced under Academic Free License version 2.0
 # Review ps2sdk README & LICENSE files for further details.
-#
-# USB Keyboard Driver for PS2
 */
+
+/**
+ * @file
+ * USB Keyboard Driver for PS2
+ */
 
 #ifndef __LIBKBD_H__
 #define __LIBKBD_H__
@@ -22,34 +25,34 @@ typedef kbd_keymap PS2KbdKeyMap;
 extern "C" {
 #endif
 
+/** Initialise the keyboard library */
 int PS2KbdInit(void);
-/* Initialise the keyboard library */
+/** Reads 1 character from the keyboard */
 int PS2KbdRead(char *key);
-/* Reads 1 character from the keyboard */
+/** Reads 1 raw character from the keyboard */
 int PS2KbdReadRaw(PS2KbdRawKey *key);
-/* Reads 1 raw character from the keyboard */
+/** Sets the read mode to normal or raw */
 int PS2KbdSetReadmode(u32 readMode);
-/* Sets the read mode to normal or raw */
+/** Sets the blocking mode on or off */
 int PS2KbdSetBlockingMode(u32 readMode);
-/* Sets the blocking mode on or off */
+/** Sets the repeat rate in millseconds */
 int PS2KbdSetRepeatRate(u32 repeat);
-/* Sets the repeat rate in millseconds */
+/** Sets all connected keyboards leds */
 int PS2KbdSetLeds(u8 leds);
-/* Sets all connected keyboards leds */
+/** Sets the current keymap */
 int PS2KbdSetKeymap(PS2KbdKeyMap *keymaps);
-/* Sets the current keymap */
+/** Sets the control key mappings */
 int PS2KbdSetCtrlmap(u8 *ctrlmap);
-/* Sets the control key mappings */
+/** Sets the alt key mappings */
 int PS2KbdSetAltmap(u8 *altmap);
-/* Sets the alt key mappings */
+/** Sets the special key mappings */
 int PS2KbdSetSpecialmap(u8 *special);
-/* Sets the special key mappings */
+/** Flushes the keyboard buffer */
 int PS2KbdFlushBuffer(void);
-/* Flushes the keyboard buffer */
+/** Resets the keymap to the default US mapping */
 int PS2KbdResetKeymap(void);
-/* Resets the keymap to the default US mapping */
+/** Close down the keyboard library */
 int PS2KbdClose(void);
-/* Close down the keyboard library */
 
 #ifdef __cplusplus
 }
