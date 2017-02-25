a441a3ea999930e288aa6a3ee7714c01041e0882
uyjulian <uyjulian@gmail.com>
2017-02-24 22:21:25 -0600

    Comment cleanup ee/dma

--- ee/dma/include/dma.h
+++ ee/dma/include/dma.h
@@ -6,8 +6,12 @@
 # (c) 2005 Dan Peori <peori@oopo.net>
 # Licenced under Academic Free License version 2.0
 # Review ps2sdk README & LICENSE files for further details.
-#
 */
+
+/**
+ * @file
+ * DMA channel utilities
+ */
 
 #ifndef __DMA_H__
 #define __DMA_H__
@@ -34,38 +38,38 @@ extern "C" {
 
 	int dma_reset(void);
 
+	/** Initializes the specified dma channel. */
 	int dma_channel_initialize(int channel, void *handler, int flags);
-	// Initializes the specified dma channel.
 
+	/** Enables dma fast waits for that channel */
 	void dma_channel_fast_waits(int channel);
-	// Enables dma fast waits for that channel
 
+	/** Waits until channel is usable based on coprocessor status */
 	void dma_wait_fast(void);
-	// Waits until channel is usable based on coprocessor status
 
+	/** Wait until the specified dma channel is ready. */
 	int dma_channel_wait(int channel, int timeout);
-	// Wait until the specified dma channel is ready.
 
+	/** Send a dmachain to the specified dma channel. */
 	int dma_channel_send_chain(int channel, void *data, int qwc, int flags, int spr);
-	// Send a dmachain to the specified dma channel.
 
+	/** Send a ucab dmachain to the specified dma channel. */
 	int dma_channel_send_chain_ucab(int channel, void *data, int qwc, int flags);
-	// Send a ucab dmachain to the specified dma channel.
 
+	/** Send data to the specified dma channel. */
 	int dma_channel_send_normal(int channel, void *data, int qwc, int flags, int spr);
-	// Send data to the specified dma channel.
 
+	/** Send ucab data to the specified dma channel. */
 	int dma_channel_send_normal_ucab(int channel, void *data, int qwc, int flags);
-	// Send ucab data to the specified dma channel.
 
+	/** Receive data from the specified dma channel. */
 	int dma_channel_receive_normal(int channel, void *data, int data_size, int flags, int spr);
-	// Receive data from the specified dma channel.
 
+	/** Receive data from the specified dma channel. */
 	int dma_channel_receive_chain(int channel, void *data, int data_size, int flags, int spr);
-	// Receive data from the specified dma channel.
 
+	/** Shut down the specified dma channel. */
 	int dma_channel_shutdown(int channel, int flags);
-	// Shut down the specified dma channel.
 
 #ifdef __cplusplus
 }
--- ee/dma/include/dma_tags.h
+++ ee/dma/include/dma_tags.h
@@ -1,21 +1,47 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# (c) 2005 Dan Peori <peori@oopo.net>
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+*/
+
+/**
+ * @file
+ * DMA tags
+ */
+
 #ifndef __DMA_TAGS_H__
 #define __DMA_TAGS_H__
 
 #include <tamtypes.h>
 
-// T   = location of qwords
-// D   = next dmatag to be read
-// QWC = quadword count for dmatag
+/** 
+ * T   = location of qwords
+ * D   = next dmatag to be read
+ * QWC = quadword count for dmatag
+*/
 
-#define DMA_TAG_CNTS	0x00	//T=QWC D=QWC+1 MADR => STADR
-#define DMA_TAG_REFE	0x00	//T=ADDR then END
-#define DMA_TAG_CNT	0x01	//T=QWC D=QWC+1
-#define DMA_TAG_NEXT	0x02	//T=QWC D=ADDR
-#define DMA_TAG_REF	0x03	//D=D+1 T=ADDR
-#define DMA_TAG_REFS	0x04	//.. + stall ctrl
-#define DMA_TAG_CALL	0x05	//T=QWC D=ADDR QWC+1 => ASR0
-#define DMA_TAG_RET	0x06	//T=QWC (ASR0 => D) if !ASR0 then END
-#define DMA_TAG_END	0x07	//T=QWC then END
+/** T=QWC D=QWC+1 MADR => STADR */
+#define DMA_TAG_CNTS	0x00
+/** T=ADDR then END */
+#define DMA_TAG_REFE	0x00
+/** T=QWC D=QWC+1 */
+#define DMA_TAG_CNT	0x01
+/** T=QWC D=ADDR */
+#define DMA_TAG_NEXT	0x02
+/** D=D+1 T=ADDR */
+#define DMA_TAG_REF	0x03
+/** .. + stall ctrl */
+#define DMA_TAG_REFS	0x04
+/** T=QWC D=ADDR QWC+1 => ASR0 */
+#define DMA_TAG_CALL	0x05
+/** T=QWC (ASR0 => D) if !ASR0 then END */
+#define DMA_TAG_RET	0x06
+/** T=QWC then END */
+#define DMA_TAG_END	0x07
 
 #define DMATAG(QWC,PCE,ID,IRQ,ADDR,SPR) \
 	(u64)((QWC)  & 0x0000FFFF) <<  0 | (u64)((PCE) & 0x00000003) << 26 | \
@@ -27,55 +53,55 @@
 	Q->sw[2] = (u32)(W2), \
 	Q->sw[3] = (u32)(W3)
 
-// Insert before qword block, and add dmatag after qword block to continue
+/** Insert before qword block, and add dmatag after qword block to continue */
 #define DMATAG_CNT(Q,QWC,SPR,W2,W3) \
 	Q->dw[0] = DMATAG(QWC,0,DMA_TAG_CNT,0,0,SPR), \
 	Q->sw[2] = (u32)(W2), \
 	Q->sw[3] = (u32)(W3)
 
-// Insert before qword block, and add dmatag after qword block to continue
+/** Insert before qword block, and add dmatag after qword block to continue */
 #define DMATAG_CNTS(Q,QWC,SPR,W2,W3) \
 	Q->dw[0] = DMATAG(QWC,0,DMA_TAG_CNTS,0,0,SPR), \
 	Q->sw[2] = (u32)(W2), \
 	Q->sw[3] = (u32)(W3)
 
-// Insert before qword block, and add dmatag at addr to continue
+/** Insert before qword block, and add dmatag at addr to continue */
 #define DMATAG_NEXT(Q,QWC,ADDR,SPR,W2,W3), \
 	Q->dw[0] = DMATAG(QWC,0,DMA_TAG_NEXT,0,ADDR,SPR), \
 	Q->sw[2] = (u32)(W2), \
 	Q->sw[3] = (u32)(W3)
 
-// Insert before final qword block, or by itself to end transfer
+/** Insert before final qword block, or by itself to end transfer */
 #define DMATAG_END(Q,QWC,SPR,W2,W3) \
 	Q->dw[0] = DMATAG(QWC,0,DMA_TAG_END,0,0,SPR), \
 	Q->sw[2] = (u32)(W2), \
 	Q->sw[3] = (u32)(W3)
 
-// Insert before qword block, and add dmatag at qword after qwc to be saved (up to 2)
+/** Insert before qword block, and add dmatag at qword after qwc to be saved (up to 2) */
 #define DMATAG_CALL(Q,QWC,ADDR,SPR,W2,W3) \
 	Q->dw[0] = DMATAG(QWC,0,DMA_TAG_CALL,0,ADDR,SPR), \
 	Q->sw[2] = (u32)(W2), \
 	Q->sw[3] = (u32)(W3)
 
-// Insert before qword block, and continues with saved dmatag
+/** Insert before qword block, and continues with saved dmatag */
 #define DMATAG_RET(Q,QWC,SPR,W2,W3) \
 	Q->dw[0] = DMATAG(QWC,0,DMA_TAG_RET,0,0,SPR), \
 	Q->sw[2] = (u32)(W2), \
 	Q->sw[3] = (u32)(W3)
 
-// Insert anywhere, qwc at addr is sent, reads next qword to continue
+/** Insert anywhere, qwc at addr is sent, reads next qword to continue */
 #define DMATAG_REF(Q,QWC,ADDR,SPR,W2,W3) \
 	Q->dw[0] = DMATAG(QWC,0,DMA_TAG_REF,0,ADDR,SPR), \
 	Q->sw[2] = (u32)(W2), \
 	Q->sw[3] = (u32)(W3)
 
-// Insert anywhere, qwc at addr is sent, reads next qword to continue
+/** Insert anywhere, qwc at addr is sent, reads next qword to continue */
 #define DMATAG_REFS(Q,QWC,ADDR,SPR,W2,W3) \
 	Q->dw[0] = DMATAG(QWC,0,DMA_TAG_REFS,0,ADDR,SPR), \
 	Q->sw[2] = (u32)(W2), \
 	Q->sw[3] = (u32)(W3)
 
-// Insert anywhere, qwc at addr is sent, then ends transfer
+/** Insert anywhere, qwc at addr is sent, then ends transfer */
 #define DMATAG_REFE(Q,QWC,ADDR,SPR,W2,W3) \
 	Q->dw[0] = DMATAG(qwc, 0, DMA_TAG_REFE, 0, addr, spr), \
 	Q->sw[2] = (u32)(W2), \
