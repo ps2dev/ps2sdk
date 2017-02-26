bc16664497e59c0b4d6658e02dbeabb26b8720e4
uyjulian <uyjulian@gmail.com>
2017-02-26 11:07:06 -0600

    Comment cleanup iop/usb/camera

--- iop/usb/camera/include/ps2cam.h
+++ iop/usb/camera/include/ps2cam.h
@@ -2,46 +2,63 @@
   PSX2 OpenSource Project
   (C)2004 lion[PS2Dev]
   (C)2004 PS2Dev.org
-  ------------------------------------------------------------------------
-  ps2cam.h
-					PS2Camera driver irx
-
 */
+
+/**
+ * @file
+ * PS2Camera driver irx
+ */
+
 #ifndef __PS2CAM_H__
 #define __PS2CAM_H__
 
 
 
 
-
 //rpc call id
-#define PS2CAM_RPC_GETIRXVERSION	40		// get irx version
-#define PS2CAM_RPC_INITIALIZE		41		// PS2CamInit
-#define PS2CAM_RPC_GETDEVCOUNT		42		// PS2CamGetDeviceCount
-#define PS2CAM_RPC_OPENDEVICE		43		//
-#define PS2CAM_RPC_CLOSEDEVICE		44		//
-#define PS2CAM_RPC_GETDEVSTATUS		45		// PS2CamGetStatus
-#define PS2CAM_RPC_GETDEVINFO		46		// PS2CamGetInfo
-#define PS2CAM_RPC_SETDEVBANDWIDTH	47		// PS2CamSetDeviceBandwidth
-#define PS2CAM_RPC_READPACKET		48		// PS2CamReadPacketCOMMAND
-#define PS2CAM_RPC_SETLEDMODE		49		// PS2CamSetLEDMode
-#define PS2CAM_RPC_SETDEVCONFIG		50		//
+/** get irx version */
+#define PS2CAM_RPC_GETIRXVERSION	40
+/** PS2CamInit */
+#define PS2CAM_RPC_INITIALIZE		41
+/** PS2CamGetDeviceCount */
+#define PS2CAM_RPC_GETDEVCOUNT		42
+#define PS2CAM_RPC_OPENDEVICE		43
+#define PS2CAM_RPC_CLOSEDEVICE		44
+/** PS2CamGetStatus */
+#define PS2CAM_RPC_GETDEVSTATUS		45
+/** PS2CamGetInfo */
+#define PS2CAM_RPC_GETDEVINFO		46
+/** PS2CamSetDeviceBandwidth */
+#define PS2CAM_RPC_SETDEVBANDWIDTH	47
+/** PS2CamReadPacketCOMMAND */
+#define PS2CAM_RPC_READPACKET		48
+/** PS2CamSetLEDMode */
+#define PS2CAM_RPC_SETLEDMODE		49
+
+#define PS2CAM_RPC_SETDEVCONFIG		50
 
 
 
+/* camera vendor(s) */
+/** Sony */
+#define PS2CAM_VEND_SONY			0x054C 
+/** D-Link */
+#define PS2CAM_VEND_DLINK			0x05A9
+/** Logitech */
+#define PS2CAM_VEND_LOGITECH		0x0000
 
-/*camera vender(s)*/
-#define PS2CAM_VEND_SONY			0x054C	// Sony
-#define PS2CAM_VEND_DLINK			0x05A9	// D-Link
-#define PS2CAM_VEND_LOGITECH		0x0000	// Logitech
+/* product id(s) */
+/** SLEH-00030 */
+#define PS2CAM_PROD_EYETOY			0x0154
+/** SLEH-00031 */
+#define PS2CAM_PROD_EYETOY2			0x0155
+/** SLEH-000??	rinco thinks its japan */
+#define PS2CAM_PROD_EYETOY3			0x0156
+/** SLEH-000??   ,,  ,,  ,, */
+#define PS2CAM_PROD_EYETOY4			0x0157
 
-/*product id(s)*/
-#define PS2CAM_PROD_EYETOY			0x0154	// SLEH-00030
-#define PS2CAM_PROD_EYETOY2			0x0155	// SLEH-00031
-#define PS2CAM_PROD_EYETOY3			0x0156	// SLEH-000??	rinco thinks its japan
-#define PS2CAM_PROD_EYETOY4			0x0157	// SLEH-000??   ,,  ,,  ,,
-
-#define PS2CAM_PROD_VISUALSTREAM	0x8519	// D-Link VisualStream DSB-C310
+/** D-Link VisualStream DSB-C310 */
+#define PS2CAM_PROD_VISUALSTREAM	0x8519
 
 
 
@@ -81,21 +98,25 @@
 #define EYETOY_CREG_ADC_CTRL		0x68
 #define EYETOY_CREG_UC_CTRL			0x6D
 
-/*these reg control extra pins on chip(eg:red led)*/
+/* these reg control extra pins on chip(eg:red led) */
 #define EYETOY_GPIO_DATA_OUT0		0x71
 #define EYETOY_GPIO_IO_CTRL0		0x72
 
 //alternate settins(select butter size)
-#define EYETOY_ALTERNATE_SIZE_0		0x00	// xfer disable
+/** xfer disable */
+#define EYETOY_ALTERNATE_SIZE_0		0x00
 #define EYETOY_ALTERNATE_SIZE_384	0x01
 #define EYETOY_ALTERNATE_SIZE_512	0x02
 #define EYETOY_ALTERNATE_SIZE_768	0x03
 #define EYETOY_ALTERNATE_SIZE_896	0x04
 
 // device status
-#define CAM_STATUS_NOTCONNECTED		0	// device not connected
-#define CAM_STATUS_CONNECTED		1	// connected but initializing
-#define CAM_STATUS_CONNECTEDREADY	2	// connect & ready for commands
+/** device not connected */
+#define CAM_STATUS_NOTCONNECTED		0
+/** connected but initializing */
+#define CAM_STATUS_CONNECTED		1
+/** connect & ready for commands */
+#define CAM_STATUS_CONNECTEDREADY	2
 
 
 // used with PS2CAM_DEVICE_CONFIG->mask
@@ -107,29 +128,40 @@
 
 
 // error codes
-#define CAM_ERROR_NONE				 (00)	// ok
-#define CAM_ERROR_NOTINIT			-(20)	//
-#define CAM_ERROR_INVALIDDEVICE		-(21)	//
-#define CAM_ERROR_COMMUNKNOWN		-(22)	// unknown command
-#define CAM_ERROR_DEVNOTREADY		-(23)	// device not ready
-#define CAM_ERROR_NODEVICE			-(24)	// no compatible device connected
-#define CAM_ERROR_BADRANGE			-(25)	// a value was out of range
-#define CAM_ERROR_UNKNOWN			-(26)	// unknown error
-#define CAM_ERROR_MAXHANDLE			-(27)	// out of free device handle(s)
-#define CAM_ERROR_BADHANDLE			-(28)	// invalid device handle
-#define CAM_ERROR_DISCONNECTED		-(29)	// device was removed
+/** ok */
+#define CAM_ERROR_NONE				 (00)
+#define CAM_ERROR_NOTINIT			-(20)
+#define CAM_ERROR_INVALIDDEVICE		-(21)
+ /** unknown command */
+#define CAM_ERROR_COMMUNKNOWN		-(22)
+/** device not ready */
+#define CAM_ERROR_DEVNOTREADY		-(23)
+/** no compatible device connected */
+#define CAM_ERROR_NODEVICE			-(24)
+/** a value was out of range */
+#define CAM_ERROR_BADRANGE			-(25)
+/** unknown error */
+#define CAM_ERROR_UNKNOWN			-(26)
+/** out of free device handle(s) */
+#define CAM_ERROR_MAXHANDLE			-(27)
+/** invalid device handle */
+#define CAM_ERROR_BADHANDLE			-(28)
+/** device was removed */
+#define CAM_ERROR_DISCONNECTED		-(29)
+
 
 
 
 
 typedef struct
 {
-	unsigned int	ssize;				// sizeof this struct
-	unsigned short	vendor_id;			//
-	unsigned short	product_id;			//
-	unsigned char	vendor_name[32];	//
-	unsigned char	product_name[32];	//
-	unsigned char	model[16];			//
+	/** sizeof this struct */
+	unsigned int	ssize;
+	unsigned short	vendor_id;
+	unsigned short	product_id;
+	unsigned char	vendor_name[32];
+	unsigned char	product_name[32];
+	unsigned char	model[16];
 
 
 }PS2CAM_DEVICE_INFO;
@@ -146,12 +178,15 @@ typedef struct
 	int			bandwidth;
 	int			stream_pocket_size;
 
-	int			controll;				//controll endpoint
-	int			stream;					//stream   endpoint
+	/** controll endpoint */
+	int			controll;
+	/** stream   endpoint */
+	int			stream;
 
-	int			result;					//info extracted from callback
-	int			bytes;					//
-	void		*arg;					//
+	/** info extracted from callback */
+	int			result;
+	int			bytes;
+	void		*arg;
 
 
 }CAMERA_DEVICE;
@@ -175,15 +210,24 @@ typedef struct
 
 typedef struct
 {
-	unsigned int	ssize;			// structure size
-	unsigned int	mask;			//
-	unsigned short	width;			// camera horizontal resolution
-	unsigned short	height;			// camera vertical   resolution
-	unsigned short	x_offset;		// x offset of camera image
-	unsigned short	y_offset;		// y offset of camera image
-	unsigned char	h_divider;		// Horizontal Divider
-	unsigned char	v_divider;		// Vertical Divider
-	unsigned short	framerate;		// framerate
+	/** structure size */
+	unsigned int	ssize;
+	unsigned int	mask;
+	/** camera horizontal resolution */
+	unsigned short	width;
+	/** camera vertical   resolution */
+	unsigned short	height;
+	/** x offset of camera image */
+	unsigned short	x_offset;
+	/** y offset of camera image */
+	unsigned short	y_offset;
+	/** Horizontal Divider */
+	unsigned char	h_divider;
+	/** Vertical Divider */
+	unsigned char	v_divider;
+	/** framerate */
+	unsigned short	framerate;
+
 
 }PS2CAM_DEVICE_CONFIG;
 
@@ -193,22 +237,29 @@ typedef struct
 
 typedef struct
 {
-	unsigned char	magic1;	//0x01
-	unsigned char	magic2;	//0xff
-	unsigned char	magic3;	//0xff
-	unsigned char	type;	//0x51:EOF     0x50:SOF
-	unsigned char	uk1;	//?
-	unsigned char	uk2;	//?
-	unsigned char	uk3;	//?
-	unsigned char	uk4;	//?
-	unsigned char	uk5;	//?
-	unsigned char	frame;	//0x01:framehead+no data, 0x00:framehead+data
-	unsigned char	uk6;	//?
-	unsigned char	uk7;	//?
-	unsigned char	uk8;	//?
-	unsigned char	uk9;	//?
-	unsigned char	Lo;		//file size Lo byte (only in EOF)
-	unsigned char	Hi;		//file size Hi byte (only in EOF)
+	/** 0x01 */
+	unsigned char	magic1;
+	/** 0xff */
+	unsigned char	magic2;
+	/** 0xff */
+	unsigned char	magic3;
+	/** 0x51:EOF     0x50:SOF */
+	unsigned char	type;
+	unsigned char	uk1;
+	unsigned char	uk2;
+	unsigned char	uk3;
+	unsigned char	uk4;
+	unsigned char	uk5;
+	/** 0x01:framehead+no data, 0x00:framehead+data */
+	unsigned char	frame;
+	unsigned char	uk6;
+	unsigned char	uk7;
+	unsigned char	uk8;
+	unsigned char	uk9;
+	/** file size Lo byte (only in EOF) */
+	unsigned char	Lo;
+	/** file size Hi byte (only in EOF) */
+	unsigned char	Hi;
 
 }EYETOY_FRAME_HEAD;
 
