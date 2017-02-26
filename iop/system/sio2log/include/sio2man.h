bfdfa124b57bc648ac5b8d231e831dd24669b26d
uyjulian <uyjulian@gmail.com>
2017-02-26 10:50:31 -0600

    Comment cleanup iop/tcpip/tcpip

--- iop/system/sio2log/include/sio2man.h
+++ iop/system/sio2log/include/sio2man.h
@@ -6,9 +6,12 @@
 # Copyright (c) 2003  Marcus R. Brown <mrbrown@0xd6.org>
 # Licenced under Academic Free License version 2.0
 # Review ps2sdk README & LICENSE files for further details.
-#
-# rom0:SIO2MAN module definitions
 */
+
+/**
+ * @file
+ * rom0:SIO2MAN module definitions
+ */
 
 #ifndef SIO2MAN_H
 #define SIO2MAN_H
