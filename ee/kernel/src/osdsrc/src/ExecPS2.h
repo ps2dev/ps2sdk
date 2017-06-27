bb6b792f3c136e55594a5670d6f44fecaa8ad5b7
sp193 <pirorin187@gmail.com>
2017-06-27 20:50:26 +0800

    Updated copyright messages for kernel patches.

--- ee/kernel/src/osdsrc/src/ExecPS2.h
+++ ee/kernel/src/osdsrc/src/ExecPS2.h
@@ -1,2 +1,10 @@
-void *ExecPS2Patch(void *EntryPoint, void *gp, int argc, char *argv[]);
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+*/
 
+void *ExecPS2Patch(void *EntryPoint, void *gp, int argc, char *argv[]);
--- ee/kernel/src/tlbfunc.c
+++ ee/kernel/src/tlbfunc.c
@@ -1,3 +1,12 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+*/
+
 #include <kernel.h>
 #include <stdio.h>
 #include <tlbfunc.h>
