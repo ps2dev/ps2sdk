580c3f336468e574f8be6c42329ba31ffb4b5ac7
Rick Gaiser <rgaiser@gmail.com>
2018-04-28 22:00:40 +0200

    Add USB block-device driver for BDM

---
+++ iop/usb/usbmass_bd/src/main.c
@@ -0,0 +1,35 @@
+#define MAJOR_VER 1
+#define MINOR_VER 1
+#define MODNAME "usbmass_bd"
+
+#include "scsi.h"
+#include <irx.h>
+#include <loadcore.h>
+#include <stdio.h>
+
+//#define DEBUG  //comment out this line when not debugging
+#include "module_debug.h"
+
+IRX_ID(MODNAME, MAJOR_VER, MINOR_VER);
+
+extern int usb_mass_init(void);
+
+int _start(int argc, char* argv[])
+{
+    M_PRINTF("USB MASS Driver v%d.%d\n", MAJOR_VER, MINOR_VER);
+
+    // initialize the SCSI driver
+    if (scsi_init() != 0) {
+        M_PRINTF("ERROR: initializing SCSI driver!\n");
+        return MODULE_NO_RESIDENT_END;
+    }
+
+    // initialize the USB driver
+    if (usb_mass_init() != 0) {
+        M_PRINTF("ERROR: initializing USB driver!\n");
+        return MODULE_NO_RESIDENT_END;
+    }
+
+    // return resident
+    return MODULE_RESIDENT_END;
+}
