ps2dev/ps2sdk

commit ba30aa1e750f8921d66ead3cc84954d41403e578

(NETMAN) Changed NETMAN to support zero-copy receiving for the EE-side protocol stack.

common/include/netman_rpc.h
@@ -32,7 +32,6 @@ enum NETMAN_IOP_RPC_FUNC_NUMS{
 
 struct NetManEEInitResult{
 	s32 result;
-	void *FrameBuffer;
 };
 
 struct NetManRegNetworkStack{
@@ -50,7 +49,7 @@ struct NetManQueryMainNetIFResult{
 };
 
 #define NETMAN_MAX_FRAME_SIZE	1536	//Maximum 1518 bytes, rounded up to nearest multiple of 16-byte units + 16 (for alignment)
-#define NETMAN_RPC_BLOCK_SIZE	64
+#define NETMAN_RPC_BLOCK_SIZE	32
 
 struct NetManIoctl{
 	u32 command;
@@ -69,6 +68,13 @@ struct NetManPktCmd {
 	u8 id;
 	u8 offset;	//For alignment correction on the EE (unused for IOP->EE).
 	u16 length;
+};
+
+struct NetManBD {
+	u32 length;	//When set to 0, buffer is available for use by MAC driver.
+	void *packet;
+	void *payload;	//Pointer to the data section of the packet.
+	u32 unused;
 };
 
 #endif /* __NETMAN_RPC_H__ */

ee/network/tcpip/src/include/lwipopts.h
@@ -107,7 +107,7 @@
  * PBUF_POOL_SIZE: the number of buffers in the pbuf pool.
  */
 //SP193: should be at least ((TCP_WND/PBUF_POOL_BUFSIZE)+1). But that is too small to accommodate data not accepted by the application layer and multiple connections.
-#define PBUF_POOL_SIZE			60
+#define PBUF_POOL_SIZE			64
 
 /**
  * MEMP_NUM_TCPIP_MSG_INPKT: the number of struct tcpip_msg, which are used

common/include/netman.h
@@ -8,6 +8,7 @@
 
 //Common structures
 #define NETMAN_NETIF_NAME_MAX_LEN	4
+#define NETMAN_NETIF_FRAME_SIZE		1514
 
 struct NetManNetProtStack{
 	void (*LinkStateUp)(void);
@@ -17,7 +18,8 @@ struct NetManNetProtStack{
 	void (*EnQRxPacket)(void *packet);
 	int (*NextTxPacket)(void **payload);
 	void (*DeQTxPacket)(void);
-	int (*AfterTxPacket)(void **payload);	//For EE only, peek at the packet after the current packet.
+	int (*AfterTxPacket)(void **payload);				//For EE only, peek at the packet after the current packet.
+	void (*ReallocRxPacket)(void *packet, unsigned int size);	//For EE only, update the size of the Rx packet (size will be always smaller than NETMAN_NETIF_FRAME_SIZE).
 };
 
 /** Flow-control */
@@ -105,7 +107,8 @@ void NetManNetProtStackEnQRxPacket(void *packet);
 int NetManTxPacketNext(void **payload);
 void NetManTxPacketDeQ(void);
 
-int NetManTxPacketAfter(void **payload);	//For EE only, for NETMAN's internal use
+int NetManTxPacketAfter(void **payload);					//For EE only, for NETMAN's internal use.
+void NetManNetProtStackReallocRxPacket(void *packet, unsigned int length);	//For EE only, for NETMAN's internal use.
 
 /* NETIF flags. */
 /** Set internally by NETMAN. Do not set externally. */

ee/network/netman/src/include/rpc_server.h
@@ -1,2 +1,3 @@
 int NetManInitRPCServer(void);
 void NetManDeinitRPCServer(void);
+int NetManRPCAllocRxBuffers(void);

ee/network/netman/src/netman.c
@@ -79,7 +79,8 @@ int NetManRegisterNetworkStack(const struct NetManNetProtStack *stack)
 			{
 				memcpy(&MainNetProtStack, stack, sizeof(MainNetProtStack));
 				IsNetStackInitialized=1;
-				NetManUpdateStackNIFLinkState();
+				if((result=NetManRPCAllocRxBuffers()) == 0)
+					NetManUpdateStackNIFLinkState();
 			}
 		}
 		else result=0;
@@ -140,4 +141,9 @@ void NetManTxPacketDeQ(void)
 int NetManTxPacketAfter(void **payload)
 {
 	return IsInitialized?MainNetProtStack.AfterTxPacket(payload):-1;
+}
+
+void NetManNetProtStackReallocRxPacket(void *packet, unsigned int length)
+{
+	if(IsNetStackInitialized) MainNetProtStack.ReallocRxPacket(packet, length);
 }

ee/network/netman/src/rpc_server.c
@@ -15,42 +15,64 @@ static unsigned char NETMAN_RpcSvr_ThreadStack[0x1000] ALIGNED(16);
 static unsigned char NETMAN_Rx_ThreadStack[0x1000] ALIGNED(16);
 static unsigned char IsInitialized=0, IsProcessingRx;
 
-static u8 *FrameBuffer = NULL;
-static u16 *FrameBufferStatus = NULL;
-static u8 *RxIOPFrameBufferStatus;
+static struct NetManBD *FrameBufferStatus = NULL;
+static struct NetManBD *RxIOPFrameBufferStatus;
 static unsigned short int RxBufferRdPtr;
 extern void *_gp;
 
 static void NETMAN_RxThread(void *arg);
 
-static void ClearBufferLen(int index)
+static void ClearBufferLen(int index, void *packet, void *payload)
 {
-	static u32 zero[4] ALIGNED(16) = {0, 0, 0, 0};
+	struct NetManBD *bd;
 	SifDmaTransfer_t dmat;
 
-	FrameBufferStatus[index] = 0;
+	bd = UNCACHED_SEG(&FrameBufferStatus[index]);
+	bd->length = 0;
+	bd->packet = packet;
+	bd->payload = payload;
 
 	//Transfer to IOP RAM
-	dmat.src = (void*)zero;
-	dmat.dest = &RxIOPFrameBufferStatus[index * 16];
-	dmat.size = sizeof(zero);
+	dmat.src = (void*)&FrameBufferStatus[index];
+	dmat.dest = &RxIOPFrameBufferStatus[index];
+	dmat.size = sizeof(struct NetManBD);
 	dmat.attr = 0;
 	while(SifSetDma(&dmat, 1) == 0){ };
 }
 
 static void HandleRxEvent(void *packet, void *common)
 {
-	struct NetManPktCmd *bd = (struct NetManPktCmd*)&((SifCmdHeader_t*)packet)->opt;
-	u8 id = bd->id;
-	u16 len = bd->length;
+	struct NetManPktCmd *cmd = (struct NetManPktCmd*)&((SifCmdHeader_t*)packet)->opt;
+	u8 id = cmd->id;
+	u16 len = cmd->length;
+	struct NetManBD *bd = UNCACHED_SEG(&FrameBufferStatus[id]);
 
-	FrameBufferStatus[id] = len;
+	bd->length = len;
 
 	if(!IsProcessingRx)
 	{
 		IsProcessingRx = 1;
 		iWakeupThread(NETMAN_Rx_threadID);
 	}
+}
+
+int NetManRPCAllocRxBuffers(void)
+{
+	int i;
+	void *packet, *payload;
+
+	for(i = 0; i < NETMAN_RPC_BLOCK_SIZE; i++)
+	{
+		if((packet = NetManNetProtStackAllocRxPacket(NETMAN_NETIF_FRAME_SIZE, &payload)) != NULL)
+		{
+			ClearBufferLen(i, packet, payload);
+		} else {
+			printf("NETMAN: error - unable to allocate Rx FIFO buffers.\n");
+			return -ENOMEM;
+		}
+	}
+
+	return 0;
 }
 
 /* Main EE RPC thread. */
@@ -64,23 +86,20 @@ static void *NETMAN_EE_RPC_Handler(int fnum, void *buffer, int NumBytes)
 		case NETMAN_EE_RPC_FUNC_INIT:
 			RxIOPFrameBufferStatus = *(void**)buffer;
 
-			if(FrameBuffer == NULL) FrameBuffer = memalign(64, NETMAN_MAX_FRAME_SIZE * NETMAN_RPC_BLOCK_SIZE);
-			if(FrameBufferStatus == NULL) FrameBufferStatus = malloc(sizeof(u16) * NETMAN_RPC_BLOCK_SIZE);
+			//Maintain 64-byte alignment to avoid non-uncached writes to the same cache line from contaminating the line.
+			if(FrameBufferStatus == NULL) FrameBufferStatus = memalign(64, sizeof(struct NetManBD) * NETMAN_RPC_BLOCK_SIZE);
 
-			if(FrameBuffer != NULL && FrameBufferStatus != NULL)
+			if(FrameBufferStatus != NULL)
 			{
-				memset(UNCACHED_SEG(FrameBuffer), 0, NETMAN_MAX_FRAME_SIZE * NETMAN_RPC_BLOCK_SIZE);
-				memset(FrameBufferStatus, 0, sizeof(u16) * NETMAN_RPC_BLOCK_SIZE);
+				memset(UNCACHED_SEG(FrameBufferStatus), 0, sizeof(struct NetManBD) * NETMAN_RPC_BLOCK_SIZE);
 				RxBufferRdPtr = 0;
 				IsProcessingRx = 0;
-
-				((struct NetManEEInitResult *)buffer)->FrameBuffer = FrameBuffer;
 
 				thread.func=&NETMAN_RxThread;
 				thread.stack=NETMAN_Rx_ThreadStack;
 				thread.stack_size=sizeof(NETMAN_Rx_ThreadStack);
 				thread.gp_reg=&_gp;
-				thread.initial_priority=0x57;	/* Should be given a higher priority than the protocol stack, so that it can dump frames in the EE and return. */
+				thread.initial_priority=0x59;	/* Should be given a lower priority than the protocol stack, so that the protocol stack can process incoming frames. */
 				thread.attr=thread.option=0;
 
 				if((NETMAN_Rx_threadID=CreateThread(&thread)) >= 0)
@@ -99,11 +118,6 @@ static void *NETMAN_EE_RPC_Handler(int fnum, void *buffer, int NumBytes)
 			result=buffer;
 			break;
 		case NETMAN_EE_RPC_FUNC_DEINIT:
-			if(FrameBuffer != NULL)
-			{
-				free(FrameBuffer);
-				FrameBuffer = NULL;
-			}
 			if(FrameBufferStatus != NULL)
 			{
 				free(FrameBufferStatus);
@@ -138,16 +152,19 @@ static void NETMAN_RPC_Thread(void *arg)
 
 static void NETMAN_RxThread(void *arg)
 {
-	void *data, *payload;
-	u32 PacketLength;
-	void *packet;
+	volatile struct NetManBD *bd;
+	void *payload, *payloadNext;
+	u32 PacketLength, PacketLengthAligned;
+	void *packet, *packetNext;
 	u8 run;
 
 	while(1)
 	{
+		bd = UNCACHED_SEG(&FrameBufferStatus[RxBufferRdPtr]);
+
 		do {
 			DI();
-			PacketLength = FrameBufferStatus[RxBufferRdPtr];
+			PacketLength = bd->length;
 			if (PacketLength > 0)
 			{
 				run = 1;
@@ -161,17 +178,21 @@ static void NETMAN_RxThread(void *arg)
 				SleepThread();
 		} while(!run);
 
-		data = UNCACHED_SEG(&FrameBuffer[RxBufferRdPtr * NETMAN_MAX_FRAME_SIZE]);
+		payload = bd->payload;
+		packet = bd->packet;
 
-		if((packet = NetManNetProtStackAllocRxPacket(PacketLength, &payload)) != NULL)
-		{
-			memcpy(payload, data, PacketLength);
-			NetManNetProtStackEnQRxPacket(packet);
-		}
+		//Must successfully allocate a replacement buffer for the input buffer.
+		while((packetNext = NetManNetProtStackAllocRxPacket(NETMAN_NETIF_FRAME_SIZE, &payloadNext)) == NULL){};
+		ClearBufferLen(RxBufferRdPtr, packetNext, payloadNext);
 
-		ClearBufferLen(RxBufferRdPtr);
 		//Increment read pointer by one place.
 		RxBufferRdPtr = (RxBufferRdPtr + 1) % NETMAN_RPC_BLOCK_SIZE;
+
+		//Now process the received packet.
+		PacketLengthAligned = (PacketLength + 63) & ~63;
+		SifWriteBackDCache(payload, PacketLengthAligned);
+		NetManNetProtStackReallocRxPacket(packet, PacketLength);
+		NetManNetProtStackEnQRxPacket(packet);
 	}
 }
 

ee/network/tcpip/src/ps2ip.c
@@ -178,6 +178,11 @@ static void *AllocRxPacket(unsigned int size, void **payload)
 	return pbuf;
 }
 
+static void ReallocRxPacket(void *packet, unsigned int size)
+{
+	pbuf_realloc((struct pbuf *)packet, size);
+}
+
 static void FreeRxPacket(void *packet)
 {
 	pbuf_free(packet);
@@ -307,7 +312,8 @@ int ps2ipInit(struct ip4_addr *ip_address, struct ip4_addr *subnet_mask, struct 
 		&EnQRxPacket,
 		&NextTxPacket,
 		&DeQTxPacket,
-		&AfterTxPacket
+		&AfterTxPacket,
+		&ReallocRxPacket
 	};
 
 	NetManInit();

iop/network/netman/src/rpc_client.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <intrman.h>
+#include <limits.h>
 #include <sifcmd.h>
 #include <sifman.h>
 #include <sysclib.h>
@@ -25,7 +26,6 @@ static union{
 
 //Data for IOP -> EE transfers
 static unsigned short int EEFrameBufferWrPtr;
-static u8 *EEFrameBuffer = NULL;	/* On the EE side */
 
 static int NetManIOSemaID = -1;
 
@@ -38,24 +38,27 @@ struct NetManPacketBuffer{
 //Data for SPEED -> IOP transfers
 static struct NetManPacketBuffer pbufs[NETMAN_RPC_BLOCK_SIZE];
 static u8 *FrameBuffer = NULL;
-static u8 *FrameBufferStatus = NULL;
+static struct NetManBD *FrameBufferStatus = NULL;
 static SifCmdHeader_t *SifCmdBuffer = NULL;
 
 int NetManInitRPCClient(void)
 {
 	static const char NetManID[] = "NetMan";
 	iop_sema_t sema;
-	int result;
+	int result, i;
 
 	if(FrameBuffer == NULL) FrameBuffer = malloc(NETMAN_RPC_BLOCK_SIZE * NETMAN_MAX_FRAME_SIZE);
-	if(FrameBufferStatus == NULL) FrameBufferStatus = malloc(NETMAN_RPC_BLOCK_SIZE * 16);
+	if(FrameBufferStatus == NULL) FrameBufferStatus = malloc(NETMAN_RPC_BLOCK_SIZE * sizeof(struct NetManBD));
 	if(SifCmdBuffer == NULL) SifCmdBuffer = malloc(NETMAN_RPC_BLOCK_SIZE * sizeof(SifCmdHeader_t));
 
 	if(FrameBuffer != NULL && FrameBufferStatus != NULL && SifCmdBuffer != NULL)
 	{
 		memset(FrameBuffer, 0, NETMAN_RPC_BLOCK_SIZE * NETMAN_MAX_FRAME_SIZE);
-		memset(FrameBufferStatus, 0, NETMAN_RPC_BLOCK_SIZE * 16);
+		memset(FrameBufferStatus, 0, NETMAN_RPC_BLOCK_SIZE * sizeof(struct NetManBD));
 		EEFrameBufferWrPtr = 0;
+
+		for(i = 0; i < NETMAN_RPC_BLOCK_SIZE; i++)	//Mark all descriptors as "in-use", until the EE-side allocates buffers.
+			FrameBufferStatus[i].length = UINT_MAX;
 
 		while((result=sceSifBindRpc(&EEClient, NETMAN_RPC_NUMBER, 0))<0 || EEClient.server==NULL) DelayThread(500);
 
@@ -63,8 +66,6 @@ int NetManInitRPCClient(void)
 		{
 			if((result=SifRpcRxBuffer.EEInitResult.result) == 0)
 			{
-				EEFrameBuffer = SifRpcRxBuffer.EEInitResult.FrameBuffer;
-
 				sema.attr = 0;
 				sema.option = (u32)NetManID;
 				sema.initial = 1;
@@ -115,13 +116,14 @@ void NetManRpcToggleGlobalNetIFLinkState(int state)
 void *NetManRpcNetProtStackAllocRxPacket(unsigned int length, void **payload)
 {
 	struct NetManPacketBuffer *result;
+	volatile const struct NetManBD *bd = &FrameBufferStatus[EEFrameBufferWrPtr];
 
 	//Wait for a free spot to appear in the ring buffer.
-	while(*(vu32*)&FrameBufferStatus[EEFrameBufferWrPtr * 16] != 0) { }
+	while(bd->length != 0) { }
 
 	//Allocation of PBUF descriptors is tied with the allocation of frame slots in the ring buffer by EnQ.
 	result = &pbufs[EEFrameBufferWrPtr];
-	result->handle = &FrameBufferStatus[EEFrameBufferWrPtr * 16];
+	result->handle = (void*)bd;
 	result->payload = &FrameBuffer[EEFrameBufferWrPtr * NETMAN_MAX_FRAME_SIZE];
 	result->length = length;
 
@@ -141,23 +143,25 @@ void NetManRpcNetProtStackFreeRxPacket(void *packet)
 static void EnQFrame(const void *frame, unsigned int length)
 {
 	SifCmdHeader_t *pcmd;
-	struct NetManPktCmd *bd;
+	struct NetManPktCmd *cmd;
+	struct NetManBD *bd;
 
 	//No need to wait for a free spot to appear, as Alloc already took care of that.
 	pcmd = &SifCmdBuffer[EEFrameBufferWrPtr];
+	bd = &FrameBufferStatus[EEFrameBufferWrPtr];
 
 	//Prepare SIFCMD packet.
 	//Record the frame length.
-	bd = (struct NetManPktCmd*)&pcmd->opt;
-	bd->id = EEFrameBufferWrPtr;
+	cmd = (struct NetManPktCmd*)&pcmd->opt;
+	cmd->id = EEFrameBufferWrPtr;
+	cmd->length = length;
 	bd->length = length;
-	*(vu32*)&FrameBufferStatus[EEFrameBufferWrPtr * 16] = length;
 
 	//Transfer the frame over to the EE
 	//Notify the receive thread of the incoming frame.
 	while(sceSifSendCmd(NETMAN_SIFCMD_ID, pcmd, sizeof(SifCmdHeader_t),
 				(void*)frame,
-				(void*)&EEFrameBuffer[EEFrameBufferWrPtr * NETMAN_MAX_FRAME_SIZE],
+				bd->payload,
 				(length + 3) & ~3) == 0){ };
 
 	//Increase the write (IOP -> EE) pointer by one place.

commit dda2b44e83b04fa733462235915f2c27d8d8e60b

(NETMAN) fixed errors introduced in ba30aa1. Re-increased NETMAN ring buffers to 64 and increased EE LWIP's pool size to 128.
NETMAN's ring buffers cannot be smaller than 64. Performance may worsen and resource exhaustion may occur.
NETMAN does not have a packet buffer pool, as of commit ba30aa1. The buffers are taken from LWIP's PBUF, hence we should be generous here.
With 128 PBUFs, 64 can go to NETMAN while the remainder can be used for queuing by LWIP (before they are processed by the application layer).

common/include/netman_rpc.h
@@ -49,7 +49,7 @@ struct NetManQueryMainNetIFResult{
 };
 
 #define NETMAN_MAX_FRAME_SIZE	1536	//Maximum 1518 bytes, rounded up to nearest multiple of 16-byte units + 16 (for alignment)
-#define NETMAN_RPC_BLOCK_SIZE	32
+#define NETMAN_RPC_BLOCK_SIZE	64	//Small sizes will result in poorer performance and perhaps stability issues (due to resource exhaustion).
 
 struct NetManIoctl{
 	u32 command;

ee/network/tcpip/src/include/lwipopts.h
@@ -107,7 +107,7 @@
  * PBUF_POOL_SIZE: the number of buffers in the pbuf pool.
  */
 //SP193: should be at least ((TCP_WND/PBUF_POOL_BUFSIZE)+1). But that is too small to accommodate data not accepted by the application layer and multiple connections.
-#define PBUF_POOL_SIZE			64
+#define PBUF_POOL_SIZE			128
 
 /**
  * MEMP_NUM_TCPIP_MSG_INPKT: the number of struct tcpip_msg, which are used