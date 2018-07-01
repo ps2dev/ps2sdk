61b10b1876418227d9f2b2b6a525fde84bfe478e
sp193 <pirorin187@gmail.com>
2018-07-02 04:19:24 +0800

    (USBHDFSD) fat_write.c: redefined ALLOC_SECTOR as scache_readSector and disabled scache_allocSector. Avoid corrupting the adjacent cluster if: * Directory entries are added (renaming will destroy and add a new entry). * The cache block size does not match the cluster size. * The cache blocks are not aligned to the start of the clusters.

--- iop/usb/usbhdfsd/src/include/scache.h
+++ iop/usb/usbhdfsd/src/include/scache.h
@@ -4,7 +4,7 @@
 cache_set* scache_init(mass_dev* dev, int sectorSize);
 void scache_close(cache_set* cache);
 void scache_kill(cache_set* cache); //dlanor: added for disconnection events (flush impossible)
-int  scache_allocSector(cache_set* cache, unsigned int sector, void** buf);
+//int  scache_allocSector(cache_set* cache, unsigned int sector, void** buf);
 int  scache_readSector(cache_set* cache, unsigned int sector, void** buf);
 int  scache_writeSector(cache_set* cache, unsigned int sector);
 int  scache_flushSectors(cache_set* cache);
