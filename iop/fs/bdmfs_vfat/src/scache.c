ps2dev/ps2sdk

commit 53385fb0af1c4552887d86583c45ff819f82557a

Updated USBHDFSD to support disks >1TB and <=2TB. Modified the Makefile to automatically wipe the old PS2SDK destination folders before installing.

iop/usb/usbhdfsd/src/scache.c
@@ -55,8 +55,8 @@ typedef struct _cache_record
 struct _cache_set
 {
 	mass_dev* dev;
-	int sectorSize;
-	int indexLimit;
+	unsigned int sectorSize;
+	unsigned int indexLimit;
 	unsigned char* sectorBuf; // = NULL;		//sector content - the cache buffer
 	cache_record rec[CACHE_SIZE];	//cache info record
 
@@ -71,7 +71,7 @@ struct _cache_set
 //---------------------------------------------------------------------------
 static void initRecords(cache_set* cache)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < CACHE_SIZE; i++)
 	{
@@ -101,7 +101,7 @@ static int getSlot(cache_set* cache, unsigned int sector) {
 //---------------------------------------------------------------------------
 /* search cache records for the sector number stored in cache */
 static int getIndexRead(cache_set* cache, unsigned int sector) {
-	int i;
+	unsigned int i;
 	int index =-1;
 
 	for (i = 0; i < CACHE_SIZE; i++) {
@@ -121,9 +121,9 @@ static int getIndexRead(cache_set* cache, unsigned int sector) {
 //---------------------------------------------------------------------------
 /* select the best record where to store new sector */
 static int getIndexWrite(cache_set* cache, unsigned int sector) {
-	int i, ret;
-	int minTax = 0x0FFFFFFF;
-	int index = 0;
+	int ret;
+	unsigned int minTax = 0x0FFFFFFF;
+	unsigned int i, index = 0;
 
 	for (i = 0; i < CACHE_SIZE; i++) {
 		if (cache->rec[i].tax < minTax) {
@@ -134,14 +134,14 @@ static int getIndexWrite(cache_set* cache, unsigned int sector) {
 
 	//this sector is dirty - we need to flush it first
 	if (cache->rec[index].writeDirty) {
-		XPRINTF("scache: getIndexWrite: sector is dirty : %d   index=%d \n", cache->rec[index].sector, index);
-		ret = WRITE_SECTOR(cache->dev, cache->rec[index].sector, cache->sectorBuf + (index * BLOCK_SIZE), BLOCK_SIZE);
-		cache->rec[index].writeDirty = 0;
-		//TODO - error handling
+		XPRINTF("scache: getIndexWrite: sector is dirty : %u   index=%d \n", cache->rec[index].sector, index);
+		ret = WRITE_SECTOR(cache->dev, cache->rec[index].sector, cache->sectorBuf + (index * BLOCK_SIZE), BLOCK_SIZE/cache->sectorSize);
 		if (ret < 0) {
-			printf("scache: ERROR writing sector to disk! sector=%d\n", sector);
+			printf("scache: ERROR writing sector to disk! sector=%u\n", sector);
+			return ret;
 		}
 
+		cache->rec[index].writeDirty = 0;
 	}
 	cache->rec[index].tax +=2;
 	cache->rec[index].sector = sector;
@@ -154,8 +154,8 @@ static int getIndexWrite(cache_set* cache, unsigned int sector) {
 	flush dirty sectors
  */
 int scache_flushSectors(cache_set* cache) {
-	int i,ret;
-	int counter = 0;
+	unsigned int i;
+	int counter = 0, ret;
 
 	XPRINTF("cache: flushSectors devId = %i \n", cache->dev->devId);
 
@@ -167,14 +167,14 @@ int scache_flushSectors(cache_set* cache) {
 
 	for (i = 0; i < CACHE_SIZE; i++) {
 		if (cache->rec[i].writeDirty) {
-			XPRINTF("scache: flushSectors dirty index=%d sector=%d \n", i, cache->rec[i].sector);
-			ret = WRITE_SECTOR(cache->dev, cache->rec[i].sector, cache->sectorBuf + (i * BLOCK_SIZE), BLOCK_SIZE);
-			cache->rec[i].writeDirty = 0;
-			//TODO - error handling
+			XPRINTF("scache: flushSectors dirty index=%d sector=%u \n", i, cache->rec[i].sector);
+			ret = WRITE_SECTOR(cache->dev, cache->rec[i].sector, cache->sectorBuf + (i * BLOCK_SIZE), BLOCK_SIZE/cache->sectorSize);
 			if (ret < 0) {
-				printf("scache: ERROR writing sector to disk! sector=%d\n", cache->rec[i].sector);
+				printf("scache: ERROR writing sector to disk! sector=%u\n", cache->rec[i].sector);
 				return ret;
 			}
+
+			cache->rec[i].writeDirty = 0;
 			counter ++;
 		}
 	}
@@ -188,7 +188,7 @@ int scache_readSector(cache_set* cache, unsigned int sector, void** buf) {
 	int ret;
 	unsigned int alignedSector;
 
-	XPRINTF("cache: readSector devId = %i %X sector = %i \n", cache->dev->devId, (int) cache, sector);
+	XPRINTF("cache: readSector devId = %i %p sector = %u \n", cache->dev->devId, cache, sector);
 	if (cache == NULL) {
 		printf("cache: devId cache not created = %i \n", cache->dev->devId);
 		return -1;
@@ -212,11 +212,11 @@ int scache_readSector(cache_set* cache, unsigned int sector, void** buf) {
 	//compute alignedSector - to prevent storage of duplicit sectors in slots
 	alignedSector = (sector/cache->indexLimit)*cache->indexLimit;
 	index = getIndexWrite(cache, alignedSector);
-	XPRINTF("cache: indexWrite=%i slot=%d  alignedSector=%i\n", index, index / cache->indexLimit, alignedSector);
-	ret = READ_SECTOR(cache->dev, alignedSector, cache->sectorBuf + (index * cache->sectorSize), BLOCK_SIZE);
+	XPRINTF("cache: indexWrite=%i slot=%d  alignedSector=%u\n", index, index / cache->indexLimit, alignedSector);
+	ret = READ_SECTOR(cache->dev, alignedSector, cache->sectorBuf + (index * cache->sectorSize), BLOCK_SIZE/cache->sectorSize);
 
 	if (ret < 0) {
-		printf("scache: ERROR reading sector from disk! sector=%d\n", alignedSector);
+		printf("scache: ERROR reading sector from disk! sector=%u\n", alignedSector);
 		return ret;
 	}
 	*buf = cache->sectorBuf + (index * cache->sectorSize) + ((sector%cache->indexLimit) * cache->sectorSize);
@@ -238,7 +238,7 @@ int scache_allocSector(cache_set* cache, unsigned int sector, void** buf) {
 	//int ret;
 	unsigned int alignedSector;
 
-	XPRINTF("cache: allocSector devId = %i sector = %i \n", cache->dev->devId, sector);
+	XPRINTF("cache: allocSector devId = %i sector = %u \n", cache->dev->devId, sector);
 
 	index = getIndexRead(cache, sector);
 	XPRINTF("cache: indexRead=%i \n", index);
@@ -262,7 +262,7 @@ int scache_writeSector(cache_set* cache, unsigned int sector) {
 	int index; //index is given in single sectors not octal sectors
 	//int ret;
 
-	XPRINTF("cache: writeSector devId = %i sector = %i \n", cache->dev->devId, sector);
+	XPRINTF("cache: writeSector devId = %i sector = %u \n", cache->dev->devId, sector);
 
 	index = getSlot(cache, sector);
 	if (index <  0) { //sector not found in cache
@@ -293,7 +293,7 @@ int scache_writeSector(cache_set* cache, unsigned int sector) {
 cache_set* scache_init(mass_dev* dev, int sectSize)
 {
 	cache_set* cache;
-	XPRINTF("cache: init devId = %i sectSize = %i \n", dev->devId, sectSize);
+	XPRINTF("cache: init devId = %i sectSize = %u \n", dev->devId, sectSize);
 
 	cache = malloc(sizeof(cache_set));
 	if (cache == NULL) {

iop/usb/usbhdfsd/src/usb_driver.c
@@ -3,6 +3,7 @@
  * See usbmass-ufi10.pdf
  */
 
+#include <errno.h>
 #include <sysclib.h>
 #include <thsemap.h>
 #include <stdio.h>
@@ -636,7 +637,7 @@ static inline int cbw_scsi_read_capacity(mass_dev* dev, void *buffer, int size)
 	return ret;
 }
 
-static inline int cbw_scsi_read_sector(mass_dev* dev, unsigned int lba, void* buffer, short int sectorSize, short int sectorCount)
+static inline int cbw_scsi_read_sector(mass_dev* dev, unsigned int lba, void* buffer, unsigned short int sectorCount)
 {
 	int ret;
 	static cbw_packet cbw={
@@ -665,7 +666,7 @@ static inline int cbw_scsi_read_sector(mass_dev* dev, unsigned int lba, void* bu
 	};
 	XPRINTF("USBHDFSD: cbw_scsi_read_sector\n");
 
-	cbw.dataTransferLength = sectorSize * sectorCount;
+	cbw.dataTransferLength = dev->sectorSize * sectorCount;
 
 	//scsi command packet
 	cbw.comData[2] = (lba & 0xFF000000) >> 24;	//lba 1 (MSB)
@@ -677,7 +678,7 @@ static inline int cbw_scsi_read_sector(mass_dev* dev, unsigned int lba, void* bu
 
 	usb_bulk_command(dev, &cbw);
 
-	if ((ret = usb_bulk_transfer(dev->bulkEpI, buffer, sectorSize * sectorCount)) == USB_RC_OK)
+	if ((ret = usb_bulk_transfer(dev->bulkEpI, buffer, dev->sectorSize * sectorCount)) == USB_RC_OK)
 		ret = usb_bulk_manage_status(dev, -TAG_READ);
 	else
 		XPRINTF("USBHDFSD: cbw_scsi_read_sector error from usb_bulk_transfer %d\n", ret);
@@ -685,7 +686,7 @@ static inline int cbw_scsi_read_sector(mass_dev* dev, unsigned int lba, void* bu
 	return ret;
 }
 
-static inline int cbw_scsi_write_sector(mass_dev* dev, unsigned int lba, void* buffer, short int sectorSize, short int sectorCount)
+static inline int cbw_scsi_write_sector(mass_dev* dev, unsigned int lba, const void* buffer, unsigned short int sectorCount)
 {
 	int ret;
 	static cbw_packet cbw={
@@ -714,7 +715,7 @@ static inline int cbw_scsi_write_sector(mass_dev* dev, unsigned int lba, void* b
 	};
 	XPRINTF("USBHDFSD: cbw_scsi_write_sector\n");
 
-	cbw.dataTransferLength = sectorSize * sectorCount;
+	cbw.dataTransferLength = dev->sectorSize * sectorCount;
 
 	//scsi command packet
 	cbw.comData[2] = (lba & 0xFF000000) >> 24;	//lba 1 (MSB)
@@ -726,7 +727,7 @@ static inline int cbw_scsi_write_sector(mass_dev* dev, unsigned int lba, void* b
 
 	usb_bulk_command(dev, &cbw);
 
-	if ((ret = usb_bulk_transfer(dev->bulkEpO, buffer, sectorSize * sectorCount)) == USB_RC_OK)
+	if ((ret = usb_bulk_transfer(dev->bulkEpO, (void*)buffer, dev->sectorSize * sectorCount)) == USB_RC_OK)
 		ret = usb_bulk_manage_status(dev, -TAG_WRITE);
 	else
 		XPRINTF("USBHDFSD: cbw_scsi_write_sector error from usb_bulk_transfer %d\n", ret);
@@ -755,28 +756,31 @@ static mass_dev* mass_stor_findDevice(int devId, int create)
 }
 
 /* size should be a multiple of sector size */
-int mass_stor_readSector(mass_dev* mass_device, unsigned int sector, unsigned char* buffer, int size) {
+int mass_stor_readSector(mass_dev* mass_device, unsigned int sector, unsigned char* buffer, unsigned short int count) {
 	//assert(size % mass_device->sectorSize == 0);
 	//assert(sector <= mass_device->maxLBA);
-	int ret;
+	int i;
 
-	ret = 1;
-	do {
-		ret = cbw_scsi_read_sector(mass_device, sector, buffer, mass_device->sectorSize, size/mass_device->sectorSize);
-	} while (ret != USB_RC_OK);
-	return (size / mass_device->sectorSize) * mass_device->sectorSize;
+	for(i=0; i<32; i++){
+		if(cbw_scsi_read_sector(mass_device, sector, buffer, count) == USB_RC_OK){
+			return count;
+		}
+	}
+	return -EIO;
 }
 
 /* size should be a multiple of sector size */
-int mass_stor_writeSector(mass_dev* mass_device, unsigned int sector, unsigned char* buffer, int size) {
+int mass_stor_writeSector(mass_dev* mass_device, unsigned int sector, const unsigned char* buffer, unsigned short int count) {
 	//assert(size % mass_device->sectorSize == 0);
 	//assert(sector <= mass_device->maxLBA);
-	int ret;
+	int i;
 
-	do {
-		ret = cbw_scsi_write_sector(mass_device, sector, buffer, mass_device->sectorSize, size/mass_device->sectorSize);
-	} while (ret != USB_RC_OK) ;
-	return (size / mass_device->sectorSize) * mass_device->sectorSize;
+	for(i=0; i<32; i++){
+		if(cbw_scsi_write_sector(mass_device, sector, buffer, count) == USB_RC_OK){
+			return count;
+		}
+	}
+	return -EIO;
 }
 
 /* test that endpoint is bulk endpoint and if so, update device info */

Makefile
@@ -60,6 +60,9 @@ release_base: env_release_check
 	@if test ! -d $(PS2SDK) ; then \
 	  $(MKDIR) -p $(PS2SDK) ; \
 	fi
+	rm -fr $(PS2SDK)/iop
+	rm -fr $(PS2SDK)/ee
+	rm -fr $(PS2SDK)/common
 	cp -f README $(PS2SDK)
 	cp -f CHANGELOG $(PS2SDK)
 	cp -f AUTHORS $(PS2SDK)

iop/usb/usbhdfsd/src/include/fat.h
@@ -147,7 +147,7 @@ static USBHD_INLINE unsigned int fat_cluster2sector(fat_bpb* partBpb, unsigned i
 
 unsigned int fat_getClusterRecord12(unsigned char* buf, int type);
 int      fat_getDirentry(unsigned char fatType, fat_direntry* dir_entry, fat_direntry_summary* dir );
-int      fat_getDirentrySectorData(fat_driver* fatd, unsigned int* startCluster, unsigned int* startSector, int* dirSector);
+int      fat_getDirentrySectorData(fat_driver* fatd, unsigned int* startCluster, unsigned int* startSector, unsigned int* dirSector);
 void     fat_invalidateLastChainResult(fat_driver* fatd);
 void     fat_getClusterAtFilePos(fat_driver* fatd, fat_dir* fatDir, unsigned int filePos, unsigned int* cluster, unsigned int* clusterPos);
 

iop/usb/usbhdfsd/src/include/fat_driver.h
@@ -98,7 +98,7 @@ int fat_getNextDirentry(fat_driver* fatd, fat_dir_list* fatdlist, fat_dir* fatDi
 
 fat_driver * fat_getData(int device);
 int      fat_getFileStartCluster(fat_driver* fatd, const unsigned char* fname, unsigned int* startCluster, fat_dir* fatDir);
-int      fat_getClusterChain(fat_driver* fatd, unsigned int cluster, unsigned int* buf, int bufSize, int start);
+int      fat_getClusterChain(fat_driver* fatd, unsigned int cluster, unsigned int* buf, unsigned int bufSize, int startFlag);
 
 #endif
 

iop/usb/usbhdfsd/src/include/mass_stor.h
@@ -15,8 +15,8 @@ struct _mass_dev
 	int status;
 	int interfaceNumber;	//interface number
 	int interfaceAlt;	//interface alternate setting
-	unsigned sectorSize; // = 512; // store size of sector from usb mass
-	unsigned maxLBA;
+	unsigned int sectorSize; // = 512; // store size of sector from usb mass
+	unsigned int maxLBA;
 	cache_set* cache;
 };
 
@@ -24,8 +24,8 @@ int InitUSB(void);
 int mass_stor_disconnect(int devId);
 int mass_stor_connect(int devId);
 int mass_stor_probe(int devId);
-int mass_stor_readSector(mass_dev* mass_device, unsigned int sector, unsigned char* buffer, int size);
-int mass_stor_writeSector(mass_dev* mass_device, unsigned int sector, unsigned char* buffer, int size);
+int mass_stor_readSector(mass_dev* mass_device, unsigned int sector, unsigned char* buffer, unsigned short int count);
+int mass_stor_writeSector(mass_dev* mass_device, unsigned int sector, const unsigned char* buffer, unsigned short int count);
 int mass_stor_configureNextDevice(void);
 
 #endif

commit 41ead8269c4320232228dbe44f6bf8bd5fd721d9

(USBHDFSD) fixed incorrect cache block eviction due to failure in comparing the clock (tax) variable correctly. Bug introduced in 53385fb. Re-added more debug messages.

iop/fs/bdmfs_vfat/src/scache.c
@@ -96,7 +96,7 @@ static int getIndexRead(cache_set* cache, unsigned int sector)
 static int getIndexWrite(cache_set* cache, unsigned int sector)
 {
     int ret;
-    unsigned int minTax = 0x0FFFFFFF;
+    int minTax = 0x0FFFFFFF;
     unsigned int i, index = 0;
 
     for (i = 0; i < CACHE_SIZE; i++) {

iop/usb/usbhdfsd/src/scache.c
@@ -99,7 +99,7 @@ static int getIndexRead(cache_set* cache, unsigned int sector) {
 /* select the best record where to store new sector */
 static int getIndexWrite(cache_set* cache, unsigned int sector) {
 	int ret;
-	unsigned int minTax = 0x0FFFFFFF;
+	int minTax = 0x0FFFFFFF;
 	unsigned int i, index = 0;
 
 	for (i = 0; i < CACHE_SIZE; i++) {

iop/usb/usbhdfsd/src/usb_driver.c
@@ -225,7 +225,7 @@ static int usb_bulk_clear_halt(mass_dev* dev, int endpoint) {
 		ret = cb_data.returnCode;
 	}
 	if (ret != USB_RC_OK) {
-		XPRINTF("USBHDFSD: Error - sending clear halt %d\n", ret);
+		printf("USBHDFSD: Error - sending clear halt %d\n", ret);
 	}
 
 	return ret;
@@ -265,7 +265,7 @@ static void usb_bulk_reset(mass_dev* dev, int mode) {
 			ret = usb_bulk_clear_halt(dev, USB_BLK_EP_OUT);
 	}
 	if(ret != USB_RC_OK){
-		XPRINTF("USBHDFSD: Error - sending reset %d to device %d.\n", ret, dev->devId);
+		printf("USBHDFSD: Error - sending reset %d to device %d.\n", ret, dev->devId);
 		dev->status |= USBMASS_DEV_STAT_ERR;
 	}
 }
@@ -293,13 +293,11 @@ static int usb_bulk_status(mass_dev* dev, csw_packet* csw, unsigned int tag) {
 		WaitSema(cb_data.sema);
 		ret = cb_data.returnCode;
 
-#ifdef DEBUG
 		if (cb_data.returnSize != 13)
 			printf("USBHDFSD: bulk csw.status returnSize: %i != 13\n", cb_data.returnSize);
 		if (csw->dataResidue != 0)
 			printf("USBHDFSD: bulk csw.status residue: %i\n", csw->dataResidue);
 		XPRINTF("USBHDFSD: bulk csw result: %d, csw.status: %i\n", ret, csw->status);
-#endif
 	}
 
 	return ret;
@@ -322,7 +320,7 @@ static int usb_bulk_manage_status(mass_dev* dev, unsigned int tag) {
 	if (ret != USB_RC_OK) { /* STALL bulk in  -OR- Bulk error */
 		usb_bulk_clear_halt(dev, USB_BLK_EP_IN); /* clear the stall condition for bulk in */
 
-		XPRINTF("USBHDFSD: usb_bulk_manage_status error %d ...\n", ret);
+		printf("USBHDFSD: usb_bulk_manage_status error %d ...\n", ret);
 		ret = usb_bulk_status(dev, &csw, tag); /* Attempt to read CSW from bulk in endpoint */
 	}
 
@@ -396,7 +394,7 @@ static int usb_bulk_command(mass_dev* dev, cbw_packet* packet ) {
 		ret = cb_data.returnCode;
 	}
 	if (ret != USB_RC_OK) {
-		XPRINTF("USBHDFSD: Error - sending bulk command %d. Calling reset recovery.\n", ret);
+		printf("USBHDFSD: Error - sending bulk command %d. Calling reset recovery.\n", ret);
 		usb_bulk_reset(dev, 3);
 	}
 
@@ -419,7 +417,7 @@ static int usb_bulk_transfer(mass_dev* dev, int direction, void* buffer, unsigne
 	}
 
 	if(ret != USB_RC_OK) {
-		XPRINTF("USBHDFSD: Error - bulk data transfer %d. Clearing HALT state.\n", cb_data.returnCode);
+		printf("USBHDFSD: Error - bulk data transfer %d. Clearing HALT state.\n", cb_data.returnCode);
 		usb_bulk_clear_halt(dev, direction);
 	}
 