2b469b52b69d7bdd9a96ade781bc6e244af55e3e
h4570 <hastoklai@gmail.com>
2020-11-20 12:32:45 +0100

    moved vif_added_bytes to packet2

--- ee/packet2/include/packet2_types.h
+++ ee/packet2/include/packet2_types.h
@@ -319,6 +319,11 @@ typedef struct
      * NULL, if no DIRECT/UNPACK is open. 
      */
     vif_code_t *vif_code_opened_at;
+    /** 
+     * Helper variable to control bytes that 
+     * was already "written" to VU. 
+     */
+    u32 vif_added_bytes;
 } packet2_t;
 
 /** Mask, used in VIF's STMASK opcode. */
