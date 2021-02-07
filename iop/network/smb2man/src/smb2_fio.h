ca3c8d11c031feff02cc1151d332dbf4c82218ff
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2021-02-07 12:43:47 +1000

    SMB2MAN initial checking
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

---
+++ iop/network/smb2man/src/smb2_fio.h
@@ -0,0 +1,14 @@
+/*
+  SMB2MAN
+  Ronnie Sahlberg <ronniesahlberg@gmail.com> 2021
+
+  Based on SMBMAN:
+  Copyright 2009-2010, jimmikaelkael
+  Licenced under Academic Free License version 3.0
+*/
+#ifndef __SMB2_FIO_H__
+#define __SMB2_FIO_H__
+
+int SMB2_initdev(void);
+
+#endif
