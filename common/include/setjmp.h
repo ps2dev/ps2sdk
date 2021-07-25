4619018d6d6adf158f04ce63078d5cd7f63b74ad
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-07-25 14:09:49 +0200

    Revert "setjmp header file"

--- common/include/setjmp.h
+++ common/include/setjmp.h
@@ -20,9 +20,15 @@
 
 #include <tamtypes.h>
 
+#ifdef _EE
+/* we need some room after the gpr, to store some fp registers. */
+#define _JBLEN 14
+#define _JBTYPE u128
+#else
 /* seems the IOP's sysclib does have one more value than the newlib's version... */
 #define _JBLEN 12
 #define _JBTYPE u32
+#endif
 
 typedef _JBTYPE jmp_buf[_JBLEN];
 
