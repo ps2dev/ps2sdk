ea5d0f365b8be775ef6debedd7fd5720c09818d2
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-08-08 18:20:02 +0200

    Improve elf-loader

--- ee/elf-loader/include/elf-loader.h
+++ ee/elf-loader/include/elf-loader.h
@@ -22,6 +22,15 @@ extern "C" {
 
 // Before call this method be sure that you have previously called sbv_patch_disable_prefix_check();
 int LoadELFFromFile(const char *filename, int argc, char *argv[]);
+/* partition is useful when you're launching an ELF from HDD. If this is the case a example could be:
+*   filename = pfs:something/myprogram.elf
+*   partition = hdd0:__common:
+* What iternally we will do is to concatenate partition + filename, and send it in the argv[0], which is used by CWD
+* So folowing the previous example. when elf is launched argv[0] = hdd0:__common:pfs:something/myprogram.elf
+* If you aren't going to load from HDD you can pass in partition either NULL or ""
+*/
+
+int LoadELFFromFileWithPartition(const char *filename, const char *partition, int argc, char *argv[]);
 
 #ifdef __cplusplus
 }
