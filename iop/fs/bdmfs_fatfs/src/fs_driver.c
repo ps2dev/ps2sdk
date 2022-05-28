3218f269a8317673c2d88817105f1a72bd366eb1
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2022-05-28 13:30:10 +0200

    Fill stat buffer.

--- iop/fs/bdmfs_fatfs/src/fs_driver.c
+++ iop/fs/bdmfs_fatfs/src/fs_driver.c
@@ -37,9 +37,9 @@ static int _fs_init_lock(void)
     M_DEBUG("%s\n", __func__);
 
     sp.initial = 1;
-    sp.max     = 1;
-    sp.option  = 0;
-    sp.attr    = 0;
+    sp.max = 1;
+    sp.option = 0;
+    sp.attr = 0;
     if ((_lock_sema_id = CreateSema(&sp)) < 0) {
         return (-1);
     }
@@ -88,7 +88,7 @@ static int fs_dummy(void)
 }
 
 //---------------------------------------------------------------------------
-static int fs_init(iop_device_t* driver)
+static int fs_init(iop_device_t *driver)
 {
     M_DEBUG("%s\n", __func__);
 
@@ -101,7 +101,7 @@ static int fs_init(iop_device_t* driver)
 }
 
 //---------------------------------------------------------------------------
-static int fs_open(iop_file_t* fd, const char* name, int flags, int mode)
+static int fs_open(iop_file_t *fd, const char *name, int flags, int mode)
 {
     M_DEBUG("%s: %s flags=%X mode=%X\n", __func__, name, flags, mode);
 
@@ -110,23 +110,27 @@ static int fs_open(iop_file_t* fd, const char* name, int flags, int mode)
 
     _fs_lock();
 
-    if (fd->privdata == NULL){
+    if (fd->privdata == NULL) {
         fd->privdata = malloc(sizeof(FIL));
     }
 
     // translate mode
-    if (mode & O_RDONLY) f_mode |= FA_READ;
-    if (mode & O_WRONLY) f_mode |= FA_WRITE;
-    if (mode & O_CREAT) f_mode |= FA_CREATE_NEW;
-    if (mode & O_TRUNC) f_mode |= FA_CREATE_ALWAYS;
-    if (mode & O_APPEND) f_mode |= FA_OPEN_APPEND;
+    if (mode & O_RDONLY)
+        f_mode |= FA_READ;
+    if (mode & O_WRONLY)
+        f_mode |= FA_WRITE;
+    if (mode & O_CREAT)
+        f_mode |= FA_CREATE_NEW;
+    if (mode & O_TRUNC)
+        f_mode |= FA_CREATE_ALWAYS;
+    if (mode & O_APPEND)
+        f_mode |= FA_OPEN_APPEND;
 
     ret = f_open(fd->privdata, name, f_mode);
 
-    if (ret == FR_OK){
+    if (ret == FR_OK) {
         ret = 1;
-    }
-    else{
+    } else {
         free(fd->privdata);
         fd->privdata = NULL;
         ret = -ENOENT;
@@ -137,9 +141,9 @@ static int fs_open(iop_file_t* fd, const char* name, int flags, int mode)
 }
 
 //---------------------------------------------------------------------------
-static int fs_close(iop_file_t* fd)
+static int fs_close(iop_file_t *fd)
 {
-    
+
 
     M_DEBUG("%s\n", __func__);
 
@@ -147,7 +151,7 @@ static int fs_close(iop_file_t* fd)
 
     _fs_lock();
 
-    if (fd->privdata){
+    if (fd->privdata) {
         ret = f_close(fd->privdata);
         free(fd->privdata);
         fd->privdata = NULL;
@@ -158,20 +162,21 @@ static int fs_close(iop_file_t* fd)
 }
 
 //---------------------------------------------------------------------------
-static int fs_lseek(iop_file_t* fd, int offset, int whence)
+static int fs_lseek(iop_file_t *fd, int offset, int whence)
 {
 
     M_DEBUG("%s\n", __func__);
 
     int res;
 
-    if (fd->privdata == NULL) return -ENOENT;
+    if (fd->privdata == NULL)
+        return -ENOENT;
 
     _fs_lock();
-    
-    FIL* file = (FIL*)(fd->privdata);
 
-    switch (whence){
+    FIL *file = (FIL *)(fd->privdata);
+
+    switch (whence) {
         case SEEK_CUR:
             offset += file->fptr;
             break;
@@ -183,47 +188,49 @@ static int fs_lseek(iop_file_t* fd, int offset, int whence)
     res = f_lseek(file, offset);
 
     _fs_unlock();
-    return (res == FR_OK)? offset : -ENOENT;
+    return (res == FR_OK) ? offset : -ENOENT;
 }
 
 //---------------------------------------------------------------------------
-static int fs_write(iop_file_t* fd, void* buffer, int size)
+static int fs_write(iop_file_t *fd, void *buffer, int size)
 {
     M_DEBUG("%s\n", __func__);
 
     int ret;
     UINT bw;
 
-    if (fd->privdata == NULL) return 0;
+    if (fd->privdata == NULL)
+        return 0;
 
     _fs_lock();
 
     ret = f_write(fd->privdata, buffer, size, &bw);
 
     _fs_unlock();
-    return (ret == FR_OK)? bw : 0;
+    return (ret == FR_OK) ? bw : 0;
 }
 
 //---------------------------------------------------------------------------
-static int fs_read(iop_file_t* fd, void* buffer, int size)
+static int fs_read(iop_file_t *fd, void *buffer, int size)
 {
     M_DEBUG("%s\n", __func__);
 
     int ret;
     UINT br;
 
-    if (fd->privdata == NULL) return 0;
+    if (fd->privdata == NULL)
+        return 0;
 
     _fs_lock();
 
     ret = f_read(fd->privdata, buffer, size, &br);
 
     _fs_unlock();
-    return (ret == FR_OK)? br : 0;
+    return (ret == FR_OK) ? br : 0;
 }
 
 //---------------------------------------------------------------------------
-static int fs_remove(iop_file_t* fd, const char* name)
+static int fs_remove(iop_file_t *fd, const char *name)
 {
     M_DEBUG("%s\n", __func__);
 
@@ -234,11 +241,11 @@ static int fs_remove(iop_file_t* fd, const char* name)
     ret = f_unlink(name);
 
     _fs_unlock();
-    return (ret == FR_OK)? 0 : -ENOENT;
+    return (ret == FR_OK) ? 0 : -ENOENT;
 }
 
 //---------------------------------------------------------------------------
-static int fs_mkdir(iop_file_t* fd, const char* name, int mode)
+static int fs_mkdir(iop_file_t *fd, const char *name, int mode)
 {
     M_DEBUG("%s\n", __func__);
 
@@ -247,13 +254,13 @@ static int fs_mkdir(iop_file_t* fd, const char* name, int mode)
     _fs_lock();
 
     ret = f_mkdir(name);
-   
+
     _fs_unlock();
-    return (ret == FR_OK)? 0 : -ENOENT;
+    return (ret == FR_OK) ? 0 : -ENOENT;
 }
 
 //---------------------------------------------------------------------------
-static int fs_dopen(iop_file_t* fd, const char* name)
+static int fs_dopen(iop_file_t *fd, const char *name)
 {
     M_DEBUG("%s: unit %d name %s\n", __func__, fd->unit, name);
 
@@ -261,16 +268,15 @@ static int fs_dopen(iop_file_t* fd, const char* name)
 
     _fs_lock();
 
-    if (fd->privdata == NULL){
+    if (fd->privdata == NULL) {
         fd->privdata = malloc(sizeof(DIR));
     }
 
     ret = f_opendir(fd->privdata, name);
 
-    if (ret == FR_OK){
+    if (ret == FR_OK) {
         ret = 1;
-    }
-    else{
+    } else {
         free(fd->privdata);
         fd->privdata = NULL;
         ret = -ENOENT;
@@ -281,7 +287,7 @@ static int fs_dopen(iop_file_t* fd, const char* name)
 }
 
 //---------------------------------------------------------------------------
-static int fs_dclose(iop_file_t* fd)
+static int fs_dclose(iop_file_t *fd)
 {
     M_DEBUG("%s\n", __func__);
 
@@ -289,36 +295,73 @@ static int fs_dclose(iop_file_t* fd)
 
     _fs_lock();
 
-    if (fd->privdata){
+    if (fd->privdata) {
         ret = f_closedir(fd->privdata);
         free(fd->privdata);
         fd->privdata = NULL;
     }
-    
+
     _fs_unlock();
     return 0;
 }
 
+//--------------------------------------------------------------------------
+
+static int fileInfoToStat(FILINFO *fno, iox_stat_t *stat)
+{
+    unsigned char *cdate = (unsigned char *)&(fno->fdate);
+    unsigned char *ctime = (unsigned char *)&(fno->ftime);
+    stat->mode = O_RDWR;
+    stat->attr = 0777;
+    stat->size = fno->fsize;
+    // set created Date: Day, Month, Year
+    stat->ctime[4] = cdate[0];
+    stat->ctime[5] = cdate[1];
+    stat->ctime[6] = cdate[2];
+    stat->ctime[7] = cdate[3];
+
+    // set created Time: Hours, Minutes, Seconds
+    stat->ctime[3] = ctime[0];
+    stat->ctime[2] = ctime[1];
+    stat->ctime[1] = ctime[2];
+
+    // set accessed Date: Day, Month, Year
+    stat->atime[4] = cdate[0];
+    stat->atime[5] = cdate[1];
+    stat->atime[6] = cdate[2];
+    stat->atime[7] = cdate[3];
+
+    // set modified Date: Day, Month, Year
+    stat->mtime[4] = cdate[0];
+    stat->mtime[5] = cdate[1];
+    stat->mtime[6] = cdate[2];
+    stat->mtime[7] = cdate[3];
+
+    // set modified Time: Hours, Minutes, Seconds
+    stat->mtime[3] = ctime[0];
+    stat->mtime[2] = ctime[1];
+    stat->mtime[1] = ctime[2];
+}
+
 //---------------------------------------------------------------------------
-static int fs_dread(iop_file_t* fd, iox_dirent_t* buffer)
+static int fs_dread(iop_file_t *fd, iox_dirent_t *buffer)
 {
     M_DEBUG("%s\n", __func__);
 
     int ret;
     FILINFO fno;
 
-    if (fd->privdata == NULL) return 0;
+    if (fd->privdata == NULL)
+        return 0;
 
     _fs_lock();
 
     ret = f_readdir(fd->privdata, &fno);
 
-    if (ret == FR_OK){
-        ret = 0;
+    if (ret == FR_OK) {
         strncpy(buffer->name, fno.fname, 255);
-        // TODO: fill buffer->stat structure from fno
-    }
-    else{
+        fileInfoToStat(&fno, &(buffer->stat));
+    } else {
         ret = -ENOENT;
     }
 
@@ -327,26 +370,32 @@ static int fs_dread(iop_file_t* fd, iox_dirent_t* buffer)
 }
 
 //---------------------------------------------------------------------------
-static int fs_getstat(iop_file_t* fd, const char* name, iox_stat_t* stat)
+static int fs_getstat(iop_file_t *fd, const char *name, iox_stat_t *stat)
 {
     M_DEBUG("%s: unit %d name %s\n", __func__, fd->unit, name);
 
     int ret;
     FILINFO fno;
 
-    if (fd->privdata == NULL) return -ENOENT;
+    if (fd->privdata == NULL)
+        return -ENOENT;
 
     _fs_lock();
 
     ret = f_stat(name, &fno);
-    // TODO: fill stat structure from fno
+
+    if (ret == FR_OK) {
+        fileInfoToStat(&fno, stat);
+    } else {
+        ret = -ENOENT;
+    }
 
     _fs_unlock();
-    return 0;
+    return ret;
 }
 
 //---------------------------------------------------------------------------
-int fs_ioctl(iop_file_t* fd, int cmd, void* data)
+int fs_ioctl(iop_file_t *fd, int cmd, void *data)
 {
     M_DEBUG("%s\n", __func__);
 
@@ -356,18 +405,18 @@ int fs_ioctl(iop_file_t* fd, int cmd, void* data)
 
     // TODO
     switch (cmd) {
-    case USBMASS_IOCTL_RENAME:
-        break;
-    case USBMASS_IOCTL_GET_CLUSTER:
-        break;
-    case USBMASS_IOCTL_GET_LBA:
-        break;
-    case USBMASS_IOCTL_GET_DRIVERNAME:
-        break;
-    case USBMASS_IOCTL_CHECK_CHAIN:
-        break;
-    default:
-        break;
+        case USBMASS_IOCTL_RENAME:
+            break;
+        case USBMASS_IOCTL_GET_CLUSTER:
+            break;
+        case USBMASS_IOCTL_GET_LBA:
+            break;
+        case USBMASS_IOCTL_GET_DRIVERNAME:
+            break;
+        case USBMASS_IOCTL_CHECK_CHAIN:
+            break;
+        default:
+            break;
     }
 
     _fs_unlock();
@@ -380,14 +429,15 @@ int fs_rename(iop_file_t *fd, const char *path, const char *newpath)
 
     int ret;
 
-    if (fd->privdata == NULL) return -ENOENT;
+    if (fd->privdata == NULL)
+        return -ENOENT;
 
     _fs_lock();
 
-    ret = f_rename(path, newpath);    
+    ret = f_rename(path, newpath);
 
     _fs_unlock();
-    return (ret == FR_OK)? 0 : -ENOENT;
+    return (ret == FR_OK) ? 0 : -ENOENT;
 }
 
 static int fs_devctl(iop_file_t *fd, const char *name, int cmd, void *arg, unsigned int arglen, void *buf, unsigned int buflen)
@@ -397,8 +447,7 @@ static int fs_devctl(iop_file_t *fd, const char *name, int cmd, void *arg, unsig
     _fs_lock();
 
     // TODO
-    switch(cmd)
-    {
+    switch (cmd) {
         case USBMASS_DEVCTL_STOP_UNIT:
             break;
         case USBMASS_DEVCTL_STOP_ALL:
@@ -415,8 +464,8 @@ static int fs_devctl(iop_file_t *fd, const char *name, int cmd, void *arg, unsig
 
 static iop_device_ops_t fs_functarray = {
     &fs_init,
-    (void*)&fs_dummy,
-    (void*)&fs_dummy,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
     &fs_open,
     &fs_close,
     &fs_read,
@@ -430,25 +479,24 @@ static iop_device_ops_t fs_functarray = {
     &fs_dclose,
     &fs_dread,
     &fs_getstat,
-    (void*)&fs_dummy,
+    (void *)&fs_dummy,
     &fs_rename,
-    (void*)&fs_dummy,
-    (void*)&fs_dummy,
-    (void*)&fs_dummy,
-    (void*)&fs_dummy,
-    (void*)&fs_dummy,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
     &fs_devctl,
-    (void*)&fs_dummy,
-    (void*)&fs_dummy,
-    (void*)&fs_dummy,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
 };
 static iop_device_t fs_driver = {
     "mass",
     IOP_DT_FS | IOP_DT_FSEXT,
     2,
     "FATFS driver",
-    &fs_functarray
-};
+    &fs_functarray};
 
 /* init file system driver */
 int InitFS(void)
