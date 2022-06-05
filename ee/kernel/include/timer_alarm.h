e8ae859ecf2f35db7db5d25ab0a0686083a20966
David Guillen Fandos <david@davidgf.net>
2022-06-05 09:35:26 +0200

    Add a timer-based alarm system based on TIM0/1
    Using TIM0/1 we can schedule events in the future. An event queue is used
    for the purpose of book keeping, holding the events in chronological
    order. A user callback is provided (running as a IRQ handler).
    
    This can be used for accurate thread sleeping (by using BIOS sempahores)

---
+++ ee/kernel/include/timer_alarm.h
@@ -0,0 +1,68 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2022, ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+*/
+
+#ifndef __TIMER_ALARM_H__
+#define __TIMER_ALARM_H__
+
+#include <tamtypes.h>
+#include <timer.h>
+
+struct timer_alarm_t;
+typedef void (*timer_alarm_callback_t)(struct timer_alarm_t *alarm, void *arg);
+
+// Defines an alarm object
+// Do not manipulate fields directly, but through functions.
+
+struct timer_alarm_t {
+    u64 scheduled_time;                  // Clock tick when the alarm should be triggered
+    u64 timer_cycles;                    // Alarm period in clock ticks
+    struct timer_alarm_t *prev, *next;   // Previous and next alarm in the sorted event list
+    timer_alarm_callback_t callback;     // User callback to call during the IRQ
+    void *usr_arg;
+};
+
+// Init/Deinit functions
+void InitTimerAlarm();
+void DeinitTimerAlarm();
+
+// Allocate a timer-based alarm
+void InitializeTimerAlarm(struct timer_alarm_t *alarm);
+// Sets up an alarm (with a user callback function and argument) that triggers some cycles in the future.
+void SetTimerAlarm(struct timer_alarm_t *alarm, u64 clock_cycles, timer_alarm_callback_t user_callback, void *user_arg);
+// Starts/Enables the alarm.
+void iStartTimerAlarm(struct timer_alarm_t *alarm);
+void StartTimerAlarm(struct timer_alarm_t *alarm);
+// Stops/Disabled the alarm.
+void iStopTimerAlarm(struct timer_alarm_t *alarm);
+void StopTimerAlarm(struct timer_alarm_t *alarm);
+// Sleeping function
+void ThreadWaitClock(u64 clock_cycles);
+
+// Conversion functions, from time to bus cycles
+static inline u64 NSecToCycles(u64 usec) {
+    // Approximate, avoid 64 bit division
+    return ((((kBUSCLK / 1000) * 65536ULL) / 1000000) * usec) >> 16;
+}
+
+static inline u64 USecToCycles(u64 usec) {
+    // Approximate, avoid 64 bit division
+    return ((((kBUSCLK / 1000) * 1024) / 1000) * usec) >> 10;
+}
+
+static inline u64 MSecToCycles(u64 msec) {
+    return (kBUSCLK / 1000) * msec;
+}
+
+static inline u64 SecToCycles(u64 sec) {
+    return kBUSCLK * sec;
+}
+
+#endif
+
---
+++ ee/kernel/samples/timer_alarm/test_alarm.c
@@ -0,0 +1,48 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2022, ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+#
+*/
+
+#include <kernel.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <timer.h>
+#include <timer_alarm.h>
+
+int flag = 0, flag2 = 0;
+
+void usercb(struct timer_alarm_t *alarm, void *arg) {
+    *(int*)arg = 1;
+    // The alarm does not trigger again unless we re-enable it manually
+    iStartTimerAlarm(alarm);
+}
+
+int main(int argc, char **argv)
+{
+    struct timer_alarm_t alarm, alarm2;
+    InitializeTimerAlarm(&alarm);
+    InitializeTimerAlarm(&alarm2);
+    SetTimerAlarm(&alarm, SecToCycles(2), &usercb, &flag);
+    SetTimerAlarm(&alarm2, MSecToCycles(1414), &usercb, &flag2);
+    StartTimerAlarm(&alarm);
+    StartTimerAlarm(&alarm2);
+
+    while (1) {
+        if (flag) {
+            printf("Hello there, 2 second tick!\n");
+            flag = 0;
+        }
+        if (flag2) {
+            printf("Hello there, sqrt(2) second tick!\n");
+            flag2 = 0;
+        }
+    }
+
+    return 0;
+}
