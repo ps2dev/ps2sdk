709cad687798966178722f31343e0b7f22fd4307
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2022-06-08 11:01:19 +0200

    Add FatFS driver for BDM devices. Adds compatibility with exFAT formatted drives.
    * fatfs dummy
    
    * test
    
    * remove documentation
    
    * Initial code.
    
    * Format.
    
    * Initial fs_driver implementation.
    
    * Added whence functionality to lseek. Added missing files.
    
    * Fixed.
    
    * It compiles now.
    
    * Cleanup.
    
    * Fixes.
    
    * Mode conversion in file open function.
    
    * Fill stat buffer.
    
    * most of devctl and ioctl is done.
    
    * ioctl check chain.
    
    * Cleanup and fixes.
    
    * Implemented disk_ioctl.
    
    * Fixes.
    
    * Fixes.
    
    * Fixes.
    
    * Fixes.
    
    * Clang format.
    
    * Fixed O_CREAT translation.
    
    * Added missing file
    
    * Small fix.
    
    * Fixed lseek.
    
    * Fixed return values.
    
    * Fixes.
    
    * Fix.
    
    * Fixed files above 4GB.
    
    * Implemented lseek64
    
    * Reduce module size.
    
    * Cleanup
    
    * Implemented chdir
    
    * Removed O_CREAT as default open flag.
    
    * Fixed file open error.
    
    * Added minimized fatfs.
    
    * Added new ioctl command to get cluster chain.
    
    * Clang format.
    
    * New ioctl commands for file chain.
    
    * Cleanup.
    
    * Cleanup.
    
    * Fix.
    
    * Removed unused fragmentation code.
    
    * Removed unused code.
    
    Co-authored-by: AKuHAK <621640+AKuHAK@users.noreply.github.com>

---
+++ iop/fs/bdmfs_fatfs/src/fs_driver.c
@@ -0,0 +1,556 @@
+/*
+ * fat_driver.c - USB Mass storage driver for PS2
+ *
+ * (C) 2004, Marek Olejnik (ole00@post.cz)
+ * (C) 2004  Hermes (support for sector sizes from 512 to 4096 bytes)
+ * (C) 2004  raipsu (fs_dopen, fs_dclose, fs_dread, fs_getstat implementation)
+ *
+ * FAT filesystem layer
+ *
+ * See the file LICENSE included with this distribution for licensing terms.
+ */
+//---------------------------------------------------------------------------
+#include <errno.h>
+#include <iomanX.h>
+#include <stdio.h>
+
+#include <sysclib.h>
+#include <sysmem.h>
+#include <thbase.h>
+#include <thsemap.h>
+
+#include <bdm.h>
+
+#include <usbhdfsd-common.h>
+
+#include "fs_driver.h"
+
+//#define DEBUG  //comment out this line when not debugging
+#include "module_debug.h"
+
+FATFS fatfs;
+struct block_device *mounted_bd;
+
+// TODO: if all drives have the same mount point, it will only allow for one mounted block device
+int connect_bd(struct block_device *bd)
+{
+    mounted_bd = bd;
+    if (f_mount(&fatfs, "", 0) == FR_OK) {
+        return 0;
+    } else {
+        mounted_bd = NULL;
+        return -1;
+    }
+}
+
+void disconnect_bd(struct block_device *bd)
+{
+    f_unmount("");
+    mounted_bd = NULL;
+    return 0;
+}
+
+//---------------------------------------------------------------------------
+static int _lock_sema_id = -1;
+
+//---------------------------------------------------------------------------
+static int _fs_init_lock(void)
+{
+    iop_sema_t sp;
+
+    M_DEBUG("%s\n", __func__);
+
+    sp.initial = 1;
+    sp.max     = 1;
+    sp.option  = 0;
+    sp.attr    = 0;
+    if ((_lock_sema_id = CreateSema(&sp)) < 0) {
+        return (-1);
+    }
+
+    return (0);
+}
+
+//---------------------------------------------------------------------------
+static void _fs_lock(void)
+{
+    M_DEBUG("%s\n", __func__);
+
+    WaitSema(_lock_sema_id);
+}
+
+//---------------------------------------------------------------------------
+static void _fs_unlock(void)
+{
+    M_DEBUG("%s\n", __func__);
+
+    SignalSema(_lock_sema_id);
+}
+
+//---------------------------------------------------------------------------
+static void fs_reset(void)
+{
+    int i;
+
+    M_DEBUG("%s\n", __func__);
+
+    if (_lock_sema_id >= 0)
+        DeleteSema(_lock_sema_id);
+
+    _fs_init_lock();
+}
+
+//---------------------------------------------------------------------------
+static int fs_inited = 0;
+
+//---------------------------------------------------------------------------
+static int fs_dummy(void)
+{
+    M_DEBUG("%s\n", __func__);
+
+    return -5;
+}
+
+//---------------------------------------------------------------------------
+static int fs_init(iop_device_t *driver)
+{
+    M_DEBUG("%s\n", __func__);
+
+    if (!fs_inited) {
+        fs_reset();
+        fs_inited = 1;
+    }
+
+    return 1;
+}
+
+//---------------------------------------------------------------------------
+static int fs_open(iop_file_t *fd, const char *name, int flags, int mode)
+{
+    M_DEBUG("%s: %s flags=%X mode=%X\n", __func__, name, flags, mode);
+
+    int ret;
+    BYTE f_mode = FA_OPEN_EXISTING;
+
+    _fs_lock();
+
+    if (fd->privdata == NULL) {
+        fd->privdata = malloc(sizeof(FIL));
+    }
+
+    // translate mode
+    if (flags & O_RDONLY)
+        f_mode |= FA_READ;
+    if (flags & O_WRONLY)
+        f_mode |= FA_WRITE;
+    if (flags & O_CREAT)
+        f_mode |= FA_OPEN_ALWAYS;
+    if (flags & O_TRUNC)
+        f_mode |= FA_CREATE_ALWAYS;
+    if (flags & O_APPEND)
+        f_mode |= FA_OPEN_APPEND;
+
+    ret = f_open(fd->privdata, name, f_mode);
+
+    if (ret != FR_OK) {
+        free(fd->privdata);
+        fd->privdata = NULL;
+        ret          = -ret;
+    } else {
+        ret = 1;
+    }
+
+    _fs_unlock();
+    return ret;
+}
+
+//---------------------------------------------------------------------------
+static int fs_close(iop_file_t *fd)
+{
+
+
+    M_DEBUG("%s\n", __func__);
+
+    int ret = FR_OK;
+
+    _fs_lock();
+
+    if (fd->privdata) {
+        ret = f_close(fd->privdata);
+        free(fd->privdata);
+        fd->privdata = NULL;
+    }
+
+    _fs_unlock();
+    return -ret;
+}
+
+//---------------------------------------------------------------------------
+
+s64 fs_lseek64(iop_file_t *fd, s64 offset, int whence)
+{
+    M_DEBUG("%s\n", __func__);
+
+    int res;
+
+    if (fd->privdata == NULL)
+        return -ENOENT;
+
+    _fs_lock();
+
+    FIL *file = (FIL *)(fd->privdata);
+
+    FSIZE_t off = offset;
+
+    switch (whence) {
+        case SEEK_CUR:
+            off += file->fptr;
+            break;
+        case SEEK_END:
+            off = file->obj.objsize - offset;
+            break;
+    }
+
+    res = f_lseek(file, off);
+
+    _fs_unlock();
+    return (res == FR_OK) ? file->fptr : -res;
+}
+
+static int fs_lseek(iop_file_t *fd, int offset, int whence)
+{
+    return fs_lseek64(fd, (s64)offset, whence);
+}
+
+//---------------------------------------------------------------------------
+static int fs_write(iop_file_t *fd, void *buffer, int size)
+{
+    M_DEBUG("%s\n", __func__);
+
+    int ret;
+    UINT bw;
+
+    if (fd->privdata == NULL)
+        return -ENOENT;
+
+    _fs_lock();
+
+    ret = f_write(fd->privdata, buffer, size, &bw);
+
+    _fs_unlock();
+    return (ret == FR_OK) ? bw : 0;
+}
+
+//---------------------------------------------------------------------------
+static int fs_read(iop_file_t *fd, void *buffer, int size)
+{
+    M_DEBUG("%s\n", __func__);
+
+    int ret;
+    UINT br;
+
+    if (fd->privdata == NULL)
+        return -ENOENT;
+
+    _fs_lock();
+
+    ret = f_read(fd->privdata, buffer, size, &br);
+
+    _fs_unlock();
+    return (ret == FR_OK) ? br : 0;
+}
+
+//---------------------------------------------------------------------------
+static int fs_remove(iop_file_t *fd, const char *name)
+{
+    M_DEBUG("%s\n", __func__);
+
+    int ret;
+
+    _fs_lock();
+
+    ret = f_unlink(name);
+
+    _fs_unlock();
+    return -ret;
+}
+
+//---------------------------------------------------------------------------
+static int fs_mkdir(iop_file_t *fd, const char *name, int mode)
+{
+    M_DEBUG("%s\n", __func__);
+
+    int ret;
+
+    _fs_lock();
+
+    ret = f_mkdir(name);
+
+    _fs_unlock();
+    return -ret;
+}
+
+//---------------------------------------------------------------------------
+static int fs_dopen(iop_file_t *fd, const char *name)
+{
+    M_DEBUG("%s: unit %d name %s\n", __func__, fd->unit, name);
+
+    int ret;
+
+    _fs_lock();
+
+    if (fd->privdata == NULL) {
+        fd->privdata = malloc(sizeof(DIR));
+    }
+
+    ret = f_opendir(fd->privdata, name);
+
+    if (ret != FR_OK) {
+        free(fd->privdata);
+        fd->privdata = NULL;
+        ret          = -ret;
+    }
+
+    _fs_unlock();
+    return ret;
+}
+
+//---------------------------------------------------------------------------
+static int fs_dclose(iop_file_t *fd)
+{
+    M_DEBUG("%s\n", __func__);
+
+    int ret = ENOENT;
+
+    _fs_lock();
+
+    if (fd->privdata) {
+        ret = f_closedir(fd->privdata);
+        free(fd->privdata);
+        fd->privdata = NULL;
+    }
+
+    _fs_unlock();
+    return -ret;
+}
+
+//--------------------------------------------------------------------------
+
+static int fileInfoToStat(FILINFO *fno, iox_stat_t *stat)
+{
+    unsigned char *cdate = (unsigned char *)&(fno->fdate);
+    unsigned char *ctime = (unsigned char *)&(fno->ftime);
+    stat->mode           = O_RDWR;
+    stat->attr           = 0777;
+    stat->size           = fno->fsize;
+    // set created Date: Day, Month, Year
+    stat->ctime[4] = cdate[0];
+    stat->ctime[5] = cdate[1];
+    stat->ctime[6] = cdate[2];
+    stat->ctime[7] = cdate[3];
+
+    // set created Time: Hours, Minutes, Seconds
+    stat->ctime[3] = ctime[0];
+    stat->ctime[2] = ctime[1];
+    stat->ctime[1] = ctime[2];
+
+    // set accessed Date: Day, Month, Year
+    stat->atime[4] = cdate[0];
+    stat->atime[5] = cdate[1];
+    stat->atime[6] = cdate[2];
+    stat->atime[7] = cdate[3];
+
+    // set modified Date: Day, Month, Year
+    stat->mtime[4] = cdate[0];
+    stat->mtime[5] = cdate[1];
+    stat->mtime[6] = cdate[2];
+    stat->mtime[7] = cdate[3];
+
+    // set modified Time: Hours, Minutes, Seconds
+    stat->mtime[3] = ctime[0];
+    stat->mtime[2] = ctime[1];
+    stat->mtime[1] = ctime[2];
+}
+
+//---------------------------------------------------------------------------
+static int fs_dread(iop_file_t *fd, iox_dirent_t *buffer)
+{
+    M_DEBUG("%s\n", __func__);
+
+    int ret;
+    FILINFO fno;
+
+    if (fd->privdata == NULL)
+        return -ENOENT;
+
+    _fs_lock();
+
+    ret = f_readdir(fd->privdata, &fno);
+
+    if (ret == FR_OK && fno.fname[0]) {
+        strncpy(buffer->name, fno.fname, 255);
+        fileInfoToStat(&fno, &(buffer->stat));
+        ret = 1;
+    } else {
+        ret = 0;
+    }
+
+    _fs_unlock();
+    return ret;
+}
+
+//---------------------------------------------------------------------------
+static int fs_getstat(iop_file_t *fd, const char *name, iox_stat_t *stat)
+{
+    M_DEBUG("%s: unit %d name %s\n", __func__, fd->unit, name);
+
+    int ret;
+    FILINFO fno;
+
+    if (fd->privdata == NULL)
+        return -ENOENT;
+
+    _fs_lock();
+
+    ret = f_stat(name, &fno);
+
+    if (ret == FR_OK) {
+        fileInfoToStat(&fno, stat);
+    } else {
+        ret = -ret;
+    }
+
+    _fs_unlock();
+    return ret;
+}
+
+//---------------------------------------------------------------------------
+int fs_ioctl(iop_file_t *fd, int cmd, void *data)
+{
+    M_DEBUG("%s\n", __func__);
+
+    int ret   = 0;
+    FIL *file = ((FIL *)(fd->privdata));
+
+    if (file == NULL)
+        return -ENXIO;
+
+    _fs_lock();
+
+    switch (cmd) {
+        case USBMASS_IOCTL_RENAME:
+            // TODO
+            // f_rename() requires two string parameters (newpath, oldpath)
+            // figure out how to get the path from the current entry object
+            ret = -ENOENT;
+            break;
+        case USBMASS_IOCTL_GET_CLUSTER:
+            ret = file->obj.sclust;
+            break;
+        case USBMASS_IOCTL_GET_LBA:
+            ret = file->obj.fs->database + (file->obj.fs->csize * (file->obj.sclust - 2));
+            break;
+        case USBMASS_IOCTL_GET_DRIVERNAME:
+            ret = *(int *)(mounted_bd->name);
+            break;
+        case USBMASS_IOCTL_CHECK_CHAIN:
+            ret = (file->obj.n_frag < 2);
+            break;
+        default:
+            break;
+    }
+
+    _fs_unlock();
+    return ret;
+}
+
+int fs_rename(iop_file_t *fd, const char *path, const char *newpath)
+{
+    M_DEBUG("%s\n", __func__);
+
+    int ret;
+
+    if (fd->privdata == NULL)
+        return -ENXIO;
+
+    _fs_lock();
+
+    ret = f_rename(path, newpath);
+
+    _fs_unlock();
+    return -ret;
+}
+
+static int fs_devctl(iop_file_t *fd, const char *name, int cmd, void *arg, unsigned int arglen, void *buf, unsigned int buflen)
+{
+    int ret;
+    FIL *file = ((FIL *)(fd->privdata));
+
+    if (file == NULL)
+        return -ENXIO;
+
+    _fs_lock();
+
+
+    switch (cmd) {
+        case USBMASS_DEVCTL_STOP_UNIT:
+        case USBMASS_DEVCTL_STOP_ALL:
+            mounted_bd->stop(mounted_bd);
+            f_unmount("");
+            mounted_bd = NULL;
+            ret        = FR_OK;
+            break;
+        default:
+            ret = -ENXIO;
+            break;
+    }
+
+    _fs_unlock();
+
+    return ret;
+}
+
+static iop_device_ops_t fs_functarray = {
+    &fs_init,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
+    &fs_open,
+    &fs_close,
+    &fs_read,
+    &fs_write,
+    &fs_lseek,
+    &fs_ioctl,
+    &fs_remove,
+    &fs_mkdir,
+    &fs_remove,
+    &fs_dopen,
+    &fs_dclose,
+    &fs_dread,
+    &fs_getstat,
+    (void *)&fs_dummy,
+    &fs_rename,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
+    &fs_lseek64,
+    &fs_devctl,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
+    (void *)&fs_dummy,
+};
+static iop_device_t fs_driver = {
+    "mass",
+    IOP_DT_FS | IOP_DT_FSEXT,
+    2,
+    "FATFS driver",
+    &fs_functarray};
+
+/* init file system driver */
+int InitFS(void)
+{
+    M_DEBUG("%s\n", __func__);
+
+    DelDrv("mass");
+    return (AddDrv(&fs_driver) == 0 ? 0 : -1);
+}
---
+++ iop/fs/bdmfs_fatfs/src/main.c
@@ -0,0 +1,63 @@
+#include <bdm.h>
+#include <intrman.h>
+#include <irx.h>
+#include <loadcore.h>
+#include <stdio.h>
+#include <sysmem.h>
+#include <cdvdman.h>
+
+#include "ff.h"
+#include "fs_driver.h"
+
+//#define DEBUG  //comment out this line when not debugging
+#include "module_debug.h"
+
+#define MAJOR_VER 1
+#define MINOR_VER 4
+
+IRX_ID("bdmff", MAJOR_VER, MINOR_VER);
+
+static struct file_system g_fs = {
+    .priv = NULL,
+    .name = "fatfs",
+    .connect_bd = connect_bd,
+    .disconnect_bd = disconnect_bd,
+};
+
+int _start(int argc, char *argv[])
+{
+    printf("BDM FatFs driver (FAT/exFAT) v%d.%d\n", MAJOR_VER, MINOR_VER);
+
+    // initialize the file system driver
+    if (InitFS() != 0) {
+        M_DEBUG("Error initializing FatFs driver!\n");
+        return MODULE_NO_RESIDENT_END;
+    }
+
+    // Connect to block device manager
+    bdm_connect_fs(&g_fs);
+
+    // return resident
+    return MODULE_RESIDENT_END;
+}
+
+void *malloc(int size)
+{
+    void *result;
+    int OldState;
+
+    CpuSuspendIntr(&OldState);
+    result = AllocSysMemory(ALLOC_FIRST, size, NULL);
+    CpuResumeIntr(OldState);
+
+    return result;
+}
+
+void free(void *ptr)
+{
+    int OldState;
+
+    CpuSuspendIntr(&OldState);
+    FreeSysMemory(ptr);
+    CpuResumeIntr(OldState);
+}
