960bb2251b4479c821297f1e625d67f91d469df1
uyjulian <uyjulian@gmail.com>
2022-12-27 07:09:30 -0600

    Clone lwip to temporary directory, then rename after cloning

--- dowload_dependencies.sh
+++ dowload_dependencies.sh
@@ -18,7 +18,8 @@ REPO_URL="https://github.com/ps2dev/lwip.git"
 REPO_FOLDER="common/external_deps/lwip"
 BRANCH_NAME="ps2-v2.0.3"
 if test ! -d "$REPO_FOLDER"; then
-  git clone --depth 1 -b $BRANCH_NAME $REPO_URL "$REPO_FOLDER" || exit 1
+  git clone --depth 1 -b $BRANCH_NAME $REPO_URL "$REPO_FOLDER"_inprogress || exit 1
+  mv "$REPO_FOLDER"_inprogress "$REPO_FOLDER"
 else
   (cd "$REPO_FOLDER" && git fetch origin && git reset --hard "origin/${BRANCH_NAME}" && git checkout "$BRANCH_NAME" && cd - )|| exit 1
 fi
