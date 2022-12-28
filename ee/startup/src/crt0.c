e9f7d9f209730a7d9ca6063571c1a7ab33b9ada8
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2022-12-28 15:41:05 +0100

    Migrate crt0 to ps2sdk and C

---
+++ ee/startup/src/crt0.c
@@ -0,0 +1,165 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright (c) 2003  Marcus R. Brown <mrbrown@0xd6.org>
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+*/
+
+#include <kernel.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdnoreturn.h>  // noreturn
+#include <string.h>
+
+extern char* _end;
+extern char* _heap_size;
+extern char* _fbss;
+extern char* _stack;
+extern char* _stack_size;
+
+__attribute__((weak)) void _ps2sdk_memory_init();
+__attribute__((weak)) void _init();
+__attribute__((weak)) void _fini();
+
+void _libcglue_args_parse(int argc, char** argv);
+void _libcglue_init();
+void _libcglue_deinit();
+int main(int argc, char** argv);
+
+// System provided arguments when loaded normally
+struct sargs {
+    int32_t argc;
+    char* argv[16];
+    char payload[256];
+};
+struct sargs _args;
+
+// ps2link provided arguments
+struct sargs_ps2link {
+    int32_t pid;
+    struct sargs args;
+};
+struct sargs_ps2link *pargs_ps2link;
+
+// We need to do a backup of pargs_ps2link and _args because memset will wipe it up.
+static void clear_bss_area()
+{
+	struct sargs _args_backup;
+	struct sargs_ps2link pargs_ps2link_backup;
+
+	// Backup
+	memcpy(&_args_backup, &_args, sizeof(struct sargs));
+	memcpy(&pargs_ps2link_backup, pargs_ps2link, sizeof(struct sargs_ps2link));
+
+	// Wipe
+	memset(&_fbss, 0, &_end - &_fbss);
+
+	// Restore
+	memcpy(&_args, &_args_backup, sizeof(struct sargs));
+	memcpy(pargs_ps2link, &pargs_ps2link_backup, sizeof(struct sargs_ps2link));
+}
+
+/*
+ * Intermediate function between _start and main, stack can be used as normal.
+ */
+noreturn void _main()
+{
+    int retval;
+    struct sargs *pa;
+
+    // clear bss area
+    clear_bss_area();
+
+    // initialize heap
+    SetupHeap(&_end, (int)&_heap_size);
+
+    // writeback data cache
+    FlushCache(0);
+
+    // Capability to override 32MiB on DESR/DVR models
+    // NOTE: this call can restart the application
+    if (_ps2sdk_memory_init)
+        _ps2sdk_memory_init();
+    
+    // Use ps2link arguments if loaded by ps2link
+    pa = &_args;
+    if (_args.argc == 0 && pargs_ps2link != NULL && pargs_ps2link->args.argc != 0)
+        pa = &pargs_ps2link->args;
+
+    // call libcglue argument parsing
+	_libcglue_args_parse(pa->argc, pa->argv);
+
+    // initialize libcglue
+    _libcglue_init();
+
+    // call global constructors (weak)
+    if (_init)
+        _init();
+
+    // Initialize the kernel (Apply necessary patches).
+    _InitSys();
+
+    // Enable interruts
+    EI();
+
+    // call main
+    retval = main(pa->argc, pa->argv);
+
+    // call global destructors (weak)
+    if (_fini)
+        _fini();
+
+    // uninitialize libcglue
+    _libcglue_deinit();
+
+    // Exit
+    Exit(retval);
+}
+
+noreturn void _exit(int status)
+{
+	// call global destructors (weak)
+    if (_fini)
+        _fini();
+
+    // uninitialize libcglue
+    _libcglue_deinit();
+
+    // Exit
+    Exit(status);
+}
+
+/*
+ * First function to be called by the loader
+ * This function sets up the stack and heap.
+ * DO NOT USE THE STACK IN THIS FUNCTION!
+ */
+noreturn void __start(struct sargs_ps2link *pargs)
+{
+    asm volatile(
+        "# Save first argument  \n"
+        "la     $2, %0 \n"
+        "sw     $4, ($2)        \n"
+        "                       \n"
+        "# SetupThread          \n"
+        "la     $4, _gp         \n"
+        "la     $5, _stack      \n"
+        "la     $6, _stack_size \n"
+        "la     $7, %1	        \n"
+        "la     $8, ExitThread  \n"
+        "move   $gp, $4         \n"
+        "addiu  $3, $0, 60      \n"
+        "syscall                \n"
+        "move   $sp, $2         \n"
+        "                       \n"
+        "# Jump to _main      	\n"
+        "j      %2           \n"
+		: /* No outputs. */
+		: "R"(pargs_ps2link), "R"(_args), "Csy"(_main));
+
+    // Prevent gcc noreturn warning
+    while(1){}
+}
