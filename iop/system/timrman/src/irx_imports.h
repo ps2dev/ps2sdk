b217605e63299be053d72083c7b8f190726837e8
Ziemas <ziemas@ziemas.se>
2023-02-05 00:26:36 +0100

    Implement TIMRMAN

---
+++ iop/system/timrman/src/irx_imports.h
@@ -0,0 +1,11 @@
+#ifndef IRX_IMPORTS_H_
+#define IRX_IMPORTS_H_
+
+#include <irx.h>
+
+/* Please keep these in alphabetical order!  */
+
+#include <intrman.h>
+#include <loadcore.h>
+
+#endif // IRX_IMPORTS_H_
