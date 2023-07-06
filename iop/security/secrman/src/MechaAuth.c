50edf69d0d1c328de6b7784acaf3c6fb1ce2c96c
Julian Uy <uyjulian@gmail.com>
2023-07-06 20:15:05 +0000

    secrman: flatten nested if/ternary statements

--- iop/security/secrman/src/MechaAuth.c
+++ iop/security/secrman/src/MechaAuth.c
@@ -38,7 +38,16 @@ int mechacon_auth_80(int cnum)
     unsigned char ret[16], parameter[16];
 
     parameter[0] = cnum;
-    return (SendMechaCommand(0x80, parameter, 1, ret) != 0 ? (ret[0] == 0 ? 1 : 0) : 0);
+
+    if (SendMechaCommand(0x80, parameter, 1, ret) == 0) {
+        return 0;
+    }
+
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 // 0x0000167c
@@ -47,7 +56,16 @@ int mechacon_auth_81(int cnum)
     unsigned char ret[16], parameter[16];
 
     parameter[0] = cnum;
-    return (SendMechaCommand(0x81, parameter, 1, ret) != 0 ? (ret[0] == 0 ? 1 : 0) : 0);
+
+    if (SendMechaCommand(0x81, parameter, 1, ret) == 0) {
+        return 0;
+    }
+
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 // 0x000016c4
@@ -60,7 +78,16 @@ int mechacon_auth_82(const void *buffer, const void *buffer2)
         parameters[i]     = ((unsigned char *)buffer)[i];
         parameters[8 + i] = ((unsigned char *)buffer2)[i];
     }
-    return (SendMechaCommand(0x82, parameters, 16, ret) != 0 ? (ret[0] == 0 ? 1 : 0) : 0);
+
+    if (SendMechaCommand(0x82, parameters, 16, ret) == 0) {
+        return 0;
+    }
+
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 // 0x00001740
@@ -69,39 +96,54 @@ int mechacon_auth_83(const void *buffer)
     unsigned char ret[16], parameters[16];
 
     memcpy(parameters, buffer, 8);
-    return (SendMechaCommand(0x83, parameters, 8, ret) != 0 ? (ret[0] == 0 ? 1 : 0) : 0);
+
+    if (SendMechaCommand(0x83, parameters, 8, ret) == 0) {
+        return 0;
+    }
+
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 // 0x000017ac
 int mechacon_auth_84(void *buffer, void *buffer2)
 {
     unsigned char ret[16];
-    int result;
 
-    if (SendMechaCommand(0x84, NULL, 0, ret) != 0 && ret[0] == 0) {
-        memcpy(buffer, &ret[1], 8);
-        memcpy(buffer2, &ret[9], 4);
-        result = 1;
-    } else
-        result = 0;
+    if (SendMechaCommand(0x84, NULL, 0, ret) == 0) {
+        return 0;
+    }
 
-    return result;
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    memcpy(buffer, &ret[1], 8);
+    memcpy(buffer2, &ret[9], 4);
+
+    return 1;
 }
 
 // 0x0000185c
 int mechacon_auth_85(void *buffer, void *buffer2)
 {
     unsigned char ret[16];
-    int result;
 
-    if (SendMechaCommand(0x85, NULL, 0, ret) != 0 && ret[0] == 0) {
-        memcpy(buffer, &ret[1], 4);
-        memcpy(buffer2, &ret[5], 8);
-        result = 1;
-    } else
-        result = 0;
+    if (SendMechaCommand(0x85, NULL, 0, ret) == 0) {
+        return 0;
+    }
 
-    return result;
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    memcpy(buffer, &ret[1], 4);
+    memcpy(buffer2, &ret[5], 8);
+
+    return 1;
 }
 
 // 0x0000190c
@@ -114,7 +156,16 @@ int mechacon_auth_86(void *buffer, void *buffer2)
         parameters[i]     = ((unsigned char *)buffer)[i];
         parameters[8 + i] = ((unsigned char *)buffer2)[i];
     }
-    return (SendMechaCommand(0x86, parameters, 16, ret) != 0 ? (ret[0] == 0 ? 1 : 0) : 0);
+
+    if (SendMechaCommand(0x86, parameters, 16, ret) != 0) {
+        return 0;
+    }
+
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 // 0x00001988
@@ -123,7 +174,16 @@ int mechacon_auth_87(void *buffer)
     unsigned char ret[16], parameters[16];
 
     memcpy(parameters, buffer, 8);
-    return (SendMechaCommand(0x87, parameters, 8, ret) != 0 ? (ret[0] == 0 ? 1 : 0) : 0);
+
+    if (SendMechaCommand(0x87, parameters, 8, ret) == 0) {
+        return 0;
+    }
+
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 // 0x000019f4
@@ -131,7 +191,15 @@ int mechacon_auth_88(void)
 {
     unsigned char ret[16];
 
-    return (SendMechaCommand(0x88, NULL, 0, ret) != 0 ? (ret[0] == 0 ? 1 : 0) : 0);
+    if (SendMechaCommand(0x88, NULL, 0, ret) == 0) {
+        return 0;
+    }
+
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 // 0x00001b88
@@ -144,23 +212,38 @@ int write_HD_start(unsigned char mode, int cnum, int arg2, unsigned short int He
     parameters[2] = (unsigned char)(HeaderLength >> 8);
     parameters[3] = cnum;
     parameters[4] = arg2;
-    return (SendMechaCommand(0x90, parameters, 5, ret) != 0 ? (ret[0] == 0 ? 1 : 0) : 0);
+
+    if (SendMechaCommand(0x90, parameters, 5, ret) == 0) {
+        return 0;
+    }
+
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 // 0x00001a88
 int write_data(const void *buffer, unsigned short int length)
 {
-    int result;
+    unsigned char ret[16], input[16];
 
-    if (length <= 16) {
-        unsigned char ret[16], input[16];
+    if (length > 16) {
+        return 0;
+    }
 
-        memcpy(input, buffer, length);
-        result = SendMechaCommand(0x8D, input, length, ret) != 0 ? (ret[0] == 0 ? 1 : 0) : 0;
-    } else
-        result = 0;
+    memcpy(input, buffer, length);
 
-    return result;
+    if (SendMechaCommand(0x8D, input, length, ret) == 0) {
+        return 0;
+    }
+
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 // 0x00001a38
@@ -169,66 +252,73 @@ int pol_cal_cmplt(void)
     unsigned char ret[16];
 
     do {
-        if (!SendMechaCommand(0x8F, NULL, 0, ret))
+        if (SendMechaCommand(0x8F, NULL, 0, ret) == 0) {
             return 0;
+        }
     } while (ret[0] == 1);
 
-    return ((ret[0] == 0) ? 1 : 0);
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 // 0x00001c98
 int func_00001c98(unsigned short int size)
 {
     unsigned char params[16], ret[16];
-    int result;
 
     params[0] = (unsigned char)size;
     params[1] = (unsigned char)(size >> 8);
-    if (SendMechaCommand(0x93, params, 2, ret)) {
-        result = ret[0] == 0 ? 1 : 0;
-    } else
-        result = 0;
 
-    return result;
+    if (SendMechaCommand(0x93, params, 2, ret) == 0) {
+        return 0;
+    }
+
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 // 0x00001be4
 int get_BIT_length(unsigned short int *BitLength)
 {
     unsigned char ret[16];
-    int result;
 
-    if (SendMechaCommand(0x91, NULL, 0, ret) != 0) {
-        if (ret[0] == 0) {
-            *BitLength = ret[1] + ((int)ret[2] << 8);
-            result     = 1;
-        } else {
-            result = 0;
-        }
-    } else {
-        result = 0;
+    if (SendMechaCommand(0x91, NULL, 0, ret) == 0) {
+        return 0;
     }
 
-    return result;
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    *BitLength = ret[1] + ((int)ret[2] << 8);
+
+    return 1;
 }
 
 // 0x00001b00
 int func_00001b00(void *data, unsigned short int length)
 {
-    int result;
+    unsigned char ret[16];
 
-    if (length <= 16) {
-        unsigned char ret[16];
+    if (length > 16) {
+        return 0;
+    }
 
-        if (SendMechaCommand(0x8E, NULL, 0, ret)) {
-            memcpy(data, ret, length);
-            result = 1;
-        } else
-            result = 0;
-    } else
-        result = 0;
+    if (SendMechaCommand(0x8E, NULL, 0, ret) == 0) {
+        return 0;
+    }
 
-    return result;
+    // NOTE: ret[0] check is omitted here
+
+    memcpy(data, ret, length);
+
+    return 1;
 }
 
 // 0x00001c48
@@ -238,80 +328,104 @@ int mechacon_set_block_size(unsigned short int size)
 
     parameters[0] = (unsigned char)size;
     parameters[1] = (unsigned char)(size >> 8);
-    return (SendMechaCommand(0x92, parameters, 2, ret) != 0 ? (ret[0] == 0 ? 1 : 0) : 0);
+
+    if (SendMechaCommand(0x92, parameters, 2, ret) == 0) {
+        return 0;
+    }
+
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 // 0x00001ce8
 int func_00001ce8(void *kbit1)
 {
     unsigned char ret[16];
-    int result;
 
-    if (SendMechaCommand(0x94, NULL, 0, ret) && ret[0] == 0) {
-        memcpy(kbit1, &ret[1], 8);
-        result = 1;
-    } else
-        result = 0;
+    if (SendMechaCommand(0x94, NULL, 0, ret) == 0) {
+        return 0;
+    }
 
-    return result;
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    memcpy(kbit1, &ret[1], 8);
+
+    return 1;
 }
 
 // 0x00001d64
 int func_00001d64(void *kbit2)
 {
     unsigned char ret[16];
-    int result;
 
-    if (SendMechaCommand(0x95, NULL, 0, ret) && ret[0] == 0) {
-        memcpy(kbit2, &ret[1], 8);
-        result = 1;
-    } else
-        result = 0;
+    if (SendMechaCommand(0x95, NULL, 0, ret) == 0) {
+        return 0;
+    }
 
-    return result;
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    memcpy(kbit2, &ret[1], 8);
+
+    return 1;
 }
 
 // 0x00001de0
 int func_00001de0(void *kc1)
 {
     unsigned char ret[16];
-    int result;
 
-    if (SendMechaCommand(0x96, NULL, 0, ret) && ret[0] == 0) {
-        memcpy(kc1, &ret[1], 8);
-        result = 1;
-    } else
-        result = 0;
+    if (SendMechaCommand(0x96, NULL, 0, ret) == 0) {
+        return 0;
+    }
 
-    return result;
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    memcpy(kc1, &ret[1], 8);
+
+    return 1;
 }
 
 // 0x00001e5c
 int func_00001e5c(void *kc2)
 {
     unsigned char ret[16];
-    int result;
 
-    if (SendMechaCommand(0x97, NULL, 0, ret) && ret[0] == 0) {
-        memcpy(kc2, &ret[1], 8);
-        result = 1;
-    } else
-        result = 0;
+    if (SendMechaCommand(0x97, NULL, 0, ret) == 0) {
+        return 0;
+    }
 
-    return result;
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    memcpy(kc2, &ret[1], 8);
+
+    return 1;
 }
 
 // 0x00001ed8
 int func_00001ed8(void *icvps2)
 {
     unsigned char ret[16];
-    int result;
 
-    if (SendMechaCommand(0x98, NULL, 0, ret) && ret[0] == 0) {
-        memcpy(icvps2, &ret[1], 8);
-        result = 1;
-    } else
-        result = 0;
+    if (SendMechaCommand(0x98, NULL, 0, ret) == 0) {
+        return 0;
+    }
 
-    return result;
+    if (ret[0] != 0) {
+        return 0;
+    }
+
+    memcpy(icvps2, &ret[1], 8);
+
+    return 1;
 }
