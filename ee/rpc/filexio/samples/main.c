4afed6df2f89a17c59486eaef10e159cc5e25179
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2023-12-05 22:15:02 +0100

    Creating fileXio sample

---
+++ ee/rpc/filexio/samples/main.c
@@ -0,0 +1,58 @@
+#include <stdint.h>
+#include <kernel.h>
+#include <loadfile.h>
+#include <stdio.h>
+#include <sifrpc.h>
+#include <string.h>
+
+int __iomanX_id = -1;
+int __fileXio_id = -1;
+
+/* References to IOMANX.IRX */
+extern unsigned char iomanX_irx[] __attribute__((aligned(16)));
+extern unsigned int size_iomanX_irx;
+
+/* References to FILEXIO.IRX */
+extern unsigned char fileXio_irx[] __attribute__((aligned(16)));
+extern unsigned int size_fileXio_irx;
+
+static int loadIRXs(void) {
+    /* IOMANX.IRX */
+    __iomanX_id = SifExecModuleBuffer(&iomanX_irx, size_iomanX_irx, 0, NULL, NULL);
+    if (__iomanX_id < 0)
+        return -1;
+
+    /* FILEXIO.IRX */
+    __fileXio_id = SifExecModuleBuffer(&fileXio_irx, size_fileXio_irx, 0, NULL, NULL);
+    if (__fileXio_id < 0)
+        return -2;
+
+    return 0;
+}
+
+static int initLibraries(void) {
+    return fileXioInit();
+}
+
+static int init_fileXio_driver() {
+    int __fileXio_init_status = loadIRXs();
+    if (__fileXio_init_status < 0)
+        return __fileXio_init_status;
+
+    __fileXio_init_status = initLibraries();
+
+    return __fileXio_init_status;
+}
+
+int main(int argc, char *argv[])
+{
+	init_fileXio_driver();
+
+	while (1)
+	{
+		printf("Hello using fileXio\n");
+	}
+
+	return 0;
+}
+
