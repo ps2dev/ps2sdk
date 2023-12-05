c1ceffce16e1ffc4d0fd4a694a5e7ba0af96f8a2
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2023-12-05 20:19:55 +0100

    Update timezone logic

--- ee/libcglue/src/timezone.c
+++ ee/libcglue/src/timezone.c
@@ -14,37 +14,25 @@
  */
 
 #include <time.h>
-#include <sys/_tz_structs.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <ps2sdkapi.h>
 #define OSD_CONFIG_NO_LIBCDVD
 #include "osd_config.h"
-
-static char _ps2sdk_tzname[15];
 
 __attribute__((weak))
 void _libcglue_timezone_update()
 {
-	// Set TZ and call tzset to ensure that timezone information won't get overwritten when tszet is called multiple times
-	// The TZ environment variable parsing in newlib is broken in various ways:
-	// * It doesn't support arbritary characters in the timezone name using angle brackets
-	// * The timezone offset sign is inverted
-	setenv("TZ", "", 0);
-	tzset();
-
-	// Set tzinfo manually instead.
-
-	__tzinfo_type *tz = __gettzinfo();
-
-	// _timezone is in seconds, while the return value of configGetTimezone is in minutes
-	// Add one hour if configIsDaylightSavingEnabled is 1
-	// _timezone is offset from local time to UTC (not UTC to local time), so flip the sign
-	_timezone = -((configGetTimezone() + (configIsDaylightSavingEnabled() * 60)) * 60);
-	tz->__tzrule[0].offset = _timezone;
-	snprintf(_ps2sdk_tzname, sizeof(_ps2sdk_tzname), "Etc/GMT%+ld", _timezone / 3600);
-	_tzname[0] = _ps2sdk_tzname;
-	_tzname[1] = _ps2sdk_tzname;
-
-	// Don't perform DST conversion
-	_daylight = 0;
+    /* Initialize timezone from PS2 OSD configuration */
+    int tzOffset = 0;
+	_io_driver driver = { _ps2sdk_open, _ps2sdk_close, _ps2sdk_read };
+	configGetTimezoneWithIODriver(&driver);
+    int tzOffsetAbs = tzOffset < 0 ? -tzOffset : tzOffset;
+    int hours = tzOffsetAbs / 60;
+    int minutes = tzOffsetAbs - hours * 60;
+    int daylight = configIsDaylightSavingEnabledWithIODriver(&driver);
+    static char tz[15];
+    sprintf(tz, "GMT%s%02i:%02i%s", tzOffset < 0 ? "+" : "-", hours, minutes, daylight ? "DST" : "");
+    setenv("TZ", tz, 1);
 }
