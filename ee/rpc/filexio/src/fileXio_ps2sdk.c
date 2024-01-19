a76c03837bb6189d52d24f83fd8029ea4b646b7a
Julian Uy <uyjulian@gmail.com>
2024-01-18 18:25:11 -0600

    fix: when copying filename, use filename argument of file info

--- ee/rpc/filexio/src/fileXio_ps2sdk.c
+++ ee/rpc/filexio/src/fileXio_ps2sdk.c
@@ -97,7 +97,7 @@ int __fileXioOpenHelper(_libcglue_fdman_fd_info_t *info, const char *buf, int fl
             return -ENOMEM;
         }
         userdata->fd = iop_fd;
-        memcpy(userdata, buf, buf_len);
+        memcpy(userdata->filename, buf, buf_len);
         userdata->filename[buf_len] = '\x00';
         info->userdata = (void *)userdata;
         info->ops = is_dir ? &__fileXio_fdman_ops_dir : &__fileXio_fdman_ops_file;
