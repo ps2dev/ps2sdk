e2c929ff7b53298f407b934ee0e771134120edb5
Matías Israelson <57065102+israpps@users.noreply.github.com>
2024-02-19 11:30:44 -0300

    [ppctty] simpler DECKARD console check

--- iop/debug/ppctty/README.md
+++ iop/debug/ppctty/README.md
@@ -1,5 +1,7 @@
 # PPC TTY
 
-IOP Module to redirect IOP Printf from stdout to PowerPC UART on DECKARD Models (`SCPH-75xxx` and up)
+IOP Module to redirect IOP Printf from stdout to PowerPC UART on DECKARD Models (`SCPH-75xxx` and up).
+
+If the module detects the console is NOT a DECKARD model, it will abort its start process and unload from IOP.
 
 Coded by @asmblur
--- iop/debug/ppctty/src/main.c
+++ iop/debug/ppctty/src/main.c
@@ -3,18 +3,10 @@
 #include <loadcore.h>
 #include <excepman.h>
 #include <ioman.h>
-#include <fcntl.h>
+#include <iop_regs.h>
 #include "tty.h"
 
 IRX_ID(MODNAME, 1, 0);
-
-#define IS_DECKARD() \
-({ \
-    u16 iop_revision; \
-    asm("mfc0 %0, $15\n" \
-        : "=r"(iop_revision):); \
-    iop_revision == 0x30; \
-})
 
 volatile uint8_t *PPC_UART_TX = (volatile uint8_t *) 0x1F80380C;
 
@@ -29,7 +21,7 @@ int _start(int argc, char *argv[])
 {
     (void)argc;
     (void)argv;
-    if (!IS_DECKARD())
+    if (IOP_CPU_TYPE == IOP_TYPE_MIPSR3000)
     {
         printf(MODNAME ": THIS PS2 IS NOT DECKARD\n");
         return MODULE_NO_RESIDENT_END;
