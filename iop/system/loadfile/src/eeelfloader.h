f1c1b3fbf9d592ece99cec89d57d4f776ac68f92
Julian Uy <uyjulian@gmail.com>
2024-03-22 20:10:55 -0500

    refactor: [loadfile] add/adjust copyright header

--- iop/system/loadfile/src/eeelfloader.h
+++ iop/system/loadfile/src/eeelfloader.h
@@ -1,3 +1,12 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+*/
 
 #ifndef __EEELFLOADER_H__
 #define __EEELFLOADER_H__
