abb33e3470149402b77a065deb2908d19b46a1ee
Julian Uy <uyjulian@gmail.com>
2024-03-22 20:11:55 -0500

    refactor: [loadfile] format

--- iop/system/loadfile/src/eeelfloader.h
+++ iop/system/loadfile/src/eeelfloader.h
@@ -11,7 +11,9 @@
 #ifndef __EEELFLOADER_H__
 #define __EEELFLOADER_H__
 
-extern int loadfile_elfload_innerproc(const char *filename, int epc, const char *section_name, int *result_out, int *result_module_out);
-extern int loadfile_mg_elfload_proc(const char *filename, int epc, const char *section_name, int *result_out, int *result_module_out);
+extern int loadfile_elfload_innerproc(
+	const char *filename, int epc, const char *section_name, int *result_out, int *result_module_out);
+extern int loadfile_mg_elfload_proc(
+	const char *filename, int epc, const char *section_name, int *result_out, int *result_module_out);
 
 #endif
