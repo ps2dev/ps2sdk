8d637d3cd473f4af939753f644de145ea47db2f8
Matías Israelson <57065102+israpps@users.noreply.github.com>
2024-04-16 16:20:37 -0300

    add HDDLOAD module
    Co-Authored-By: Liu Woon Yung <ysai187@yahoo.com>

---
+++ iop/hdd/hddload/src/irx_imports.h
@@ -0,0 +1,19 @@
+#ifndef IOP_IRX_IMPORTS_H
+#define IOP_IRX_IMPORTS_H
+
+#include <irx.h>
+
+/* Please keep these in an alphabetical order!  */
+#include <atad.h>
+#include <cdvdman.h>
+#include <dev9.h>
+#include <intrman.h>
+#include <loadcore.h>
+#include <stdio.h>
+#include <sifman.h>
+#include <secrman.h>
+#include <sysclib.h>
+#include <sysmem.h>
+#include <thbase.h>
+
+#endif /* IOP_IRX_IMPORTS_H */
