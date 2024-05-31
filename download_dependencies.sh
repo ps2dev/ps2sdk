ec179d804d2b1608d4e601f14b65f8232eb5bf20
André Guilherme <75867486+Wolf3s@users.noreply.github.com>
2024-05-31 06:21:23 -0300

    Add lwip specific names for dependencies

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -14,12 +14,12 @@ if [ "x$1" != "xlocked" ]; then
 fi
 
 ## Download LWIP
-REPO_URL="https://github.com/ps2dev/lwip.git"
-REPO_FOLDER="common/external_deps/lwip"
-BRANCH_NAME="ps2-v2.0.3"
-if test ! -d "$REPO_FOLDER"; then
-  git clone --depth 1 -b $BRANCH_NAME $REPO_URL "$REPO_FOLDER"_inprogress || exit 1
-  mv "$REPO_FOLDER"_inprogress "$REPO_FOLDER"
+LWIP_REPO_URL="https://github.com/ps2dev/lwip.git"
+LWIP_REPO_FOLDER="common/external_deps/lwip"
+LWIP_BRANCH_NAME="ps2-v2.0.3"
+if test ! -d "$LWIP_REPO_FOLDER"; then
+  git clone --depth 1 -b $LWIP_BRANCH_NAME $LWIP_REPO_URL "$LWIP_REPO_FOLDER"_inprogress || exit 1
+  mv "$LWIP_REPO_FOLDER"_inprogress "$LWIP_REPO_FOLDER"
 else
-  (cd "$REPO_FOLDER" && git fetch origin && git reset --hard "origin/${BRANCH_NAME}" && git checkout "$BRANCH_NAME" && cd - )|| exit 1
+  (cd "$LWIP_REPO_FOLDER" && git fetch origin && git reset --hard "origin/${LWIP_BRANCH_NAME}" && git checkout "$LWIP_BRANCH_NAME" && cd - )|| exit 1
 fi
