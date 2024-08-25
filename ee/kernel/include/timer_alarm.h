a256f6e2fadc6a4ee18810a7499e4aed96870245
Alessandro Mascolo <manoplayinc@gmail.com>
2024-08-25 20:06:02 +0200

    Update timer_alarm.h for C++ support

--- ee/kernel/include/timer_alarm.h
+++ ee/kernel/include/timer_alarm.h
@@ -14,11 +14,19 @@
 #include <tamtypes.h>
 #include <timer.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern u64 AlarmHandler(s32 alarm_id, u64 scheduled_time, u64 actual_time, void *arg, void *last_pc);
 extern s32 iSetTimerAlarm(u64 clock_cycles, timer_alarm_handler_t callback_handler, void *arg);
 extern s32 SetTimerAlarm(u64 clock_cycles, timer_alarm_handler_t callback_handler, void *arg);
 extern s32 iReleaseTimerAlarm(s32 id);
 extern s32 ReleaseTimerAlarm(s32 id);
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif
 
