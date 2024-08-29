f66c151449c55e6704c80d6270b816cde7fa93d1
Julian Uy <uyjulian@gmail.com>
2024-08-29 11:55:33 -0500

    fix: IOP gp register access on newer compiler
    Now the respective functions are always exposed

--- iop/kernel/include/defs.h
+++ iop/kernel/include/defs.h
@@ -34,50 +34,38 @@ extern "C" {
 #define KSEG1ADDR(a)	((__typeof__(a))(((u32)(a) & 0x1fffffff) | KSEG1))
 
 #if !defined(USE_GP_REGISTER)
-#if __GNUC__ > 3
-#define USE_GP_REGISTER 0
-#else
 #define USE_GP_REGISTER 1
 #endif
-#endif
 
-#if USE_GP_REGISTER
+static __inline__ void SetGP(void *gp)
+{
+	register void *gpReg __asm__("gp");
+
+	gpReg = gp;
+	__asm__ __volatile__(" " ::"r"(gpReg));
+}
+
+extern void *_gp;
+
+static __inline__ void *GetGP(void)
+{
+	register void *gpReg __asm__("gp");
+
+	__asm__ __volatile__(" " : "=r"(gpReg));
+	return gpReg;
+}
+
 static __inline__ void *ChangeGP(void *gp)
 {
 	void *OldGP;
 
-	__asm__ volatile(	"move %0, $gp\n"
-				"move $gp, %1"
-				: "=&r"(OldGP)
-				: "r"(gp)
-				: "gp", "memory");
+	OldGP = GetGP();
+	SetGP(gp);
 
 	return OldGP;
 }
 
-static __inline__ void SetGP(void *gp)
-{
-	__asm__ volatile(	"move $gp, %0"
-				:
-				: "r"(gp)
-				: "gp", "memory");
-}
-
-extern void *_gp;
 #define SetModuleGP()	ChangeGP(&_gp)
-
-static __inline__ void *GetGP(void)
-{
-	void *gp;
-
-	__asm__ volatile(	"move %0, $gp"
-				: "=r"(gp)
-				:
-				: "memory");
-
-	return gp;
-}
-#endif
 
 static inline void *iop_memcpy(void *dest, const void *src, int size)
 {
