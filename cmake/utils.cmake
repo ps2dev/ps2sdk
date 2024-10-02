f5b0d72b8a83aece9b97b2a65193a916fc16911e
Ziemas <ziemas@ziemas.se>
2024-10-02 06:28:17 +0200

    Avoid separate list var for compile_multiple

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,8 +1,10 @@
-function(compile_multiple target objects srcfile)
-    foreach(obj ${objects})
+function(compile_multiple target srcfile)
+    cmake_parse_arguments(PARSE_ARGV 2 "arg" "" "" "OBJECTS")
+
+    foreach(obj ${arg_OBJECTS})
         add_library(${obj} OBJECT ${srcfile})
         get_filename_component(def ${obj} NAME_WLE)
-        target_compile_definitions(${obj} PUBLIC "F_${def}")
+        target_compile_definitions(${obj} PRIVATE "F_${def}")
         target_include_directories(${obj} PRIVATE include)
         target_link_libraries(${target} private ${obj})
     endforeach()
