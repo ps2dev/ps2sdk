8a26992fa1ee14b1f6eb6ca276531de642ed87c7
Ziemas <ziemas@ziemas.se>
2024-10-05 10:51:42 +0200

    Add erl, and build erl for patches

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -14,3 +14,19 @@ function(compile_multiple target srcfile)
     endforeach()
 endfunction()
 
+# Add an erl output for a given target
+function(target_add_erl target)
+    add_custom_command(OUTPUT "lib${target}.erl"
+        COMMAND ${CMAKE_C_COMPILER} -nostdlib -Wl,-r -Wl,-d -o "lib${target}.erl" $<TARGET_OBJECTS:${target}>
+        COMMAND ${CMAKE_STRIP} --strip-unneeded -R .mdebug.eabi64 -R .reginfo -R .comment lib${target}.erl
+        DEPENDS ${target}
+        COMMAND_EXPAND_LISTS
+    )
+    add_custom_target(${target}_erl ALL
+        DEPENDS lib${target}.erl
+    )
+    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/lib${target}.erl
+        DESTINATION lib
+    )
+endfunction()
+
