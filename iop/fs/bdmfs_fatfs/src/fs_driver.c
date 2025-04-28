e5362b3836779c8dc000dc0650afed114190dd72
DKWDRV <166951701+DKWDRV@users.noreply.github.com>
2025-04-28 15:28:53 +0200

    Update fs_driver.c ioctl2 implement USBMASS_IOCTL_GET_USB_DEVICE_PORT_NUMBER

--- iop/fs/bdmfs_fatfs/src/fs_driver.c
+++ iop/fs/bdmfs_fatfs/src/fs_driver.c
@@ -707,6 +707,31 @@ int fs_ioctl2(iop_file_t *fd, int cmd, void *data, unsigned int datalen, void *r
                 ret = 0;
             }
             break;
+        case USBMASS_IOCTL_GET_USB_DEVICE_PORT_NUMBER:
+            struct block_device* bd = fatfs_fs_driver_get_mounted_bd_from_index(file->obj.fs->pdrv);
+            if (bd == NULL)
+                ret = -ENXIO;
+
+            if(strncmp(bd->name, "mass", 4) != 0)
+                ret = -ENXIO;
+
+            struct scsi_interface *scsi = (struct scsi_interface *)bd->priv;
+            if (scsi == NULL)
+                ret = -ENXIO;
+            
+            mass_dev *dev = (mass_dev *)scsi->priv;
+
+            if (dev == NULL)
+                ret = -ENXIO;
+
+            /*
+            <0 = Invalid
+            00 = Root Hub,
+            01 = Rightmost port, 
+            02 = Leftmost Port
+            */
+            ret = dev->usbPortNumber;
+            break;
         }
         default:
             break;
