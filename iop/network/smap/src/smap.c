726e11edcc8582e19cf0c56c7c30da4d6af3d945
Julian Uy <uyjulian@gmail.com>
2026-03-20 19:15:51 -0500

    fix: [smap] Remove parentheses in debug output that cause it to not actually call

--- iop/network/smap/src/smap.c
+++ iop/network/smap/src/smap.c
@@ -912,11 +912,9 @@ void SMAPOutputDebugInformation(void)
             rx_bd[i].pointer != 0 ||
             i == bdidx) {
             if (i == bdidx)
-                DEBUG_PRINTF()
-                (" - rx_bd[%d]: 0x%x / 0x%x / %d / 0x%x <--\n", i, rx_bd[i].ctrl_stat, rx_bd[i].reserved, rx_bd[i].length, rx_bd[i].pointer);
+                DEBUG_PRINTF(" - rx_bd[%d]: 0x%x / 0x%x / %d / 0x%x <--\n", i, rx_bd[i].ctrl_stat, rx_bd[i].reserved, rx_bd[i].length, rx_bd[i].pointer);
             else
-                DEBUG_PRINTF()
-                (" - rx_bd[%d]: 0x%x / 0x%x / %d / 0x%x\n", i, rx_bd[i].ctrl_stat, rx_bd[i].reserved, rx_bd[i].length, rx_bd[i].pointer);
+                DEBUG_PRINTF(" - rx_bd[%d]: 0x%x / 0x%x / %d / 0x%x\n", i, rx_bd[i].ctrl_stat, rx_bd[i].reserved, rx_bd[i].length, rx_bd[i].pointer);
         }
     }
 
