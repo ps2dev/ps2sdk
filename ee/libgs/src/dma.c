0823176cf6e25fd3a73ef71b9c7f26fb5a0817f3
Julian Uy <uyjulian@gmail.com>
2026-05-08 07:43:39 -0500

    cleanup: convert MMIO assembly to C

--- ee/libgs/src/dma.c
+++ ee/libgs/src/dma.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <kernel.h>
 #include <libgs.h>
+#include <ee_regs.h>
 
 #include "internal.h"
 
@@ -59,30 +60,20 @@ void GsDmaInit(void)
 {
 	/* This appears to have been based on code from Sony that initializes DMA channels 0-9, in bulk.
            Reset/init DMA CH 2 (GIF) only. */
-	__asm__(
-	"li	$2,0x1000A000	\n"
-	"sw	$0,0x80($2)		\n" // D2_SADR = 0. Documented to not exist, but is done.
-	"sw	$0,0($2)		\n" // D2_CHCR = 0
-	"sw	$0,0x30($2)		\n" // D2_TADR = 0
-	"sw	$0,0x10($2)		\n" // D2_MADR = 0
-	"sw	$0,0x50($2)		\n" // D2_ASR1 = 0
-	"sw	$0,0x40($2)		\n" // D2_ASR0 = 0
-	"li	$2,0xFF1F		\n" // Clear all interrupt status under D_STAT, other than SIF0, SIF1 & SIF2.
-	"sw	$2,0x1000E010	\n"
-	"lw	$2,0x1000E010	\n"
-	"lui	$3,0xFF1F		\n" // Clear all interrupt masks under D_STAT, other SIF0, SIF1 & SIF2. Writing a 1 reverses the bit.
-	"and	$2,$3		\n"
-	"sw	$2,0x1000E010	\n"
-	"sw	$0,0x1000E000	\n" // D_CTRL = 0
-	"sw	$0,0x1000E020	\n" // D_PCR = 0
-	"sw	$0,0x1000E030	\n" // D_SQWC = 0
-	"sw	$0,0x1000E050	\n" // D_RBOR = 0
-	"sw	$0,0x1000E040	\n" // D_RBSR = 0
-	"li	$3,1			\n"
-	"lw	$2,0x1000E000	\n"
-	"ori	$3,$2,1		\n" // D_CTRL (DMAE 1)
-	"sw	$3,0x1000E000	\n"
-	);
+	*((vu32 *)0x1000A080) = 0; // D2_SADR = 0. Documented to not exist, but is done.
+	*R_EE_D2_CHCR = 0;
+	*R_EE_D2_TADR = 0;
+	*R_EE_D2_MADR = 0;
+	*R_EE_D2_ASR1 = 0;
+	*R_EE_D2_ASR0 = 0;
+	*R_EE_D_STAT = 0xFF1F; // Clear all interrupt status under D_STAT, other than SIF0, SIF1 & SIF2.
+	*R_EE_D_STAT &= 0xFF1F << 16; // Clear all interrupt masks under D_STAT, other SIF0, SIF1 & SIF2. Writing a 1 reverses the bit.
+	*R_EE_D_CTRL = 0;
+	*R_EE_D_PCR = 0;
+	*R_EE_D_SQWC = 0;
+	*R_EE_D_RBOR = 0;
+	*R_EE_D_RBSR = 0;
+	*R_EE_D_CTRL |= 1; // (DMAE 1)
 }
 
 void GsDmaSend(const void *addr, u32 qwords)
@@ -157,6 +148,6 @@ void GsDmaSend_tag(const void *addr, u32 qwords, const GS_GIF_DMACHAIN_TAG *tag)
 
 void GsDmaWait(void)
 {
-	while(*((vu32 *)(0x1000a000)) & ((u32)1<<8));
+	while(*R_EE_D2_CHCR & ((u32)1<<8));
 	asm("":::"memory");
 }
