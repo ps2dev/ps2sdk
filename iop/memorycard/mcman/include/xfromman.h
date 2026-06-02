9521169d2233416c6c9760c6fe4e5e0085596769
Julian Uy <uyjulian@gmail.com>
2026-06-02 18:41:52 -0500

    add: Add xfromman.h header (includes mcman.h)

---
+++ iop/memorycard/mcman/include/xfromman.h
@@ -0,0 +1,16 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright (c) 2009 jimmikaelkael
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+*/
+
+#ifndef __XFROMMAN_H__
+#define __XFROMMAN_H__
+
+#include <mcman.h>
+
+#endif /* __XFROMMAN_H__ */
